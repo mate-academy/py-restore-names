f37778aaf7bbffbaf07aa5aaa6cff9815d784643
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-03 20:20:29 +0200

    Solution

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -1,3 +1,38 @@
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_empty_users_list():
+
+    users = []
+    restore_names(users)
+    assert users == []
+
+
+def test_first_name_is_none_or_missing():
+
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
