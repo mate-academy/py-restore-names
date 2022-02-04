1e7b63fe1346ae1f015b0f569b481fa5864b9709
Denys Lipin <lipin.transstream@gmail.com>
2022-02-04 16:44:53 +0200

    'Solution2'

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -12,10 +12,27 @@ def test_function_should_set_correct_first_name_if_it_none_or_missing():
              {"last_name": "Adams",
               "full_name": "Mike Adams"}]
     restore_names(users)
-    correct_user = [{"first_name": "Jack",
-                     "last_name": "Holy",
-                     "full_name": "Jack Holy"},
-                    {"first_name": "Mike",
-                     "last_name": "Adams",
-                     "full_name": "Mike Adams"}]
-    assert users == correct_user
+    correct_users = [{"first_name": "Jack",
+                      "last_name": "Holy",
+                      "full_name": "Jack Holy"},
+                     {"first_name": "Mike",
+                      "last_name": "Adams",
+                      "full_name": "Mike Adams"}]
+    assert users == correct_users
+
+
+def test_function_should_not_change_list_if_all_is_right():
+    users = [{"first_name": "Jack",
+              "last_name": "Holy",
+              "full_name": "Jack Holy"},
+             {"first_name": "Mike",
+              "last_name": "Adams",
+              "full_name": "Mike Adams"}]
+    correct_users = [{"first_name": "Jack",
+                      "last_name": "Holy",
+                      "full_name": "Jack Holy"},
+                     {"first_name": "Mike",
+                      "last_name": "Adams",
+                      "full_name": "Mike Adams"}]
+    restore_names(users)
+    assert users == correct_users
