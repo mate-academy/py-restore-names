2083990c06e6aaa2b9f70596893d67e58c365a2c
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-04 14:49:06 +0200

    'Solution'

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -1,7 +1,7 @@
 from app.restore_names import restore_names
 
 
-def test_name_none():
+def test_first_name_equals_none():
     initial_list = [
   {
     "first_name": None,
@@ -30,7 +30,7 @@ def test_name_none():
 ]
 
 
-def test_now_key_name():
+def test_key_first_name_does_not_exist():
     initial_list = [
       {
         "first_name": "Jack",
@@ -87,4 +87,33 @@ def test_list_should_be_modified():
     ]
 
     assert id(initial_list) == initial_id
+
+
+def test_nothing_should_happen():
+    initial_list = [
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+
+    restore_names(initial_list)
+    assert initial_list == [
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
 
