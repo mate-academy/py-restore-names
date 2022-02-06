3721bec8e513d368054f69931def1ed0c070cd35
nikon4uk <nikonchukmax4@gmail.com>
2022-02-06 22:40:55 +0200

    Solution

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -1,3 +1,65 @@
 from app.restore_names import restore_names
+import pytest
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "lost_user_data,restored_user_data",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="test restore with lost data"
+            ),
+        pytest.param(
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="test with valid data"
+        )
+    ]
+)
+def test_restore_names(lost_user_data, restored_user_data):
+    restore_names(lost_user_data)
+    assert lost_user_data == restored_user_data
