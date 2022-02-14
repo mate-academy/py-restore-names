a0b0d716ff25624aff6a4c2c68ba6ec933c37ac2
nikon4uk <nikonchukmax4@gmail.com>
2022-02-14 19:46:49 +0200

    was splited to more tests

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -12,9 +12,46 @@ import pytest
                     "last_name": "Holy",
                     "full_name": "Jack Holy",
                 },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+            ],
+            id="test restore None value"),
+        pytest.param(
+            [
                 {
                     "last_name": "Adams",
                     "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="test restore lost key")
+    ]
+)
+def test_restore_names_with_lost_data(lost_user_data, restored_user_data):
+    restore_names(lost_user_data)
+    assert lost_user_data == restored_user_data
+
+
+@pytest.mark.parametrize(
+    "lost_user_data,restored_user_data",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
                 }
             ],
             [
@@ -22,44 +59,11 @@ import pytest
                     "first_name": "Jack",
                     "last_name": "Holy",
                     "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
+                }
             ],
-            id="test restore with lost data"
-            ),
-        pytest.param(
-            [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            id="test with valid data"
-        )
+            id="test with valid data")
     ]
 )
-def test_restore_names(lost_user_data, restored_user_data):
+def test_restore_names_with_valid_data(lost_user_data, restored_user_data):
     restore_names(lost_user_data)
     assert lost_user_data == restored_user_data
