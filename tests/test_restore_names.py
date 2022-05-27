ec282f091908255d1e0419f8ceacb0d890afe8dc
Marko Khodan <khodanmarko@gmail.com>
2022-05-27 13:08:18 +0300

    solution

--- tests/test_restore_names.py
+++ tests/test_restore_names.py
@@ -1,3 +1,62 @@
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_first_name_when_it_is_none():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+def test_restore_first_name_when_it_is_not_in_list():
+    users = [
+
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_should_do_nothing_when_first_name_is_in_list():
+    users = [
+
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
