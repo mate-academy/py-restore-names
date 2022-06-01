5bca7b7d478720c68cf64adcfb2316c2b9d499a8
Taras Nester <tarasnester414@gmail.com>
2022-06-01 18:30:11 +0300

    added tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,74 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": None,
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+
+        )
+    ]
+)
+def test_should_replace_name_when_first_name_is_none(users, expected):
+    restore_names(users)
+    assert users == expected
+
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
+            [
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+
+        )
+    ]
+)
+def test_should_add_first_name_when_user_has_not_first_name(users, expected):
+    restore_names(users)
+    assert users == expected
