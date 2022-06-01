a0e1303f2e39cb91f10b00ca6ba967310f6ff1da
IvanRamyk <ivan.r@mate.academy>
2022-06-01 11:04:05 +0300

    wrong tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,12 +7,12 @@ from app.restore_names import restore_names
     [
         [
             [
-                {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-                {"last_name": "Adams", "full_name": "Mike Adams"},
+                {"first_name": None,
+                 "last_name": "Holy", "full_name": "Jack Holy"},
             ],
             [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+                {"first_name": "Jack", "last_name": "Holy",
+                 "full_name": "Jack Holy"},
             ],
         ]
     ],
@@ -20,24 +20,3 @@ from app.restore_names import restore_names
 def test_update_of_none_or_missing_value(users, expected_users):
     restore_names(users)
     assert users == expected_users
-
-
-@pytest.mark.parametrize(
-    "users, expected_users",
-    [
-        [
-            [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-            ],
-            [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-            ],
-        ]
-    ],
-)
-def test_do_nothing_if_value_is_correct(users, expected_users):
-    restore_names(users)
-    assert users == expected_users
-
