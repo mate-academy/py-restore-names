fce1b4e32897a7dfec27cacf44a32bbbe8799a23
Dima Svirsa <august4smile@gmail.com>
2022-06-02 14:07:09 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "user_incoming,user_output",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+            ],
+            "Jack",
+            id="first name is None"
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            "Mike",
+            id="first name is not exist"
+        )
+    ]
+)
+def test_first_name_restored(user_incoming: list,
+                             user_output: str):
+    restore_names(user_incoming)
+    assert user_incoming[0]["first_name"] == user_output
