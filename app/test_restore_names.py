9b29e1065e832e46b5d997d1fb5c8d987643921b
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-06-02 16:09:11 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,50 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames:
+    @pytest.mark.parametrize(
+        "initial_users,expected_users",
+        [
+            pytest.param(
+                [
+                    {
+                        "first_name": None,
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    }
+                ],
+                id="Should add firstname when first name is None"
+            ),
+            pytest.param(
+                [
+                    {
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    }
+                ],
+                id="Should add firstname when first name doesnt exist"
+            ),
+        ]
+    )
+    def test_correct_change(
+        self,
+        initial_users,
+        expected_users
+    ):
+        restore_names(initial_users)
+        assert initial_users == expected_users
