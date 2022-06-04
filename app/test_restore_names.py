aa226267230c42b0fcd44ba4d45433a9bbe5afda
Dorius <kornelydorius@gmail.com>
2022-06-04 09:22:59 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_empty_value():
+    user = [
+        {
+            "last_name": "Smith",
+            "full_name": "Tim Smith",
+        },
+    ]
+
+    result = [
+        {
+            "first_name": "Tim",
+            "last_name": "Smith",
+            "full_name": "Tim Smith",
+        },
+    ]
+
+    restore_names(user)
+    assert user == result
+
+
+def test_none_value():
+    user = [
+        {
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "Tim Smith",
+        },
+    ]
+
+    result = [
+        {
+            "first_name": "Tim",
+            "last_name": "Smith",
+            "full_name": "Tim Smith",
+        },
+    ]
+
+    restore_names(user)
+    assert user == result
