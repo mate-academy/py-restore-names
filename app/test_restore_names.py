42b919b80b85511c94e26ee0fe5b48ac50c5eed0
Dasha Protsko <protscodasha@icloud.com>
2022-06-04 11:29:35 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,34 +2,22 @@ import pytest
 from app.restore_names import restore_names
 
 
-class TestRestoreNames:
+def test_restore_names_user_is_none():
+    users = (
+        [{"first_name": None,
+          "last_name": "Daniels",
+          "full_name": "Jack Daniels"}])
 
-    @pytest.mark.parametrize(
-        "users_data, restored_data",
-        [
-            pytest.param(
-                [{"first_name": None,
-                  "last_name": "Holy",
-                  "full_name": "Jack Holy"}],
-                [{"first_name": "Jack",
-                  "last_name": "Holy",
-                  "full_name": "Jack Holy"}],
-                id="first_name shouldn't be None"
-            ),
-            pytest.param(
-                [{"last_name": "Adams",
-                  "full_name": "Mike Adams"}],
-                [{"first_name": "Mike",
-                  "last_name": "Adams",
-                  "full_name": "Mike Adams"}],
-                id="user should have first name restored"
-            )
-        ]
-    )
-    def test_restore_name(self,
-                          users_data,
-                          restored_data):
-        user = users_data
-        restore_names(user)
-        assert user == restored_data
+    restore_names(users)
 
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_names_not_name():
+    users = (
+        [{"last_name": "Walker",
+          "full_name": "Johnie Walker"}])
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Johnie"
