0b3ee09aaf3b1f390c24ae0fe935fa3954ff1c18
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-06-05 10:34:46 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,37 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_should_result_function():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    assert restore_names(users) is None
+
+
+def test_should_result_function_when_first_name_is_None():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+
