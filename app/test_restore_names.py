ce9727ac5f2772ee6926f1107feeae0785ca8803
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-06-05 10:38:54 +0300

    second solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -35,3 +35,20 @@ def test_should_result_function_when_first_name_is_None():
 
     assert users[0]["first_name"] == "Jack"
 
+
+def test_should_result_function_when_first_name_is_missing():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users[1]["first_name"] == "Mike"
