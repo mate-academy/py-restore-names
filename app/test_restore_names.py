b75a0a12c70a0d7ff6b4337e3a4dd50bab0459d9
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-05 21:04:50 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,44 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_add_firstname_when_firstname_is_missing():
+    user = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(user)
+
+    full_user = [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+
+    assert user == full_user
+
+
+def test_add_firstname_when_firstname_is_none():
+    user = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    restore_names(user)
+
+    full_user = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    assert user == full_user
