8e176b250fc133a14132a2c454a09cb27f3caec0
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-05 21:31:21 +0300

    Another solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,44 +1,47 @@
+import pytest
+
 from app.restore_names import restore_names
 
 
-def test_add_firstname_when_firstname_is_missing():
-    user = [
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        }
-    ]
+class TestAddFirstname:
 
-    restore_names(user)
+    @pytest.mark.parametrize(
+        "user,full_user",
+        [
+            (
+                    [
+                        {
+                            "last_name": "Adams",
+                            "full_name": "Mike Adams",
+                        }
+                    ],
+                    [
+                        {
+                            "first_name": "Mike",
+                            "last_name": "Adams",
+                            "full_name": "Mike Adams"
+                        }
 
-    full_user = [
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams"
-        }
-    ]
-
-    assert user == full_user
-
-
-def test_add_firstname_when_firstname_is_none():
-    user = [
-        {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        }
-    ]
-
-    restore_names(user)
-
-    full_user = [
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        }
-    ]
-
-    assert user == full_user
+                    ]
+            ),
+            (
+                    [
+                        {
+                            "first_name": None,
+                            "last_name": "Holy",
+                            "full_name": "Jack Holy",
+                        }
+                    ],
+                    [
+                        {
+                            "first_name": "Jack",
+                            "last_name": "Holy",
+                            "full_name": "Jack Holy",
+                        }
+                    ]
+            )
+        ]
+    )
+    def test_add_firstname(self, user, full_user):
+        restore_names(user)
+        assert user == full_user
