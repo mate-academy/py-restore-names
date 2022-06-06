6335900b1a189f2093150b3b4c32fe97a899c0dd
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-06-06 15:29:24 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,41 +2,42 @@ import pytest
 from app.restore_names import restore_names
 
 
-@pytest.mark.parametrize(
-    "users, expected",
-    [
-        pytest.param([
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            }
-        ],
-            [
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            }
-        ],
-            id="if 'first_name' is None "
-               "'first_name' should be added"),
-        pytest.param([
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ],
-            [
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ],
-            id="if no 'first_name'"
-               "'first_name' should be added")
-    ]
-)
-def test_restore_names(users, expected):
-    assert restore_names(users) == expected
+class TestRestoreNames:
+    @pytest.mark.parametrize(
+        "users, expected",
+        [
+            pytest.param([
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+                [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+                id="if 'first_name' is None "
+                   "'first_name' should be added"),
+            pytest.param([
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+                [
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+                id="if no 'first_name'"
+                   "'first_name' should be added")
+        ]
+    )
+    def test_restore_names(self, users, expected):
+        assert restore_names(users) == expected
