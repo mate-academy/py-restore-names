de1d374d8ad80ccfaa862a8c7aa6f13e2af66e30
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-06-06 15:18:08 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -29,39 +29,7 @@ from app.restore_names import restore_names
             },
         ],
             id="if no 'first_name' or first_name' is None "
-               "'first_name' should be added"),
-        # pytest.param(
-        #     [
-        #     {
-        #         "last_name": "Holy",
-        #         "full_name": "Jack Holy",
-        #       },
-        #       {
-        #         "last_name": "Adams",
-        #         "full_name": "Mike Adams",
-        #       },
-        #     ],
-        #     [
-        #       {
-        #         "first_name": "Jack",
-        #         "last_name": "Holy",
-        #         "full_name": "Jack Holy",
-        #       },
-        #       {
-        #         "first_name": "Mike",
-        #         "last_name": "Adams",
-        #         "full_name": "Mike Adams",
-        #       },
-        #     ],
-        #     id="'look' is not isogram"),
-        # pytest.param(
-        #     "Adam",
-        #     False,
-        #     id="'Adam' is not isogram"),
-        # pytest.param(
-        #     '',
-        #     True,
-        #     id="Empty string is True"),
+               "'first_name' should be added")
     ]
 )
 def test_restore_names(users, expected):
