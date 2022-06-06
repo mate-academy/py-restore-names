fe96cb53937372eb2de1d42aa98ffc5da5911de7
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-06-06 15:35:48 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,37 +6,37 @@ class TestRestoreNames:
     @pytest.mark.parametrize(
         "users, expected",
         [
-            pytest.param([
-                {
-                    "first_name": None,
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                }
-            ],
+            pytest.param(
                 [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                }
-            ],
-                id="if 'first_name' is None "
-                   "'first_name' should be added"),
-            pytest.param([
-                {
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
+                    {
+                        "first_name": None,
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    }
+                ],
                 [
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-                id="if no 'first_name'"
-                   "'first_name' should be added")
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    }
+                ],
+                id="should have first_name != None"),
+            pytest.param(
+                [
+                    {
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams",
+                    },
+                ],
+                [
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams",
+                    },
+                ],
+                id="should have key first_name")
         ]
     )
     def test_restore_names(self, users, expected):
