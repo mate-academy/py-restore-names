7d60151b965cf63e18a03f6c94839528c3b022e7
Denis <sairustv228@gmail.com>
2022-06-08 18:18:58 +0300

    v1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_first_name_undefined():
+    users = [
+        {
+            "last_name": "Rysin",
+            "full_name": "Denis Rysin"
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Denis"
+
+
+def test_first_name_is_equal_none():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Rysin",
+            "full_name": "Denis Rysin"
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Denis"
