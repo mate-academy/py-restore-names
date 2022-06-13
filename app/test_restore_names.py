2443f94fb8582d58dc6577b9b3ea14a411e66f63
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-06-13 13:08:32 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("initial_users, expected_first_name",
+                         [
+                             pytest.param(
+                                 [{
+                                     "first_name": None,
+                                     "last_name": "Holy",
+                                     "full_name": "Jack Holy",
+                                 }],
+                                 "Jack",
+                                 id="add first name when none provided"),
+                             pytest.param(
+                                 [{
+                                     "last_name": "Adams",
+                                     "full_name": "Mike Adams",
+                                 }],
+                                 "Mike",
+                                 id="add first name when completely missing"),
+                             pytest.param(
+                                 [{
+                                     "first_name": "Sam",
+                                     "last_name": "Smith",
+                                     "full_name": "Sam Smith",
+                                 }],
+                                 "Sam",
+                                 id="no changes when first name provided"),
+                         ])
+def test_modify_first_name_correctly(initial_users, expected_first_name):
+    restore_names(initial_users)
+    assert initial_users[0]["first_name"] == expected_first_name
