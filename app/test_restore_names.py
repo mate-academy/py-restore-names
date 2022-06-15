1e882d9b616e4361d31045d8c54a15afa3b0537c
kamilla-boiko <kamilla.boyko@gmail.com>
2022-06-15 15:32:12 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,23 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreName:
+    @pytest.mark.parametrize(
+        "user, expected_dict",
+        [
+            pytest.param(
+                [{"first_name": None, "full_name": "Jack Holy"}],
+                [{"first_name": "Jack", "full_name": "Jack Holy"}],
+                id="set correct `first_name` to users if it is None"
+            ),
+            pytest.param(
+                [{"full_name": "Mike Adams"}],
+                [{"first_name": "Mike", "full_name": "Mike Adams"}],
+                id="set correct `first_name` to users who do not have it"
+            )
+        ]
+    )
+    def test_restore_name_work_correctly(self, user, expected_dict):
+        restore_names(user)
+        assert user == expected_dict
