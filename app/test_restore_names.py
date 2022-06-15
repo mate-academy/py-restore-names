482ed8a0821e2becad070d15e3c912490102e00a
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-15 10:14:12 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_when_first_name_is_none():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    for user in users:
+        if user["first_name"] not in user or user["first_name"] is None:
+            restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
