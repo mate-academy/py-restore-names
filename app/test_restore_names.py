46aaed5fe2918603ac8318c217b8831e9b56f3ed
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-24 15:38:21 +0300

    Solution_2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,24 +4,20 @@ from app.restore_names import restore_names
 
 @pytest.fixture()
 def temp_user():
-    return [
-        {
-            "first_name": "Jack",
+    return {"first_name": "Jack",
             "last_name": "Holy",
-            "full_name": "Jack Holy"
-        }
-    ]
+            "full_name": "Jack Holy"}
 
 
 def test_first_name_is_none(temp_user):
     user = [
-        {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy"
-        }
+        {"first_name": None,
+         "last_name": "Holy",
+         "full_name": "Jack Holy"}
     ]
-    assert restore_names(user) == temp_user
+    restore_names(user)
+    for item in user:
+        assert item["first_name"] == temp_user["first_name"]
 
 
 def test_when_user_have_not_first_name(temp_user):
@@ -31,4 +27,6 @@ def test_when_user_have_not_first_name(temp_user):
             "full_name": "Jack Holy"
         }
     ]
-    assert restore_names(user) == temp_user
+    restore_names(user)
+    for item in user:
+        assert item["first_name"] == temp_user["first_name"]
