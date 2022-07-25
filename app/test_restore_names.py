c076bf7d233a4030760ffc25448886dca8273c27
Misha Hrynko <misha@mate.academy>
2022-07-25 11:03:40 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,30 +5,32 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users, corrected_data",
     [
-        pytest.param([
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            }
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
             ],
             [
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ],
-            id="test restore missing names and None names")
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="test restore missing names and None names"
+        )
     ]
 )
 def test_check_if_first_name_will_be_corrected(users, corrected_data):
