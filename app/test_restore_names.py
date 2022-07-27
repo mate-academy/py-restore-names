5c4f3a2161ffbc03b409d34c36c98ddeb16d4b20
Andrii Padalko <a.padalko@gmail.com>
2022-07-27 20:22:44 +0300

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,42 +2,32 @@ import pytest
 from app.restore_names import restore_names
 
 
-def test_output_empty_list_when_input_empty_list():
-    users = []
-    restore_names(users)
-    assert users == []
-
-
 def test_correct_first_name_when_first_name_is_equal_to_None():
-    users = [
+    assert restore_names([
         {
             "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
         }
-    ]
-    restore_names(users)
-    assert users == [
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        }
-    ]
+    ]) == [
+               {
+                   "first_name": "Jack",
+                   "last_name": "Holy",
+                   "full_name": "Jack Holy",
+               }
+           ]
 
 
 def test_correct_first_name_when_first_name_is_not_in_dict():
-    users = [
-       {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
-    restore_names(users)
-    assert users == [
+    assert restore_names([
         {
-            "first_name": "Mike",
             "last_name": "Adams",
             "full_name": "Mike Adams",
         },
-    ]
+    ]) == [
+               {
+                   "first_name": "Mike",
+                   "last_name": "Adams",
+                   "full_name": "Mike Adams",
+               },
+           ]
