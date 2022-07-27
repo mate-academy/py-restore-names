4a719cff7988b931c5a72733cf9b4a575cb9ef9b
Andrii Padalko <a.padalko@gmail.com>
2022-07-27 20:37:47 +0300

    Solution3

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,33 +1,37 @@
-import pytest
 from app.restore_names import restore_names
 
 
 def test_correct_first_name_when_first_name_is_equal_to_None():
-    assert restore_names([
-        {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        }
-    ]) == [
+    assert restore_names(
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            }
+        ]
+    ) == [
                {
                    "first_name": "Jack",
                    "last_name": "Holy",
-                   "full_name": "Jack Holy",
+                   "full_name": "Jack Holy"
                }
            ]
 
 
 def test_correct_first_name_when_first_name_is_not_in_dict():
-    assert restore_names([
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]) == [
+    assert restore_names(
+        [
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }
+        ]
+    ) == [
                {
                    "first_name": "Mike",
                    "last_name": "Adams",
-                   "full_name": "Mike Adams",
-               },
+                   "full_name": "Mike Adams"
+               }
            ]
+
