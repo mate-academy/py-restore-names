a4b9f14ac54287635b31e0a28c287608b8da7ac1
muhactive <muhactive@gmail.com>
2022-08-03 20:29:48 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,39 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_name():
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_if_first_name_is_none(users_name):
+    restore_names([users_name[0]])
+    assert [users_name[0]] == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+def test_if_first_name_not_in_key(users_name):
+    restore_names([users_name[1]])
+    assert [users_name[1]] == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
