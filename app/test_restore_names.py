dc0cbfcef92636a370a5b4c9c157e5166a75c907
Inna Kushnir <innakushnir63@gmail.com>
2022-08-03 22:01:02 +0300

    Solutions

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,40 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_if_first_name_none():
+    assert restore_names(
+        [
+            {
+             "first_name": None,
+             "last_name": "Spice",
+             "full_name": "Max Spice",
+             },
+            {
+             "first_name": "Adam",
+             "last_name": "Holy",
+             "full_name": "Adam Holy",
+            },
+            {
+             "last_name": "Break",
+             "full_name": "David Holy",
+             },
+        ]
+    ) == [
+            {
+             "first_name": "Max",
+             "last_name": "Spice",
+             "full_name": "Max Spice",
+             },
+            {
+             "first_name": "Adam",
+             "last_name": "Holy",
+             "full_name": "Adam Holy",
+            },
+            {
+             "first_name": "David",
+             "last_name": "Break",
+             "full_name": "David Holy",
+             },
+        ]
+
