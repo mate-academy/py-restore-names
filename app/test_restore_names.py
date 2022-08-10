d21c3a5b332eab2bb7e6b6aa9adc0472d0db5bc1
Alexandr <netiazhevskyi@gmail.com>
2022-08-10 14:49:24 +0300

    tests for restore_name

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,42 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users():
+    return [{
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+@pytest.fixture
+def result_users():
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_should_raise_exception_if_parameter_not_list():
+    with pytest.raises(TypeError):
+        restore_names(({"Firstname": "Oliver"}))
+
+
+def test_should_restore_names(users, result_users):
+    restore_names(users)
+    assert users == result_users
