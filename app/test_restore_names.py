de542bf6a6192975b7dd6f916720700c42de8a0b
Alexandr <netiazhevskyi@gmail.com>
2022-08-10 14:58:07 +0300

    tests for restore_name

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,15 +4,16 @@ from app.restore_names import restore_names
 
 @pytest.fixture
 def users():
-    return [{
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy"
-    },
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
         {
             "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
+            "full_name": "Mike Adams"
+        }
     ]
 
 
