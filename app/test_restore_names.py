e82dbaa6f2be2095212528e86dab856e1622b609
Paul Starodub <amorallex@gmail.com>
2022-09-08 11:49:41 +0300

    Solo2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,52 +1,34 @@
-import pytest
-
 from app.restore_names import restore_names
 
+import pytest
 
-class TestRestoreNames:
-    @pytest.mark.parametrize(
-        "goal, res_goal",
-        [
-            pytest.param(
-                [
-                    {
-                        "first_name": None,
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy",
-                    },
-                ],
-                [
-                    {
-                        'first_name': 'Jack',
-                        'last_name': 'Holy',
-                        'full_name': 'Jack Holy',
-                    },
-                ],
-                id="first name isnone"
-            ),
-            pytest.param(
-                [
-                    {
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams",
-                    },
-                ],
-                [
-                    {
-                        'last_name': 'Adams',
-                        'full_name': 'Mike Adams',
-                        'first_name': 'Mike'
-                    }
-                ],
-                id="first name is missing"
-            )
-        ]
-    )
-    def test_correctly_first_name(
-            self,
-            goal,
-            res_goal
-    ):
-        restore_names(goal)
 
-        assert goal == res_goal
+@pytest.fixture()
+def test_template():
+    yield [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_first_name(test_template):
+    restore_names(test_template)
+    assert test_template == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
