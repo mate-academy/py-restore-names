1a1349e25a2e98d6efde9deb328211fc0993110f
Vlad Magdenko <vladearth7@gmail.com>
2022-09-18 19:32:34 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,15 +5,15 @@ from app.restore_names import restore_names
 @pytest.fixture()
 def user_template():
     users = [
-          {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-          },
-          {
+       {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+       },
+       {
             "last_name": "Adams",
             "full_name": "Mike Adams",
-          },
+       },
         ]
     return users
 
