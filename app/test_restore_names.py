258969546603ff0447f13d97144f1ebc8a0d6c6e
Vlad Magdenko <vladearth7@gmail.com>
2022-09-18 19:35:40 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,24 +3,24 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def user_template():
+def users_template():
     users = [
-      {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-       },
-      {
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
             "last_name": "Adams",
             "full_name": "Mike Adams",
-      },
+        }
     ]
     return users
 
 
-def test_first_name_is_none(user_template):
-    restore_names(user_template)
-    assert user_template[0]["first_name"] == "Jack"
+def test_when_first_name_equal_none(users_template):
+    restore_names(users_template)
+    assert users_template[0]["first_name"] == "Jack"
 
 
 def test_first_name_is_lost(user_template):
