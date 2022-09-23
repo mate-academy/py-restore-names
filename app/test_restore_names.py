649ce9dc3457a84ccaf14d45085d0ff96fcc125c
Alex Parkhomenko <105493988+wQuelS@users.noreply.github.com>
2022-09-23 11:26:12 +0000

    Update test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,32 +5,33 @@ from app.restore_names import restore_names
 @pytest.fixture()
 def user_template():
     users = [
-          {
+        {
             "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
-          },
-          {
+        },
+        {
             "last_name": "Adams",
             "full_name": "Mike Adams",
-          },
+        }
     ]
+
     return users
 
 
 def test_restores_first_names(user_template):
     restore_names(user_template)
     assert user_template == [
-          {
+        {
             "first_name": "Jack",
             "last_name": "Holy",
             "full_name": "Jack Holy",
-          },
-          {
+        },
+        {
             "first_name": "Mike",
             "last_name": "Adams",
             "full_name": "Mike Adams",
-          },
+        }
     ]
 
 
