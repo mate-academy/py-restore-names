cbaf7fb92738ae19b539fa23b9a46f62a5774d81
mltdll <ilya.vyplavin@gmail.com>
2022-09-25 17:20:02 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,47 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+USERS = {
+    "correct": [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ],
+    "names_are_none": [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ],
+    "names_are_absent": [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+}
+
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        pytest.param(
+            USERS["names_are_none"],
+            USERS["correct"],
+            id="should restore names when `first_name` is set to None"
+        ),
+        pytest.param(
+            USERS["names_are_absent"],
+            USERS["correct"],
+            id="should restore names when `first_name` is absent"
+        )
+    ]
+)
+def test_should_restore_names_set_to_none(users, expected):
+    restore_names(users)
+    assert users == expected
