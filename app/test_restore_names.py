149669b199cf4bedebf61f3157c7594662dd74a6
Stas Okhrym <s.okhrym@gmail.com>
2022-09-27 14:17:28 +0300

    Fixxed

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -13,31 +13,22 @@ def users_template():
         {
             "last_name": "Adams",
             "full_name": "Mike Adams",
-        }
+        },
     ]
 
 
-def test_restore_names_return_correct_names(users_template):
+def test_restore_names(users_template):
     restore_names(users_template)
+
     assert users_template == [
-      {
-        "first_name": "Jack",
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "first_name": "Mike",
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      }
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
-
-
-def test_restore_names_return_empty_list(users_template):
-    users_template = []
-    restore_names(users_template)
-    assert users_template == []
-
-
-def test_restore_names_return_none(users_template):
-    assert restore_names(users_template) is None
