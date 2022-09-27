13bd3bb6d8bbe4071b21f68321267264c04c1162
Artem <Artemvyhovanec@gmail.com>
2022-09-27 19:21:42 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,expected",
+    [
+        (
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            ["Jack"]
+        ),
+        (
+            [{
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+                {
+                "first_name": None,
+                "last_name": "Black",
+                "full_name": "Bobby Black",
+            }],
+            ["Jack", "Bobby"]
+        )
+    ]
+)
+def test_correct_function_work(users, expected):
+    restore_names(users)
+    for i, names in enumerate(expected):
+        assert users[i]["first_name"] == names
