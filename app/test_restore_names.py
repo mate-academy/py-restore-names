73cb9bb3ea97be09e7dc49d5f1ae0e60a7fcdef5
Maks Danylchuk <maxitosm00@gmail.com>
2022-10-01 00:40:04 +0300

    rdy

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,27 +2,35 @@ import pytest
 from app.restore_names import restore_names
 
 
-@pytest.mark.parametrize(
-    "users,expected_users",
-    [
-        pytest.param(
-            {"first_name": None,
-             "last_name": "Holy",
-             "full_name": "Jack Holy"},
-            {"first_name": "Jack",
-             "last_name": "Holy",
-             "full_name": "Jack Holy"},
-            id="if first_name is None , take name from full_name"
-        ),
-        pytest.param(
-            {"last_name": "Adams",
-             "full_name": "Mike Adams"},
-            {"first_name": "Mike",
-             "last_name": "Adams",
-             "full_name": "Mike Adams"},
-            id="if first_name is empty, create it and take name in full_name"
-        )
+@pytest.fixture()
+def user_template():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        },
     ]
-)
-def test_restore_name(users, expected_users):
-    assert restore_names(users) == expected_users
+    return users
+
+
+def test_restore_name(user_template):
+    result = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+    restore_names(user_template)
+
+    assert user_template == result
