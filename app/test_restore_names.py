aad9b73352d017cd30dfafbe08345a3b9d72c604
Serhii Kovalenko <27kovalenko@gmail.com>
2022-10-04 14:52:36 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,71 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,restore_name",
+    [
+        (
+            [
+                {
+                    "first_name": "Homer",
+                    "last_name": "Simpson",
+                    "full_name": "Homer Simpson",
+                }
+            ],
+            [
+                {
+                    "first_name": "Homer",
+                    "last_name": "Simpson",
+                    "full_name": "Homer Simpson",
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Simpson",
+                    "full_name": "Bart Simpson",
+                }
+            ],
+            [
+                {
+                    "first_name": "Bart",
+                    "last_name": "Simpson",
+                    "full_name": "Bart Simpson",
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "last_name": "Simpson",
+                    "full_name": "Homer Simpson",
+                },
+                {
+                    "last_name": "Simpson",
+                    "full_name": "Bart Simpson",
+                }
+            ],
+            [
+                {
+                    "first_name": "Homer",
+                    "last_name": "Simpson",
+                    "full_name": "Homer Simpson",
+                },
+                {
+                    "first_name": "Bart",
+                    "last_name": "Simpson",
+                    "full_name": "Bart Simpson",
+                }
+            ],
+        ),
+        ([], []),
+    ]
+)
+def test_restore_name(users, restore_name):
+    restore_names(users)
+    assert users == restore_name, (
+        "'restore_names' should restore 'first_name' value"
+    )
