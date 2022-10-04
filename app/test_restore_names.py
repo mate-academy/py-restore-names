71d4e4b8427e227253349b6066ffeb1ff089b6e0
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-10-04 17:29:47 +0300

    solution v01

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_temp():
+    return [
+      {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+      },
+      {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+      },
+    ]
+
+
+def test_first_name_is_missing_or_equal_none(users_temp):
+    restore_names(users_temp)
+    assert users_temp == [
+      {
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+      },
+      {
+        "first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+      },
+    ]
