7ad457462341304155594b46aff1699bca08134d
Alexander <alexzubkoff123@gmail.com>
2022-10-05 23:01:34 +0300

    Wrote test for restore names func which change list of dict

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,39 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def incoming_data():
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+@pytest.fixture
+def outcoming_data():
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_names(incoming_data, outcoming_data) -> None:
+    assert incoming_data != outcoming_data
+    restore_names(incoming_data)
+    assert incoming_data == outcoming_data
