13f42f17fdc70e514af6ac13b67bee6bd6304e1b
Nataliia Zakharchuk <62181026+nattalli@users.noreply.github.com>
2022-10-05 12:02:20 +0300

    Update

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
-import pytest
 from app.restore_names import restore_names
+from _pytest.monkeypatch import MonkeyPatch
 
-# write your tests here
+
+users = [
+    {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
+]
+
+
+def test_get_exchange_rate_prediction(monkeypatch: MonkeyPatch) -> bool:
+    def mock_input() -> list:
+        return users
+
+    monkeypatch.setattr("builtins.input", mock_input)
+
+    result = restore_names(users)
+    assert result is None
+
+
+def test_get_exchange_rate_prediction1(monkeypatch: MonkeyPatch) -> bool:
+    def mock_input() -> str:
+        return users
+    monkeypatch.setattr("builtins.input", mock_input)
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
