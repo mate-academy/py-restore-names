2c1ce504d4efb3250a033419b7968c5dc7cdf3de
Yura <pazyn404@gamil.com>
2022-10-06 23:48:21 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,83 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected_users",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ],
+            [
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ]
+        ),
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ],
+            [
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ]
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ],
+            [
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ]
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                },
+                {
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ],
+            [
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                },
+                {
+                    "first_name": "fname",
+                    "last_name": "lname",
+                    "full_name": "fname lname"
+                }
+            ]
+        ),
+    ]
+)
+def test_restore_names(users: dict, expected_users: str) -> None:
+    restore_names(users)
+    assert users == expected_users
