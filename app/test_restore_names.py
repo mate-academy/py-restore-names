a34d10ea18a34cde926aac8335b9d9b224c9066a
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-06 16:06:50 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,32 +3,33 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def users_list():
+def users_list() -> list[dict]:
     users = [
-      {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
     return users
 
 
-def test_should_return_correct_first_name(users_list):
+def test_should_return_correct_first_name(users_list) -> None:
     restore_names(users_list)
     assert users_list == \
         [
-           {
-             "first_name": "Jack",
-             "last_name": "Holy",
-             "full_name": "Jack Holy",
-           },
-           {
-             "first_name": "Mike",
-             "last_name": "Adams",
-             "full_name": "Mike Adams",
-           }, ]
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
