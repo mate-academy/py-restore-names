81a563c50010797842100510ec0d1b8d0e758128
graandda <graanddadays@gmail.com>
2022-10-06 19:01:36 +0300

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,19 +2,20 @@ import pytest
 from app.restore_names import restore_names
 
 
+# fixture
 @pytest.fixture()
 def users_template():
     return [
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
 
 
 def test_restore_names(users_template):
