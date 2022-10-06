022b2d34e78ca01eaf1af1d013e7702e0870e6c5
Sergii Franchuk <Franchuksergei@gmail.com>
2022-10-06 22:20:36 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_fill_first_name_if_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    restore_names(users)
+
+    assert users[0].get("first_name") == "Jack"
+
+
+def test_add_and_fill_first_name_if_not_exist() -> None:
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    restore_names(users)
+
+    assert users[0].get("first_name") == "Jack"
