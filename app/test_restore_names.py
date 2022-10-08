4162726ae71173c1f523cab46b8de574676ee861
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-08 11:45:35 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def none_or_not_exist_user() -> list[dict]:
+    yield [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+
+
+def test_restore_names(none_or_not_exist_user: list[dict]) -> None:
+    restore_names(none_or_not_exist_user)
+    assert none_or_not_exist_user == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
