4de46cafdef939aa2003b8ee2cf033b156da1b34
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-26 17:04:13 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,60 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def person_names() -> None:
+    return restore_names([
+           {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+           },
+           {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+           },
+           {
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "Andrew Smith"
+           }
+           ])
+
+
+def test_should_make_correct_first_name(person_names) -> None:
+    list_of_dicts = [
+           {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+           },
+           {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+           },
+           {
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "Andrew Smith"
+           }
+           ]
+    list_of_expected = [
+           {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+           },
+           {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+           },
+           {
+            "first_name": "Andrew",
+            "last_name": "Smith",
+            "full_name": "Andrew Smith"
+           }
+           ]
+    restore_names(list_of_dicts)
+    assert list_of_dicts == list_of_expected
