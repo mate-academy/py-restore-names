86ccf943bdb48c4be96b0f009a6cf3a5d6c49a1e
Anastasiia <nastya.gnusova@gmail.com>
2022-11-08 16:43:50 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,36 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> list[dict]:
+    return (
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+    )
+
+
+def test_restore_name_if_user_do_not_have_first_name_or_none(
+        user_template: list[dict]) -> None:
+    restore_names(user_template)
+    assert user_template == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
