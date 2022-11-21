7b83f8ea686d7cade89dbf94cdca04f45eb833d6
Ihor Ivashov <dm-i@i.ua>
2022-11-21 14:10:41 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,31 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_template():
+    return [{
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    }]
+
+
+def test_without_key_first_name(users_template) -> None:
+    restore_names(users_template)
+
+    assert users_template[0] == {
+        "first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    }
+
+
+def test_wit_first_name_none(users_template) -> None:
+    users_template[0]["first_name"] = None
+    restore_names(users_template)
+
+    assert users_template[0] == {
+        "first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    }
