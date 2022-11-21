84e49dbc630e9abf0d0cb6588f7de1e9d9f90430
Ihor Ivashov <dm-i@i.ua>
2022-11-21 14:14:27 +0200

    Fixed annotation

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,14 +3,14 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def users_template():
+def users_template() -> list:
     return [{
         "last_name": "Adams",
         "full_name": "Mike Adams",
     }]
 
 
-def test_without_key_first_name(users_template) -> None:
+def test_without_key_first_name(users_template: object) -> None:
     restore_names(users_template)
 
     assert users_template[0] == {
@@ -20,7 +20,7 @@ def test_without_key_first_name(users_template) -> None:
     }
 
 
-def test_wit_first_name_none(users_template) -> None:
+def test_wit_first_name_none(users_template: object) -> None:
     users_template[0]["first_name"] = None
     restore_names(users_template)
 
