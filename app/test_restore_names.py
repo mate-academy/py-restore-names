373a92d5c117d2e0f9f6694d5678b1b32773b07f
vani@linux <winfkill@gmail.com>
2022-11-23 11:50:43 +0200

    Solution1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,48 +1,16 @@
 import pytest
 from app.restore_names import restore_names
-# users = [
-#   {
-#     "first_name": None,
-#     "last_name": "Holy",
-#     "full_name": "Jack Holy",
-#   },
-#   {
-#     "last_name": "Adams",
-#     "full_name": "Mike Adams",
-#   },
-# ]
-#
-# restore_names(users)
-#
-# users == [
-#   {
-#     "first_name": "Jack",
-#     "last_name": "Holy",
-#     "full_name": "Jack Holy",
-#   },
-#   {
-#     "first_name": "Mike",
-#     "last_name": "Adams",
-#     "full_name": "Mike Adams",
-#   },
-# ]
+
+
 class TestAddValueClass:
     @pytest.mark.parametrize(
         "initial_classes,expected_classes",
         [
-            ([
-                {
-                  "first_name": None,
-                  "last_name": "Holy",
-                  "full_name": "Jack Holy"
-                }
-            ],
-             [{
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy"
-            }]
-
+            (
+                [{"first_name": None, "last_name":
+                    "Holy", "full_name": "Jack Holy"}],
+                [{"first_name": "Jack", "last_name":
+                    "Holy", "full_name": "Jack Holy"}],
             ),
             (
                 [
@@ -57,10 +25,12 @@ class TestAddValueClass:
                         "last_name": "Adams",
                         "full_name": "Mike Adams",
                     }
-                ]
-            )
-        ]
+                ],
+            ),
+        ],
     )
-    def test_check_full_user_name(self,initial_classes,expected_classes):
-        assert restore_names(initial_classes) == restore_names(expected_classes)
-
+    def test_check_full_user_name(self,
+                                  initial_classes: list,
+                                  expected_classes: list) -> None:
+        assert restore_names(initial_classes) \
+               == restore_names(expected_classes)
