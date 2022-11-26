6c3bdec78a10412ad5cce81fdb7f8dac2e9192fd
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2022-11-26 09:57:20 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,53 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_name_type():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert isinstance(users[0]["first_name"], str)
+
+
+def test_name_first():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_name_second():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users[1]["first_name"] == "Mike"
