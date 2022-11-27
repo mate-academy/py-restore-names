8ba0ff1ca0c1d63547a13def90d7efb583311a5f
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-27 23:01:02 +0200

    Ready to fix mistakes

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
-from app.restore_names import restore_names
+# from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def list_of_users() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_when_first_name_is_none(list_of_users: list[dict]) -> None:
+
+    restore_names_user = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    assert list_of_users[0].restore_names() == restore_names_user
+
+
+def test_when_first_name_is_absent(list_of_users: list[dict]) -> None:
+
+    restore_names_user = [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    assert list_of_users[1].restore_names() == restore_names_user
