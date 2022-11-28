24615885059a78ad7f82219803659c1507d3d645
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-28 13:59:11 +0200

    Restore name

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,15 +1,33 @@
-# import pytest
+import pytest
+
 from app.restore_names import restore_names
 
 
-def test_when_first_name_is_none() -> None:
-    users = [
+@pytest.fixture()
+def user_1() -> list[dict]:
+    return [
         {
             "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
         }
     ]
+
+
+@pytest.fixture()
+def user_2() -> list[dict]:
+    return [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_when_first_name_is_none(user_1: list[dict]) -> None:
+
+    restore_names(user_1)
+
     restore_names_user = [
         {
             "first_name": "Jack",
@@ -17,16 +35,13 @@ def test_when_first_name_is_none() -> None:
             "full_name": "Jack Holy",
         }
     ]
-    assert restore_names(users) == restore_names_user
+    assert user_1 == restore_names_user
 
 
-def test_when_first_name_is_absent() -> None:
-    users = [
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        }
-    ]
+def test_when_first_name_is_absent(user_2: list[dict]) -> None:
+
+    restore_names(user_2)
+
     restore_names_user = [
         {
             "first_name": "Mike",
@@ -34,4 +49,4 @@ def test_when_first_name_is_absent() -> None:
             "full_name": "Mike Adams",
         }
     ]
-    assert restore_names(users) == restore_names_user
+    assert user_2 == restore_names_user
