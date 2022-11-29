7ec25f37a4a65d801a0fb8adae7a886a4b8d5c66
Mazur Mariya <mazurinka29@gmail.com>
2022-11-29 11:07:39 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,45 @@
+from unittest import mock
+
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture(scope="function")
+def user_template() -> None:
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": None,
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    yield users
+
+
+def test_should_return_jack(user_template: list) -> None:
+    restore_names(user_template)
+    assert user_template[0]["first_name"] == "Jack"
+
+
+def test_should_return_mike(user_template: list) -> None:
+    restore_names(user_template)
+    assert user_template[1]["first_name"] == "Mike"
+
+
+def test_function_was_called(user_template: list) -> None:
+    restore_names = mock.MagicMock()
+    restore_names(user_template)
+    restore_names.assert_called_once_with(user_template)
+
+
+def test_should_return_none(user_template: list) -> None:
+    assert restore_names(user_template) is None
+
+
+def test_should_raise_error_if_users_is_str() -> None:
+    with pytest.raises(TypeError):
+        restore_names("user")
