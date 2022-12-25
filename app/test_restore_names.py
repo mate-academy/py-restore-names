b477b6fd71f4e9ceb1104d4b892cccb918b0b85c
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-25 17:41:34 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,24 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+users = [
+    {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
+]
+
+
+def test_add_first_name_if_none() -> None:
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_add_first_name_if_not_in_dict() -> None:
+    restore_names(users)
+    assert users[1]["first_name"] == "Mike"
