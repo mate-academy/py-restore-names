2710f01dab0242dee3c51398db13ee101ae15415
Haishuk Pavlo <xomas@ukr.net>
2022-12-26 22:24:21 +0200

    done

--- app/restore_names.py
+++ app/restore_names.py
@@ -5,3 +5,21 @@ def restore_names(users: List[dict]) -> None:
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
             user["first_name"] = user["full_name"].split()[0]
+
+
+users = [
+  {
+    "first_name": None,
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+
+restore_names(users)
+
+print(users
+      )
--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,51 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture(scope="function")
+def users_wrong_template_to_check() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+@pytest.fixture(scope="function")
+def users_right_template() -> list:
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_check_is_first_name_exists(users_wrong_template_to_check):
+    restore_names(users_wrong_template_to_check)
+    for user in users_wrong_template_to_check:
+        assert "first_name" in user
+
+
+def test_check_is_name_not_none(users_wrong_template_to_check):
+    restore_names(users_wrong_template_to_check)
+    for user in users_wrong_template_to_check:
+        assert user["first_name"] is not None
+
+
+def test_check_the_name_is_right(users_wrong_template_to_check):
+    restore_names(users_wrong_template_to_check)
+    for user in users_wrong_template_to_check:
+        assert user["first_name"] == user["full_name"].split()[0]
