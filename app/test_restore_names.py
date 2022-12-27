065f9ab21292604e67c275d3e0f117a2a3b87ef9
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-27 17:04:13 +0200

    created test

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> list:
+    users == [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+def test_function_returns_nothing(user_template: list) -> None:
+    assert restore_names(user_template) is None
+
+def test_function_restore_names(user_template: list) -> None:
+    restore_names(user_template)
+    assert user_template[0]["first_name"] == "Jake"
+    assert user_template[1]["first_name"] == "Mike"
