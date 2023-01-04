c6ed1f4221401a89eb8396bcce4c2455bbc6b1cf
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-04 12:28:03 +0200

    Restore name function tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,16 +1,36 @@
-import pytest
 from app.restore_names import restore_names
 
 
 def test_restore_only_missing_names() -> None:
-    users = [{"last_name": "Adams", "full_name": "Mike Adams"}]
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
     restore_names(users)
-    assert users == [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}]
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
 
 
 def test_restore_only_none_names() -> None:
-    users = [{"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"}]
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
     restore_names(users)
-    assert users == [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}]
-
-
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
