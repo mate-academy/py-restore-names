d83dcc19fc533ecd6ca92a5ea8e363247896ce76
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-05 22:22:06 +0200

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,34 +5,34 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "input_list, output_list",
     [
-        ([
-             {
-                 "first_name": None,
-                 "last_name": "Holy",
-                 "full_name": "Jack Holy",
-             },
-             {
-                 "last_name": "Adams",
-                 "full_name": "Mike Adams",
-             },
-         ], [
-             {
-                 "first_name": "Jack",
-                 "last_name": "Holy",
-                 "full_name": "Jack Holy",
-             },
-             {
-                 "first_name": "Mike",
-                 "last_name": "Adams",
-                 "full_name": "Mike Adams",
-             },
-         ])
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+        [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
     ]
-
 )
 def test_func_is_working(
         input_list: list,
         output_list: list
-):
+) -> None:
     restore_names(input_list)
     assert input_list == output_list
