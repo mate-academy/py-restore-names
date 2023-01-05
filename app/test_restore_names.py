3436d168804ddf6e3ede4cccdd4b79896d601502
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-01-05 22:28:20 +0000

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,36 +1,45 @@
 from app.restore_names import restore_names
+import pytest
 
 
-def test_name() -> None:
-    users = [
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        }
-    ]
-    restore_names(users)
-    assert users == [
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        }
-    ]
-
-
-def test_none() -> None:
-    users = [
+@pytest.fixture()
+def list_none_template() -> list:
+    return [
         {
             "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
         }
     ]
-    restore_names(users)
-    assert users == [
+
+
+@pytest.fixture()
+def list_missed_template() -> list:
+    return [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_first_name_none(list_none_template: list) -> None:
+    restore_names(list_none_template)
+    assert list_none_template == [
         {
             "first_name": "Jack",
             "last_name": "Holy",
             "full_name": "Jack Holy",
         }
+    ]
+
+
+def test_first_name_missed(list_missed_template: list) -> None:
+    restore_names(list_missed_template)
+    assert list_missed_template == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
