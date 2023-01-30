48cddfb4fc2229104f606573cfcf2087f76823d2
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-30 17:30:37 +0100

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
 import pytest
+from copy import deepcopy
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users_template() -> list:
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+
+
+def test_first_name_is_none(users_template: list) -> None:
+    new_users = deepcopy(users_template)
+    new_users[0]["first_name"] = None
+
+    assert restore_names(new_users) == users_template
+
+
+def test_parameter_first_name_missing(users_template: list) -> None:
+    new_users = deepcopy(users_template)
+    new_users[0].pop("first_name")
+
+    assert restore_names(new_users) == users_template
