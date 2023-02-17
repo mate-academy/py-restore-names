d16a9b75e5e0defba1f01cade5c3c4028fcca691
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-17 16:49:40 +0200

    v1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,114 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_names(users: list) -> None:
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_names_empty_list() -> None:
+    users = []
+    restore_names(users)
+    assert users == []
+
+
+def test_restore_names_all_have_first_name() -> None:
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_names_some_have_first_name() -> None:
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "Jane Smith",
+        }
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "full_name": "Jane Smith",
+        }
+    ]
