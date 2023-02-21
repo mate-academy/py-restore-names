f395aa84d7b0171c0ea9ee2c0e8dbe6297030982
Roman <parom95@ukr.net>
2023-02-21 17:54:56 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,47 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_when_user_first_name_is_None():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_when_user_has_not_got_key_first_name():
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users[1]["first_name"] == "Mike"
+
+
+def test_when_user_list_is_empty():
+    users = []
+    restore_names(users)
+    assert users == []
+
+
+def test_when_user_in_users_is_empty_dict():
+    users = [{}, {}]
+    assert users[0] == {}
