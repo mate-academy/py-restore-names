eb0e2a5cf24aac961c0d950f83279600b4ee8d1d
Dmytry <donchykd@gmail.com>
2023-02-21 18:38:29 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def template_fixed_users_list() -> list:
+    fixed_users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return fixed_users
+
+
+@pytest.fixture()
+def template_not_fixed_users_list() -> list:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+
+
+def test_restore_names(template_fixed_users_list: list,
+                       template_not_fixed_users_list: list) -> None:
+
+    restore_names(template_not_fixed_users_list)
+
+    assert template_fixed_users_list == template_not_fixed_users_list
