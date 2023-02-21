f9df5ed2c959bb12b91340d8d0fc2d4588040347
drapail <mv.dibrov@gmail.com>
2023-02-21 07:20:33 +0100

    added tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,27 @@
 import pytest
 from app.restore_names import restore_names
 
+
 # write your tests here
+@pytest.fixture()
+def user_template() -> list:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    yield users
+    del users
+
+
+def test_restores_missing_or_broken_names(user_template: callable) -> None:
+    users = user_template
+    restore_names(user_template)
+    for user in users:
+        assert user.get("first_name") == user.get("full_name").split()[0]
