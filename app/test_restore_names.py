8191cf5bbd94529ce1b3336d7c1a444895ca75e9
kuznetsb <kuznetsbog@gmail.com>
2023-02-22 14:25:05 +0200

    Annotations added

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,7 +3,7 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def user_template():
+def user_template() -> list:
     return [
         {
             "last_name": "Adams",
@@ -12,7 +12,7 @@ def user_template():
     ]
 
 
-def test_user_does_not_have_first_name_or_none(user_template):
+def test_user_does_not_have_first_name_or_none(user_template: list) -> None:
     restore_names(user_template)
     assert user_template == [
         {
@@ -23,7 +23,7 @@ def test_user_does_not_have_first_name_or_none(user_template):
     ]
 
 
-def test_user_first_name_is_none(user_template):
+def test_user_first_name_is_none(user_template: list) -> None:
     user_template[0]["first_name"] = None
     restore_names(user_template)
     assert user_template == [
@@ -35,7 +35,7 @@ def test_user_first_name_is_none(user_template):
     ]
 
 
-def test_func_does_not_return_anything(user_template):
+def test_func_does_not_return_anything(user_template: list) -> None:
     before_func = id(user_template[0])
     restore_names(user_template)
     after_func = id(user_template[0])
