498e78636585a4e00569fa1812e3009b714072d8
kuznetsb <kuznetsbog@gmail.com>
2023-02-22 14:23:35 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,42 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template():
+    return [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+
+
+def test_user_does_not_have_first_name_or_none(user_template):
+    restore_names(user_template)
+    assert user_template == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_user_first_name_is_none(user_template):
+    user_template[0]["first_name"] = None
+    restore_names(user_template)
+    assert user_template == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_func_does_not_return_anything(user_template):
+    before_func = id(user_template[0])
+    restore_names(user_template)
+    after_func = id(user_template[0])
+    assert before_func == after_func
