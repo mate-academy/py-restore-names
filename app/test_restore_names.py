8f4f897ca546d243989e201a8f7079c2a5a17226
Kateryna Kushnir <katerynakushnir3@gmail.com>
2023-02-23 21:44:29 +0000

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+
+@pytest.mark.parametrize(
+    "data_dict,r_dict",
+    [
+        ([{"first_name": None, "last_name": "Ho", "full_name": "Jack Ho"},
+          {"first_name": "Mike", "last_name": "Ad", "full_name": "Mike Ad"}
+          ], [
+            {"first_name": "Jack", "last_name": "Ho", "full_name": "Jack Ho"},
+            {"first_name": "Mike", "last_name": "Ad", "full_name": "Mike Ad"}
+        ]),
+        ([{"last_name": "Ho", "full_name": "Jack Ho"},
+          {"first_name": None, "last_name": "Ad", "full_name": "Mike Ad"}
+          ], [
+            {"first_name": "Jack", "last_name": "Ho", "full_name": "Jack Ho"},
+            {"first_name": "Mike", "last_name": "Ad", "full_name": "Mike Ad"}
+        ]),
+        ([], [])
+    ],
+    ids=[
+        "test_None_name",
+        "test_no_first_name",
+        "test_no_dicts"
+    ]
+)
+def test_restore_names(data_dict: list[dict],
+                       r_dict: list[dict]
+                       ) -> None:
+    restore_names(data_dict)
+    assert data_dict == r_dict, (f"should return "
+                                 f"{r_dict}, but returns"
+                                 f" {restore_names(data_dict)}")
