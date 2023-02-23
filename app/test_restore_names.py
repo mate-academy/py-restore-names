b101411feac3659eed85627b56231e74b3fbc68c
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-23 14:44:19 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,5 +1,3 @@
-import pytest
-
 from unittest import TestCase
 from app.restore_names import restore_names
 
@@ -7,18 +5,14 @@ from app.restore_names import restore_names
 class TestNames(TestCase):
     def setUp(self) -> None:
         self.users = [
-          {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-          }
+            {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}
         ]
 
-    def test_when_first_name_is_None(self):
+    def test_when_first_name_is_none(self) -> None:
         restore_names(self.users)
         assert self.users[0]["first_name"] == "Jack"
 
-    def test_when_first_name_is_deleted(self):
+    def test_when_first_name_is_deleted(self) -> None:
         del self.users[0]["first_name"]
         restore_names(self.users)
         assert self.users[0]["first_name"] == "Jack"
