b68d0438f72f5099816a1a83745dd258e85f0786
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-23 14:23:13 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,34 +3,34 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def user_template():
+def user_template() -> list:
     return [
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
 
 
 result = [
-  {
-    "first_name": "Jack",
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "first_name": "Mike",
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
+    {
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
 ]
 
 
-def test_correct_first_names(user_template):
+def test_correct_first_names(user_template: list) -> None:
     restore_names(user_template)
     assert user_template == result
