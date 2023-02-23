6853f1ce7b726c915d0044f87c43099f9535c1dc
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-23 15:29:29 +0200

    Solution with pytest

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,18 +1,21 @@
-from unittest import TestCase
+import pytest
+
 from app.restore_names import restore_names
 
 
-class TestNames(TestCase):
-    def setUp(self) -> None:
-        self.users = [
-            {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}
-        ]
+@pytest.fixture()
+def users_template() -> list:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}
+    ]
 
-    def test_when_first_name_is_none(self) -> None:
-        restore_names(self.users)
-        assert self.users[0]["first_name"] == "Jack"
 
-    def test_when_first_name_is_deleted(self) -> None:
-        del self.users[0]["first_name"]
-        restore_names(self.users)
-        assert self.users[0]["first_name"] == "Jack"
+def test_when_first_name_is_none(users_template: list) -> None:
+    restore_names(users_template)
+    assert users_template[0]["first_name"] == "Jack"
+
+
+def test_when_first_name_is_deleted(users_template: list) -> None:
+    del users_template[0]["first_name"]
+    restore_names(users_template)
+    assert users_template[0]["first_name"] == "Jack"
