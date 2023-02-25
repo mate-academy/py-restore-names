817d6f3224f897aab2f9336742e86e7d936e223e
Sashkoooo <o.denysenko@hotmail.com>
2023-02-25 18:29:48 +0200

    Solution v1.1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,7 +3,7 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def users_database():
+def users_database() -> list[dict]:
     users = [
         {
             "first_name": None,
@@ -18,7 +18,7 @@ def users_database():
     return users
 
 
-def test_restore_names(users_database):
+def test_restore_names(users_database: list[dict]) -> None:
     restore_names(users_database)
     assert users_database[0]["first_name"] == "Jack"
     assert users_database[1]["first_name"] == "Mike"
