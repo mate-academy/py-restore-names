2ca77f2e55f87a55c8d2f61d08e679db775c4c60
Dmitriy Kuzmin <kuzminds@hotmail.com>
2023-02-26 17:58:43 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,32 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_template():
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_creates_first_name_key(users_template: list) -> None:
+    restore_names(users_template)
+    assert users_template[1]["first_name"]
+
+
+def test_fills_first_name_from_full_name(users_template: list) -> None:
+    restore_names(users_template)
+    assert users_template[0]["first_name"] == "Jack"
+
+
+def test_raises_type_error(users_template) -> None:
+    with pytest.raises(TypeError):
+        restore_names(1)
