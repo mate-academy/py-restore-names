53eb1ab911f3286ce3f263ff748bed4c7899a8f6
Anna Blahitka <anettaabl@gmail.com>
2023-03-07 15:16:17 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,27 +1,53 @@
+from typing import List
+
 import pytest
 from app.restore_names import restore_names
 
 
 @pytest.fixture()
 def first_user() -> any:
-    return restore_names == {
-        "last_name": "Holy",
-        "full_name": "Jack Holy"
-    }
+    result = restore_names(
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ]
+    )
+    return result == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
 
 
 @pytest.fixture()
 def second_user() -> any:
-    return restore_names == {
-        "first_name": None,
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-    }
+    result = restore_names(
+        [
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }
+        ]
+    )
+    return result == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
 
 
-def test_should_check_first_name_in_first_user(first_user: dict) -> None:
-    assert "first_name", first_user
+def test_should_check_first_name_in_first_user(first_user: List[dict]) -> any:
+    assert "first_name" == first_user
 
 
-def test_should_check_first_name_in_second_user(second_user: dict) -> None:
+def test_should_check_first_name_in_second_user(
+        second_user: List[dict]
+) -> any:
     assert "first_name", second_user
