b266a2a547e8b84abded198c5e6afa7832d132d2
Anna Blahitka <anettaabl@gmail.com>
2023-04-10 14:37:54 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,40 +2,74 @@ import pytest
 from app.restore_names import restore_names
 
 
-@pytest.fixture()
-def first_user() -> bool:
-    result = restore_names(
-        [
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            }
-        ]
-    )
-    return result == [
+def test_restore_names():
+    # Test case with one user missing first name
+    users = [
         {
-            "first_name": "Jack",
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "John Smith",
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "John"
+
+    # Test case with one user missing full name
+    users = [
+        {
+            "first_name": "Sarah",
+            "last_name": "Jones",
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Sarah"
+
+    # Test case with one user with a first name
+    users = [
+        {
+            "first_name": "Tom",
+            "last_name": "Johnson",
+            "full_name": "Tom Johnson",
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Tom"
+
+    # Test case with multiple users
+    users = [
+        {
+            "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
-        }
-    ]
-
-
-@pytest.fixture()
-def second_user() -> bool:
-    result = restore_names(
-        [
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            }
-        ]
-    )
-    return result == [
+        },
         {
-            "first_name": "Mike",
             "last_name": "Adams",
             "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Mary",
+            "last_name": "Smith",
+            "full_name": "Mary Smith",
+        },
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+    assert users[2]["first_name"] == "Mary"
+
+    # Test case with all users already having first name
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "full_name": "Jane Doe",
         }
     ]
+    restore_names(users)
+    assert users[0]["first_name"] == "John"
+    assert users[1]["first_name"] == "Jane"
