2ad41e93a9a13af53b731c7010c5462ee0ce7eed
Bohdan Marko <goldenbohdanm@gmail.com>
2023-04-13 14:25:27 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,67 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users_input, users_result",
+    [
+        pytest.param(
+            [
+              {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+              }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            id="test when first_name is None"
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            id="test when key 'first_name' doesn't exist"
+        ),
+        pytest.param(
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            id="test when first_name is filled"
+        )
+    ]
+)
+def test_restore_names(
+        users_input: list[dict],
+        users_result: list[dict]
+) -> None:
+    restore_names(users_input)
+    assert (
+        users_input == users_result
+    )
