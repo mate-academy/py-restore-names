140731688f2a28516ad295b06044a44c913b754c
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-04-14 19:49:03 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_set_correct_first_name() -> None:
+    users_result = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == users_result
