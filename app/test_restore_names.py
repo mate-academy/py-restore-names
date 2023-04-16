b2292b892cc1c7d42fd72532b355b46015988e97
Mykola <nickkozlov90@gmail.com>
2023-04-16 18:01:25 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
 from app.restore_names import restore_names
+from typing import Callable
 
-# write your tests here
+
+@pytest.fixture()
+def incorrect_users_list() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    return users
+
+
+def test_restore_names_adds_first_name(
+        incorrect_users_list: Callable
+) -> None:
+    users = incorrect_users_list
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
