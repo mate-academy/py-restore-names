3e5f1e315dfe6cd2ada5c73a60835cce527cc09f
Dmytry Bondariev <bondarievmd@gmail.com>
2023-04-16 22:52:31 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,9 +1,10 @@
 import pytest
 from app.restore_names import restore_names
+from typing import Callable
 
 
 @pytest.fixture()
-def restored_names():
+def restored_names() -> list:
     users = [
         {
             "last_name": "Holy",
@@ -24,13 +25,13 @@ def restored_names():
     return users
 
 
-def test_missing_user_key(restored_names):
+def test_missing_user_key(restored_names: Callable) -> None:
     assert restored_names[0]["first_name"] == "Jack"
 
 
-def test_if_user_key_is_none(restored_names):
+def test_if_user_key_is_none(restored_names: Callable) -> None:
     assert restored_names[1]["first_name"] == "Mike"
 
 
-def test_if_user_name_is_provided(restored_names):
+def test_if_user_name_is_provided(restored_names: Callable) -> None:
     assert restored_names[2]["first_name"] == "Dmytro"
