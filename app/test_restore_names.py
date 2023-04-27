b1f1d6590ccdf4c4e6fc09e9b880ed66a8a42a67
Roman Kotok <kotokroman@gmail.com>
2023-04-27 14:24:21 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,13 +3,15 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def template_user():
-    return [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-            {"last_name": "Adams", "full_name": "Mike Adams"}]
+def template_user() -> list[dict]:
+    return [{"first_name": None,
+             "last_name": "Holy",
+             "full_name": "Jack Holy"},
+            {"last_name": "Adams",
+             "full_name": "Mike Adams"}]
 
 
-def test_restore_names(template_user) -> None:
+def test_restore_names(template_user: list) -> None:
     restore_names(template_user)
     assert template_user[0]["first_name"] == "Jack"
     assert template_user[1]["first_name"] == "Mike"
-
