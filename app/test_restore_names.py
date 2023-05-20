9389770d4dbf99204a85d56578f1def5e67cc545
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-20 12:50:34 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,23 +3,22 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def user_name() -> None:
-    users = [
-  {
-    "first_name": "Jack",
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "first_name": "Mike",
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+def user_name():
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }]
 
 
-def test_should_set_correct_name(user_name):
-    assert user_name == restore_names([
+def test_should_set_correct_name(user_name) -> None:
+    names_to_restore = [
         {
             "first_name": None,
             "last_name": "Holy",
@@ -29,4 +28,6 @@ def test_should_set_correct_name(user_name):
             "last_name": "Adams",
             "full_name": "Mike Adams",
         },
-    ])
+    ]
+    restore_names(names_to_restore)
+    assert user_name == names_to_restore
