e6eaa34ee1eaa668ecfafc56e25ee32be0d3c1d9
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-20 12:18:57 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,32 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_name() -> None:
+    users = [
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+
+
+def test_should_set_correct_name(user_name):
+    assert user_name == restore_names([
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ])
