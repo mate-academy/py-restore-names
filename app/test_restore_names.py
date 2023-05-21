33b78f711cddc3df491e70fec1a830468c4ffab1
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-21 15:06:45 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_dictionary():
+    yield [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+    ]
+
+
+@pytest.fixture()
+def another_user_dictionary():
+    yield [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_names_function(user_dictionary, another_user_dictionary):
+    restore_names(user_dictionary)
+    restore_names(another_user_dictionary)
+    assert user_dictionary[0]["first_name"] == "Jack"
+    assert another_user_dictionary[0]["first_name"] == "Mike"
