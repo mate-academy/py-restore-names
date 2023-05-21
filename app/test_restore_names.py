c3bfc7c4b4f30fd8887c6817fdc2e5e01f3b52d7
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-21 23:07:10 +0300

    refactoring

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,6 +1,7 @@
 import pytest
 from app.restore_names import restore_names
 
+
 @pytest.fixture()
 def user_template() -> list:
     return [
@@ -12,35 +13,35 @@ def user_template() -> list:
     ]
 
 
-def test_first_name_key_doesnt_exist(user_template) -> None:
+def test_first_name_key_doesnt_exist(user_template: list) -> None:
     user = [
         {
             "last_name": "Holy",
-            "full_name": "Jack Holy",  
+            "full_name": "Jack Holy",
         }
     ]
     restore_names(user)
     assert user == user_template
 
 
-def test_first_name_value_none(user_template) -> None:
+def test_first_name_value_none(user_template: list) -> None:
     user = [
         {
             "first_name": None,
             "last_name": "Holy",
-            "full_name": "Jack Holy",  
+            "full_name": "Jack Holy",
         }
     ]
     restore_names(user)
     assert user == user_template
 
 
-def test_first_name_exists(user_template) -> None:
+def test_first_name_exists(user_template: list) -> None:
     user = [
         {
             "first_name": "Jack",
             "last_name": "Holy",
-            "full_name": "Jack Holy",  
+            "full_name": "Jack Holy",
         }
     ]
     restore_names(user)
