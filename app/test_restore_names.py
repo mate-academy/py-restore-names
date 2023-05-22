2603278a82a9d6531cefbead44b579565e7c67b6
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-22 12:48:35 +0300

    Py restore names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,52 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_return_name() -> None:
+    yield [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_none_missing_names(user_return_name: list) -> None:
+    users = user_return_name
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_only_missing_names(user_return_name: list) -> None:
+    users = user_return_name
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
