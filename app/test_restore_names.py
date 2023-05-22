292cfd5d49e9203c6f91ad8c19014a41fde4a899
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-22 13:13:27 +0300

    Fix Py restore names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -21,12 +21,11 @@ def user_return_name() -> list:
 def test_should_return_dict_with_first_name(user_return_name: list) -> None:
     users = [
         {
-            "first_name": "Jack",
+            "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
         },
         {
-            "first_name": "Mike",
             "last_name": "Adams",
             "full_name": "Mike Adams",
         },
