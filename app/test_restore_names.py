e3fa345f78c192a0a66d23e4c5d1513630620ea5
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-05-23 18:38:32 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,46 @@
 import pytest
+from typing import Any
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def incorrect_list() -> list:
+    to_test = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return to_test
+
+
+def test_restore_names(incorrect_list: Any) -> None:
+    users = incorrect_list
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
