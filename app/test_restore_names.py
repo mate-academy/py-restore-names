0d1ec77178f9c0065ddf2a03e2dce44fbb5baaee
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-27 00:03:09 +0300

    lets try

---
+++ app/test_restore_names.py
@@ -0,0 +1,21 @@
+from app.restore_names import restore_names
+
+import pytest
+
+
+def test_check_if_first_name_valid() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    for user in users:
+        assert user["first_name"] == user["full_name"].split()[0]
+        assert user["first_name"] is not None
