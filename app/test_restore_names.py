1dbbea6e68059f00a4e8ab60fad21e2665280116
Nikita <melniknikita2002@icloud.com>
2023-06-12 10:06:53 +0300

    task

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames:
+    @pytest.fixture(scope="module")
+    def users_template(self) -> list:
+        return [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    def test_should_change_first_name_from_none_to_first_name(
+            self,
+            users_template: list[dict]
+    ) -> None:
+        restore_names(users_template)
+        assert (
+            users_template[0] == {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        )
+
+    def test_should_add_first_name_key_with_name(
+            self,
+            users_template: list[dict]
+    ) -> None:
+        assert (
+            users_template[1] == {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }
+        )
