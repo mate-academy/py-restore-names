29d83b49d4f7be50bf792f38d7333e41731d8353
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-15 21:40:47 +0300

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,29 +5,29 @@ from app.restore_names import restore_names
 @pytest.fixture()
 def users() -> list[dict]:
     return [
-              {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-              },
-              {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-              },
-            ]
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
 
 
-def test_restore_names_check_output(users) -> None:
+def test_restore_names_check_output(users: list[dict]) -> None:
     restore_names(users)
     assert users == [
-                      {
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy",
-                      },
-                      {
-                        "first_name": "Mike",
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams",
-                      },
-                    ]
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
