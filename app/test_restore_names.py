85bbcaf2fccc054b20abda801dc138e10b47ac5d
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-15 16:29:09 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,31 @@
 import pytest
 from app.restore_names import restore_names
+from typing import List
 
-# write your tests here
+
+@pytest.fixture()
+def expected_user_output() -> List[dict]:
+    return [
+        {
+            "first_name": "Andriy",
+            "last_name": "Sydorenko",
+            "full_name": "Andriy Sydorenko"
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    "users",
+    [
+        [{"last_name": "Sydorenko",
+          "full_name": "Andriy Sydorenko"}],
+
+        [{"first_name": None,
+          "last_name": "Sydorenko",
+          "full_name": "Andriy Sydorenko"}],
+    ]
+)
+def test_restores_names_correctly(users: List[dict],
+                                  expected_user_output: List[dict]) -> None:
+    restore_names(users)
+    assert users == expected_user_output
