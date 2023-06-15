827bfc13b2193d0562e7bed715f2ce8b723b6686
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-06-15 17:46:36 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,24 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> list[dict]:
+    return [{
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy", }]
+
+
+@pytest.mark.parametrize("users", [
+    ([{
+      "first_name": None,
+      "last_name": "Holy",
+      "full_name": "Jack Holy", }]),
+    ([{
+      "last_name": "Holy",
+      "full_name": "Jack Holy", }])
+])
+def test_restore_names(user_template: list[dict], users: list[dict]) -> None:
+    restore_names(users)
+    assert users == user_template
