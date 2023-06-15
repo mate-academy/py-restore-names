2143462d03165d0068f5b629ee1b3b6b863d5848
Eugene Polunin <altahoma@gmail.com>
2023-06-15 14:29:27 +0300

    Implemented tests for restore_names()

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,39 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_template() -> list:
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_none_names(users_template: list) -> None:
+    for user in users_template:
+        user["first_name"] = None
+
+    restore_names(users_template)
+
+    for user in users_template:
+        assert user["first_name"] == user["full_name"].split()[0]
+
+
+def test_restore_missing_names(users_template: list) -> None:
+    for user in users_template:
+        del user["first_name"]
+
+    restore_names(users_template)
+
+    for user in users_template:
+        assert user["first_name"] == user["full_name"].split()[0]
