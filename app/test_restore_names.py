2ed3ba5d7ceef46664905d1efcef2438edf0ee02
maccarets <maccarets@gmail.com>
2023-06-19 22:06:36 +0300

    fixed flake8

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,39 +6,39 @@ from app.restore_names import restore_names
     "users, result",
     [
         (
-                [
-                    {
-                        "first_name": None,
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy",
-                    }
-                ],
-                [
-                    {
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy",
-                    }
-                ]
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ]
         ),
         (
-                [
-                    {
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams",
-                    }
-                ],
-                [
-                    {
-                        "first_name": "Mike",
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams",
-                    }
-                ]
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ]
         ),
 
     ]
 )
-def test(users, result):
+def test(users: list[dict], result: list[dict]) -> None:
     restore_names(users)
     assert users == result
