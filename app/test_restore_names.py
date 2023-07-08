e15b3c84a4989586faf9f02597da17709b2d400a
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-08 19:05:28 +0300

    somethings

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,42 @@
 import pytest
 from app.restore_names import restore_names
+from unittest import mock
 
-# write your tests here
+
+@pytest.fixture
+def test_func_data() -> None:
+    yield [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }]
+
+@pytest.fixture
+def test_func_result() -> None:
+    yield [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+def test_func_is_was_called(test_func_data) -> None:
+    with mock.patch("app.restore_names") as rest_name:
+        rest_name(test_func_data)
+        rest_name.assert_called_once_with(test_func_data)
+
+
+def test_function_changes_state(test_func_data, test_func_result):
+    v = restore_names(test_func_data)
+    v.assertEqual(test_func_data.state, test_func_result)
