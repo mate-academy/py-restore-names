18179baca9638bbbd8bb21b35f3473e5ef02af0b
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-07-27 21:55:08 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,47 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_w_missing_1st_name() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+@pytest.fixture()
+def test_and_add_first_name(user_w_missing_1st_name):
+    return [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+
+    ]
+
+
+def test_restore_names_with_missing_first_name(user_w_missing_1st_name):
+    restore_names(user_w_missing_1st_name)
+    assert user_w_missing_1st_name[0]["first_name"] == "Jack"
+    assert user_w_missing_1st_name[1]["first_name"] == "Mike"
+
+
+# Test case for users with existing first_name
+def test_restore_names_with_existing_first_name(test_and_add_first_name):
+    restore_names(test_and_add_first_name)
+    assert test_and_add_first_name[0]["first_name"] == "Jack"
+    assert test_and_add_first_name[1]["first_name"] == "Mike"
