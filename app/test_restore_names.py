5ce8ef868333770ecbcb86d5b14d69401ef33ade
Andrew Sulym <a.sulymm@gmail.com>
2023-07-31 23:07:06 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_dict_if_there_is_no_such_key_in_it() -> None:
+    users = [{
+        "last_name": "Adams",
+        "full_name": "Mike Adams"
+    }]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+
+
+def test_if_key_is_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        }
+    ]
