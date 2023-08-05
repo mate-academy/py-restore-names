7c52338ff8781bfa9a2e400b02e963508a926bfc
Illia Khomutov <ilyakhomutov613@gmail.com>
2023-08-05 22:34:21 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
 import pytest
 from app.restore_names import restore_names
+from typing import List
 
-# write your tests here
+
+@pytest.fixture()
+def users() -> list:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+
+
+def test_restore_names(users: List[dict]) -> None:
+    restore_names(users)
+    for user in users:
+        assert "first_name" in user and user["first_name"] is not None
