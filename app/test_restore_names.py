d160729ac895e9ba465310c093234b99f2748c3b
Taras Borshch <tarasborshch4@gmail.com>
2023-08-24 17:44:28 +0300

    Completed py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,63 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_if_name_is_none() -> None:
+    user = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    restore_names(user)
+    assert user == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        }
+    ]
+
+
+def test_restore_if_name_is_null() -> None:
+    user = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    restore_names(user)
+    assert user == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        }
+    ]
+
+
+def test_restore_if_couple_users_in_list() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
