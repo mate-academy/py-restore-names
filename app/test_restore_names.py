847d241c282ff9807b17da4585f7d5f6a27106b7
oreny <orenycdota@gmail.com>
2023-08-28 15:39:26 +0300

    Tests for database

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_list, result_list",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+        ),
+    ],
+)
+def test_restore_names(input_list: list, result_list: list) -> None:
+    restore_names(input_list)
+    print("here")
+    assert input_list == result_list
