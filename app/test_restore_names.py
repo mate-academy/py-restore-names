1eff199c6b3e72b912c4291418e1627bb47a88b8
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-28 14:53:07 +0300

    py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,53 +5,57 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users,result",
     [
-        ([
-             {
-                 "first_name": None,
-                 "last_name": "Holy",
-                 "full_name": "Jack Holy",
-             },
-             {
-                 "last_name": "Adams",
-                 "full_name": "Mike Adams",
-             },
-         ],
-         [
-             {
-                 "first_name": "Jack",
-                 "last_name": "Holy",
-                 "full_name": "Jack Holy",
-             },
-             {
-                 "first_name": "Mike",
-                 "last_name": "Adams",
-                 "full_name": "Mike Adams",
-             },
-         ]),
-        ([
-             {
-                 "first_name": None,
-                 "last_name": "Hatfield",
-                 "full_name": "James Hatfield",
-             },
-             {
-                 "first_name": None,
-                 "last_name": "Ulrich",
-                 "full_name": "Lars Ulrich",
-             },
-         ],
-         [
-             {
-                 "first_name": "James",
-                 "last_name": "Hatfield",
-                 "full_name": "James Hatfield",
-             },
-             {
-                 "first_name": "Lars",
-                 "last_name": "Ulrich",
-                 "full_name": "Lars Ulrich",
-             }
-         ])
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+        ),
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Hatfield",
+                    "full_name": "James Hatfield",
+                },
+                {
+                    "first_name": None,
+                    "last_name": "Ulrich",
+                    "full_name": "Lars Ulrich",
+                },
+            ],
+            [
+                {
+                    "first_name": "James",
+                    "last_name": "Hatfield",
+                    "full_name": "James Hatfield",
+                },
+                {
+                    "first_name": "Lars",
+                    "last_name": "Ulrich",
+                    "full_name": "Lars Ulrich",
+                }
+            ]
+        )
     ]
 )
 def test_restore_names(users: list, result: list) -> None:
