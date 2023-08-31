a6c2090bc2c9fc89d91792c573a709e05fdfce99
Ostap <ostapbojko04@gmail.com>
2023-08-31 15:39:26 +0300

    py-restore1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,14 +1,10 @@
-import pytest
-from app.restore_names import restore_names
-
-
-def get_first_name_from_full_name(full_name):
+def get_first_name_from_full_name(full_name: str) -> None:
     parts = full_name.split()
     first_name = parts[0] if parts else ""
     return first_name
 
 
-def test_get_first_name_from_full_name():
+def test_get_first_name_from_full_name() -> str:
     assert get_first_name_from_full_name("Ostap Boiko") == "Ostap"
     assert get_first_name_from_full_name("Ivan Bilui") == "Ivan"
     assert get_first_name_from_full_name("Ivan Boiko") == "Ivan"
