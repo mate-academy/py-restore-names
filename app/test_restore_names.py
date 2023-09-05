c50024c3ce344ea5b6bca4a92475ca74c38a730c
Mikhail <tiunovmixs@gmail.com>
2023-09-05 12:58:37 +0300

    Implemented Restore names 2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,23 +1,42 @@
+from typing import List
 import pytest
 from app.restore_names import restore_names
 
 
-class TestRestoreNames:
-    @pytest.mark.parametrize(
-        "user, expected_output", [
-            ({"full_name": "John Doe", "first_name": None},
-             {"full_name": "John Doe", "first_name": "John"}),
-            ({"full_name": "Alice Smith", "last_name": "Smith"},
-             {"full_name": "Alice Smith", "last_name": "Smith",
-              "first_name": "Alice"}),
-            ({"full_name": "Bob Johnson"},
-             {"full_name": "Bob Johnson", "first_name": "Bob"}),
-        ])
-    def test_restore_names(
-            self,
-            user: dict,
-            expected_output: dict,
-    ) -> None:
-        user = [user]
-        restore_names(user)
-        assert user == [expected_output]
+@pytest.mark.parametrize(
+    "user, expected_output",
+    [
+        pytest.param(
+            [{
+                "first_name": None,
+                "last_name": "Doe",
+                "full_name": "John Doe",
+            }],
+            [{
+                "first_name": "John",
+                "last_name": "Doe",
+                "full_name": "John Doe",
+            }],
+            id="Test restore 'name' when first_name is None"
+        ),
+        pytest.param(
+            [{
+                "last_name": "Smith",
+                "full_name": "Alice Smith",
+            }],
+            [{
+                "first_name": "Alice",
+                "last_name": "Smith",
+                "full_name": "Alice Smith",
+            }],
+            id="Restore first_name when is no"
+        )
+    ]
+)
+def test_restore_names(
+        user: List[dict],
+        expected_output: List[dict],
+) -> None:
+    user_restore = [user]
+    restore_names(user)
+    assert user_restore == [expected_output]
