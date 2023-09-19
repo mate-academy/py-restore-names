e6ba26855e659431f415f07f641ed2315e898e8a
Olgierrd <oolgierrd@gmail.com>
2023-09-19 17:11:01 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
-import pytest
+from typing import Callable
 from app.restore_names import restore_names
+import pytest
 
-# write your tests here
+
+@pytest.fixture
+def users_template() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    yield users
+
+
+def test_restore_names(users_template: Callable) -> None:
+    restore_names(users_template)
+    assert users_template[0]["first_name"] == "Jack"
+    assert users_template[1]["first_name"] == "Mike"
