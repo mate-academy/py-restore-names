e693eef4f1022351d22c68b1606ab9d3048ec551
Maksim Stahovskij <maksss196@gmail.com>
2023-09-22 20:46:17 +0300

    py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,38 +1,25 @@
 import pytest
 from app.restore_names import restore_names
+from typing import List
 
 
 @pytest.mark.parametrize("users, expected", [
-    ([{
-        "first_name": "John",
-        "last_name": "Konor",
-        "full_name": "John Konor"
-    }],
-     [{
-         "first_name": "John",
-         "last_name": "Konor",
-         "full_name": "John Konor"
-     }]),
-    ([{
-        "first_name": None,
-        "last_name": "Konor",
-        "full_name": "John Konor"
-    }],
-     [{
-         "first_name": "John",
-         "last_name": "Konor",
-         "full_name": "John Konor"
-     }]),
-    ([{
-        "last_name": "Konor",
-        "full_name": "John Konor"
-    }],
-     [{
-         "first_name": "John",
-         "last_name": "Konor",
-         "full_name": "John Konor"
-     }])
+    ([
+        {"first_name": "John", "last_name": "Konor", "full_name": "John Konor"}
+    ], [
+        {"first_name": "John", "last_name": "Konor", "full_name": "John Konor"}
+    ]),
+    ([
+        {"first_name": None, "last_name": "Konor", "full_name": "John Konor"}
+    ], [
+        {"first_name": "John", "last_name": "Konor", "full_name": "John Konor"}
+    ]),
+    ([
+        {"last_name": "Konor", "full_name": "John Konor"}
+    ], [
+        {"first_name": "John", "last_name": "Konor", "full_name": "John Konor"}
+    ])
 ])
-def test_restore_name(users: str, expected: str) -> None:
+def test_restore_name(users: List[dict], expected: list[dict]) -> None:
     restore_names(users)
     assert users == expected
