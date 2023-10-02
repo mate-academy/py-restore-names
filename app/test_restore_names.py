a3d245bf8d881da96adaa1521c9e3d6644bbb8bb
Даня Пономаренко <you@example.com>
2023-10-02 14:07:44 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -16,21 +16,24 @@ from app.restore_names import restore_names
         ),
 
         (
-            [{
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Adam Holy",
-            }],
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Adam Holy",
+                }
+            ],
             "Adam",
             1
         ),
     ]
 )
-def test_s(ls, first_name, num_of_ls):
+def test_s(ls: list[dict], first_name: str, num_of_ls: int) -> None:
     restore_names(ls)
     assert first_name in ls[num_of_ls].get("first_name")
