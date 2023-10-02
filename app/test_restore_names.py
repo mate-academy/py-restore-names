f4494e9e4a1635a4a0bd9f513adac4cc05cc6222
Даня Пономаренко <you@example.com>
2023-10-02 13:59:39 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,36 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "ls,first_name,num_of_ls",
+    [
+        (
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            "Jack",
+            0
+        ),
+
+        (
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Adam Holy",
+            }],
+            "Adam",
+            1
+        ),
+    ]
+)
+def test_s(ls, first_name, num_of_ls):
+    restore_names(ls)
+    assert first_name in ls[num_of_ls].get("first_name")
