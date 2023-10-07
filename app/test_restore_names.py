ce0eecab91b79c01be47fa16b79bfa2ab5a72398
asdadaversa <peikqudakeus@gmail.com>
2023-10-07 14:21:28 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -9,7 +9,29 @@ def users() -> bool:
     ]
 
 
-def test_restore_names(users: list) -> None:
+
+def test_restore_names(users) -> None:
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
             assert user["first_name"] == user["full_name"].split()[0]
+    #
+    #
+    #
+    # @pytest.mark.parametrize(
+    #     "cat_age, dog_age",
+    #     [
+    #         ("k", 12),
+    #         (12, "k"),
+    #         ("k", "k"),
+    #         ({"k"}, {"k"}),
+    #         (["k"], ["k"])
+    #     ]
+    # )
+    # def test_correct_types_value_get_human_age(
+    #         self,
+    #         cat_age: int,
+    #         dog_age: int
+    # ) -> None:
+    #
+    #     with pytest.raises(TypeError):
+    #         get_human_age(cat_age, dog_age)
