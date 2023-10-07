9b35e38ecc7683dddc858ff8a41c8e6e29b27fc4
Nite NSD <nsd7night@gmail.com>
2023-10-07 17:45:29 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,16 +5,16 @@ from typing import List, Dict
 
 @pytest.mark.parametrize("users, expected_names", [
     (
-            [{"full_name": "John Doe", "first_name": None},
-             {"full_name": "Alice Smith"},
-             {"full_name": "Bob Johnson", "first_name": "Bob"}],
-            ["John", "Alice", "Bob"]
+        [{"full_name": "John Doe", "first_name": None},
+         {"full_name": "Alice Smith"},
+         {"full_name": "Bob Johnson", "first_name": "Bob"}],
+        ["John", "Alice", "Bob"]
     ),
     (
-            [{"full_name": "Jane Smith", "first_name": "Jane"},
-             {"full_name": "Michael Brown", "first_name": None},
-             {"full_name": "Emily Davis", "first_name": "Emily"}],
-            ["Jane", "Michael", "Emily"]
+        [{"full_name": "Jane Smith", "first_name": "Jane"},
+         {"full_name": "Michael Brown", "first_name": None},
+         {"full_name": "Emily Davis", "first_name": "Emily"}],
+        ["Jane", "Michael", "Emily"]
     ),
 ])
 def test_restore_names(users: List[Dict[str, str]], expected_names: List[str]):
