f349a6d3360510076f3a599221eb3b51b4b79069
Максим <www.depaton@gmail.com>
2023-10-15 00:26:07 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,60 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("input_users, expected_users", [
+    (
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+        [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+    ),
+    (
+        [
+            {
+                "first_name": "Alice",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Bob",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+        [
+            {
+                "first_name": "Alice",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Bob",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+    ),
+])
+def test_restore_names_with_parameterization(input_users, expected_users):
+    restore_names(input_users)
+    assert input_users == expected_users
