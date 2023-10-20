4c09bae841d4f877895d042ca5059db81cb369cd
Serhii <martinenko.sergiy@gmail.com>
2023-10-20 17:46:39 +0300

    fix

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -8,6 +8,8 @@ def test_user_with_no_name() -> None:
 
 
 def test_user_with_name_none() -> None:
-    user = [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}]
+    user = [{"first_name": None,
+             "last_name": "Holy",
+             "full_name": "Jack Holy"}]
     restore_names(user)
     assert user[0].get("first_name") == "Jack"
