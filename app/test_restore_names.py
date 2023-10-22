ecac4f3cbbe25162db52f7d125c06d8804619d90
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-10-22 16:19:06 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,69 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames:
+    @pytest.fixture()
+    def users_with_firstname_as_none(self) -> list:
+        return [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": None,
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    @pytest.fixture()
+    def users_with_no_firstname(self) -> list:
+        return [
+            {
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    def test_users_with_firstname_as_none(
+            self,
+            users_with_firstname_as_none: list[dict]
+    ) -> None:
+        restore_names(users_with_firstname_as_none)
+        assert users_with_firstname_as_none == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    def test_users_with_no_firstname(
+            self,
+            users_with_no_firstname: list[dict]
+    ) -> None:
+        restore_names(users_with_no_firstname)
+        assert users_with_no_firstname == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
