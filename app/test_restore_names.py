fa111ab8f071a50674ef873a0d3e4cef2381e254
Your_Name Your_Surname <your@email>
2023-10-31 11:44:36 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_dict,output_dict",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="should return correctly result list"
+        )
+    ]
+)
+def test_should_return_correctly_result_list(
+    input_dict: list,
+    output_dict: list
+) -> None:
+    restore_names(input_dict)
+    assert input_dict == output_dict
