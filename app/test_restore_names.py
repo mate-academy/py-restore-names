9e665fb1d44c89f79027b7db12822829a2b6a7ee
Artem Manko <Kruto.teny@gmail.com>
2023-11-14 17:14:55 +0100

    flake8 fix

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,12 +5,18 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users, expected",
     [
-        ({"first_name": None, "last_name": "Doe", "full_name": "John Doe"},
-         {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"}),
+        ({"first_name": None,
+          "last_name": "Doe",
+          "full_name": "John Doe"},
+         {"first_name": "John",
+          "last_name": "Doe",
+          "full_name": "John Doe"}),
         ({"last_name": "Manko", "full_name": "Artem Manko"},
-         {"first_name": "Artem", "last_name": "Manko", "full_name": "Artem Manko"}),
+         {"first_name": "Artem",
+          "last_name": "Manko",
+          "full_name": "Artem Manko"}),
     ]
 )
-def test_restore_names(users, expected):
+def test_restore_names(users: dict, expected: dict) -> None:
     restore_names([users])
     assert users == expected
