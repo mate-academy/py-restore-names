ab38886ba91b3c35bf3b7df8675c941cec75f54b
Alex <oleksa.shcherbakov@gmail.com>
2023-11-18 10:34:55 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "initial_users, complite_isers",
+    [
+        (
+                [
+                    {
+                        "first_name": None,
+                     "last_name": "Holy",
+                     "full_name": "Jack Holy"
+                     },
+                    {
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    },
+],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    },
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    }
+                ]
+        )
+    ]
+)
+
+def test_restore_names(
+        initial_users: list,
+        complite_isers: list
+) -> None:
+    assert restore_names(initial_users) == complite_isers
+
