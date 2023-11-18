880b229ed766dba8655efa622bdad2b7f031ab23
Alex <oleksa.shcherbakov@gmail.com>
2023-11-18 10:37:37 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,36 +6,30 @@ from app.restore_names import restore_names
     "initial_users, complite_isers",
     [
         (
-                [
-                    {
-                        "first_name": None,
-                     "last_name": "Holy",
-                     "full_name": "Jack Holy"
-                     },
-                    {
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    },
-],
-                [
-                    {
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    },
-                    {
-                        "first_name": "Mike",
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    }
-                ]
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }, {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+        ),
+        (
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }, {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
         )
     ]
 )
-
 def test_restore_names(
         initial_users: list,
         complite_isers: list
 ) -> None:
     assert restore_names(initial_users) == complite_isers
-
