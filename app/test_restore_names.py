d05cd25194e19e86a28bb0d3be57c76f622225f1
unknown <temakot254@gmail.com>
2023-11-25 19:21:39 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
-import pytest
+import unittest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames(unittest.TestCase):
+    def test_restore_names(self) -> None:
+        users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+        restore_names(users)
+        self.assertEqual(users, [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ])
+
+
+if __name__ == "__main__":
+    unittest.main()
