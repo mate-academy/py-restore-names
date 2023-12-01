44dcc1357bece89b94142023e729cf0f23a3e3a6
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-12-01 17:07:23 +0200

    Fixed over-indented.

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -9,40 +9,40 @@ class TestRestoreNames:
         "input_data,expected_output",
         [
             (
-                    [{
-                        "first_name": None,
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    }],
-                    [{
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    }]
+                [{
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }],
+                [{
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }]
             ),
             (
-                    [{
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    }],
-                    [{
-                        "first_name": "Mike",
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    }]
+                [{
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                }],
+                [{
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                }]
 
             ),
             (
-                    [{
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    }],
-                    [{
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    }]
+                [{
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }],
+                [{
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }]
             )
         ],
         ids=[
