795e26b2b81d0a788faee34b674371a7a2a6a1cf
Vlad <mr.darmstadtium@gmail.com>
2023-12-06 18:41:58 +0100

    some flake again

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,42 +7,42 @@ from app.restore_names import restore_names
     [
         (
             [
-                    {"first_name": None,
-                     "last_name": "Holy",
-                     "full_name": "Jack Holy"}
+                {"first_name": None,
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"}
             ],
             [
-                    {"first_name": "Jack",
-                     "last_name": "Holy",
-                     "full_name": "Jack Holy"}
+                {"first_name": "Jack",
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"}
             ]
         ),
         (
             [
-                    {"last_name": "Heavy",
-                     "full_name": "Jack Heavy"}
+                {"last_name": "Heavy",
+                 "full_name": "Jack Heavy"}
             ],
             [
-                    {"first_name": "Jack",
-                     "last_name": "Heavy",
-                     "full_name": "Jack Heavy"}
+                {"first_name": "Jack",
+                 "last_name": "Heavy",
+                 "full_name": "Jack Heavy"}
             ]
         ),
         (
             [
-                    {"first_name": None,
-                     "last_name": "Firstovich",
-                     "full_name": "First Firstovich"},
-                    {"last_name": "Secondowich",
-                     "full_name": "Second Secondowich"}
+                {"first_name": None,
+                 "last_name": "Firstovich",
+                 "full_name": "First Firstovich"},
+                {"last_name": "Secondowich",
+                 "full_name": "Second Secondowich"}
             ],
             [
-                    {"first_name": "First",
-                     "last_name": "Firstovich",
-                     "full_name": "First Firstovich"},
-                    {"first_name": "Second",
-                     "last_name": "Secondowich",
-                     "full_name": "Second Secondowich"}
+                {"first_name": "First",
+                 "last_name": "Firstovich",
+                 "full_name": "First Firstovich"},
+                {"first_name": "Second",
+                 "last_name": "Secondowich",
+                 "full_name": "Second Secondowich"}
             ]
         )
     ]
