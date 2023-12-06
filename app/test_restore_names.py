ef260ed86ef08a16765c6393ced17483930a9664
Vlad <mr.darmstadtium@gmail.com>
2023-12-06 18:33:01 +0100

    wrote all code

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,55 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, result",
+    [
+        (
+                [
+                    {"first_name": None,
+                     "last_name": "Holy",
+                     "full_name": "Jack Holy"}
+                ],
+                [
+                    {"first_name": "Jack",
+                     "last_name": "Holy",
+                     "full_name": "Jack Holy"}
+                ]
+        ),
+        (
+                [
+                    {"last_name": "Heavy",
+                     "full_name": "Jack Heavy"}
+                ],
+                [
+                    {"first_name": "Jack",
+                     "last_name": "Heavy",
+                     "full_name": "Jack Heavy"}
+                ]
+        ),
+        (
+                [
+                    {"first_name": None,
+                     "last_name": "Firstovich",
+                     "full_name": "First Firstovich"},
+                    {"last_name": "Secondowich",
+                     "full_name": "Second Secondowich"}
+                ],
+                [
+                    {"first_name": "First",
+                     "last_name": "Firstovich",
+                     "full_name": "First Firstovich"},
+                    {"first_name": "Second",
+                     "last_name": "Secondowich",
+                     "full_name": "Second Secondowich"}
+                ]
+        )
+    ]
+)
+def test_restore_names(users: list[dict],
+                       result: list[dict]) -> None:
+    assert (
+            restore_names(users) == result
+    ), (f"Function restore_names should change {users} "
+        f"into {result}, but it isn't")
