e1263ffcc19147fb9f779b26dbf864c535d2c6cd
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-09 13:35:32 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,19 +1,15 @@
-import pytest
-
-import app.restore_names
 from app.restore_names import restore_names
 
-
 users = [
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
+    {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
 ]
 
 
@@ -22,12 +18,12 @@ def test_restore_names() -> None:
 
     assert users == [
         {
-          "first_name": "Jack",
-          "last_name": "Holy",
-          "full_name": "Jack Holy"
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
         }, {
-          "first_name": "Mike",
-          "last_name": "Adams",
-          "full_name": "Mike Adams"
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
         }
-      ]
+    ]
