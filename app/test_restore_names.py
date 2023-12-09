4ccbd0164873ccc5064d1f7f277451bcf846e88e
Nataliia Povashevych <interchanal@ukr.net>
2023-12-09 12:07:10 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "wrong_users, rule_users",
+    [
+        (
+            [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
+        ),
+        (
+            [{"last_name": "Holy", "full_name": "Jack Holy"}],
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
+        ),
+    ],
+    ids=[
+        "first_name is None",
+        "first_name is not",
+    ]
+)
+def test_restore_names(wrong_users: list[dict], rule_users: list[dict]) -> None:
+    restore_names(wrong_users)
+    assert wrong_users == rule_users
