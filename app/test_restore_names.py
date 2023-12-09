3ffe1738c0c1bd2f8f89f3a1a965d92e3069744a
Nataliia Povashevych <interchanal@ukr.net>
2023-12-09 12:08:58 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,12 +7,35 @@ from app.restore_names import restore_names
     "wrong_users, rule_users",
     [
         (
-            [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
-            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
         ),
         (
-            [{"last_name": "Holy", "full_name": "Jack Holy"}],
-            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
+            [
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
         ),
     ],
     ids=[
@@ -20,6 +43,8 @@ from app.restore_names import restore_names
         "first_name is not",
     ]
 )
-def test_restore_names(wrong_users: list[dict], rule_users: list[dict]) -> None:
+def test_restore_names(
+        wrong_users: list[dict],
+        rule_users: list[dict]) -> None:
     restore_names(wrong_users)
     assert wrong_users == rule_users
