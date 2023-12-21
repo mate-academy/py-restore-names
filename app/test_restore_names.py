ed858425c74bc344ec52a6241e243ce164ab0576
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-21 13:58:02 +0200

    added tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,60 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names():
+    # Test case with missing or None first_name
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    # Test case with already existing first_name
+    users = [
+        {
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "full_name": "Alice Smith",
+        },
+        {
+            "first_name": "Bob",
+            "last_name": "Johnson",
+            "full_name": "Bob Johnson",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "full_name": "Alice Smith",
+        },
+        {
+            "first_name": "Bob",
+            "last_name": "Johnson",
+            "full_name": "Bob Johnson",
+        },
+    ]
+
+if __name__ == "__main__":
+    pytest.main()
