1e923317d810becd29023dfe4f66f16b39f0c1ae
Andrii Zubchenko <dissom1987@gmail.com>
2024-01-11 21:54:11 +0200

    solution1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,8 +3,21 @@ from app.restore_names import restore_names
 
 
 @pytest.fixture
-def users_with_missing_first_name() -> list:
-    yield [
+def users_with_missing_first_name() -> tuple:
+    expected_result = [
+
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    users = [
         {
             "first_name": None,
             "last_name": "Holy",
@@ -15,45 +28,25 @@ def users_with_missing_first_name() -> list:
             "full_name": "Mike Adams",
         },
     ]
+    yield users, expected_result
 
 
 def test_restore_names_with_missing_first_name(
-        users_with_missing_first_name: list
+        users_with_missing_first_name: tuple
 ) -> None:
-    users = users_with_missing_first_name
+    users, expected_result = users_with_missing_first_name
+
     restore_names(users)
-    assert users == [
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        },
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
+    assert users == expected_result
 
 
 def test_restore_names_with_existing_first_name(
-        users_with_missing_first_name: list
+        users_with_missing_first_name: tuple
 ) -> None:
-    users = users_with_missing_first_name
+    users, expected_result = users_with_missing_first_name
     users[0]["first_name"] = "Jack"
-    users[1]["first_name"] = "Mike"
+    users[1]["first_name"] = ("Mike"
+                              "")
     restore_names(users)
 
-    assert users == [
-
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        },
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
+    assert users == expected_result
