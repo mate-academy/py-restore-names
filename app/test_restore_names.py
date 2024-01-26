8c2b85b93bb013f32a39b2207f84923c593c6475
Dmytry-S <sviridoffd@gmail.com>
2024-01-26 16:38:29 +0200

    Implement restore-names-test task.

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,47 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "broken_list,restored_list",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+                {
+                    "first_name": "Tom",
+                    "last_name": "Dow",
+                    "full_name": "Tom Dow",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+                {
+                    "first_name": "Tom",
+                    "last_name": "Dow",
+                    "full_name": "Tom Dow",
+                },
+            ]
+        )
+    ]
+)
+def test_restore_names(broken_list: list, restored_list: list) -> None:
+    restore_names(broken_list)
+    assert broken_list == restored_list
