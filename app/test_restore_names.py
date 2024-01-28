acff3e5f62e1313290ee3a07744a086a0dcef9e2
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-28 22:23:21 +0100

    Solutiuon

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,18 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+list_to_check = [
+    {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+    {"last_name": "Adams", "full_name": "Mike Adams"},
+]
+
+result_list = [
+    {"first_name": "Jack", "full_name": "Jack Holy", "last_name": "Holy"},
+    {"first_name": "Mike", "full_name": "Mike Adams", "last_name": "Adams"},
+]
+
+
+def test_check_restore_users() -> None:
+    restore_names(list_to_check)
+    assert (
+        list_to_check == result_list
+    )
