b44f6277a0e43a91922db043ee5cb73150d07251
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-29 16:46:12 +0200

    Solved

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,48 @@
 import pytest
+from typing import List
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,expected_users",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Viktorovich",
+                    "full_name": "Viktor Viktorovich"
+                },
+                {
+                    "last_name": "Dmytrovich",
+                    "full_name": "Dmytro Dmytrovich"
+                },
+                {
+                    "first_name": "Nastya",
+                    "last_name": "Lisovska",
+                    "full_name": "Nastya Lisovska"
+                }
+            ],
+            [
+                {
+                    "first_name": "Viktor",
+                    "last_name": "Viktorovich",
+                    "full_name": "Viktor Viktorovich"
+                },
+                {
+                    "first_name": "Dmytro",
+                    "last_name": "Dmytrovich",
+                    "full_name": "Dmytro Dmytrovich"
+                },
+                {
+                    "first_name": "Nastya",
+                    "last_name": "Lisovska",
+                    "full_name": "Nastya Lisovska"
+                }
+            ]
+        )
+    ]
+)
+def test_restore_names(users: List[dict], expected_users: List[dict]) -> None:
+    restore_names(users)
+    assert users == expected_users
