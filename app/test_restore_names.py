c5d5f873d1b8272893d1f115c83d2f2472cd9a71
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-30 17:09:56 +0200

    Sol

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,14 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+@pytest.fixture()
+def user_example():
+    user = [{"first_name": None, "full_name": "John Smith"},
+            {"full_name": "Matt Jackson"}]
+    return user
+
+
+def test_restore_names_with_none_name(user_example):
+    restore_names(user_example)
+    assert user_example == [{"first_name": "John", "full_name": "John Smith"},
+            {"first_name": "Matt", "full_name": "Matt Jackson"}]
