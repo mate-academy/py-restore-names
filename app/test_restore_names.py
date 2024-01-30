d2c8baedd38943b42da71a8f86394a716ca76961
unknown <artem.sapiens@gmail.com>
2024-01-30 15:26:10 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,66 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("input_users, expected", [
+    (
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }
+        ],
+        [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }
+        ]
+    ),
+    (
+        [
+            {
+                "first_name": "Alice",
+                "last_name": "Smith",
+                "full_name": "Alice Smith"
+            }
+        ],
+        [
+            {
+                "first_name": "Alice",
+                "last_name": "Smith",
+                "full_name": "Alice Smith"
+            }
+        ]
+    ),
+    (
+        [
+            {
+                "first_name": None,
+                "last_name": "Single",
+                "full_name": "SingleName"
+            }
+        ],
+        [
+            {
+                "first_name": "SingleName",
+                "last_name": "Single",
+                "full_name": "SingleName"
+            }
+        ]
+    )
+])
+def test_restore_names(input_users: list, expected: list) -> None:
+    restore_names(input_users)
+    assert input_users == expected
