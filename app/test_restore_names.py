9802f5c1dd36a594954859c468686acee89a723a
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-30 14:55:56 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,28 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users_fixture() -> list:
+    return [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_names_no_first_name(users_fixture: list) -> None:
+    restore_names(users_fixture)
+    assert users_fixture[0]["first_name"] == "Jack"
+    assert users_fixture[1]["first_name"] == "Mike"
+
+
+def test_restore_names_when_first_name_is_none(users_fixture: list) -> None:
+    users_fixture[0]["first_name"] = None
+    restore_names(users_fixture)
+    assert users_fixture[0]["first_name"] == "Jack"
