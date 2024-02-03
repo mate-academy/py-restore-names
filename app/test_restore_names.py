663cf1b36d460507237188248d387b9d4456853c
angelzagniy <angelzagniy@gmail.com>
2024-02-03 22:41:46 +0200

    fix problems with continuation of lines

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,3 @@
-import pytest
 from app.restore_names import restore_names
 
 
@@ -16,14 +15,14 @@ def test_all_users_receive_first_name() -> None:
     ]
     restore_names(users)
     assert users == [
-  {
-    "first_name": "Jack",
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "first_name": "Mike",
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
