3795eacf8c08a37a1e74e03e36250a115c53947b
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-02-06 16:11:19 +0200

    implement-py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,55 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "user_list, expected_result",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Black",
+                    "full_name": "John Black"
+                }
+            ],
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Black",
+                    "full_name": "John Black"
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "last_name": "Seinfeld",
+                    "full_name": "Jerry Seinfeld"
+                },
+                {
+                    "last_name": "Seinfeld",
+                    "full_name": "Jerry Seinfeld"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jerry",
+                    "last_name": "Seinfeld",
+                    "full_name": "Jerry Seinfeld"
+                },
+                {
+                    "first_name": "Jerry",
+                    "last_name": "Seinfeld",
+                    "full_name": "Jerry Seinfeld"
+                }
+
+            ]
+        )
+    ]
+)
+def test_when_first_name_is_none(user_list: list[dict],
+                                 expected_result: list[dict]) -> None:
+    restore_names(user_list)
+    assert user_list == expected_result
