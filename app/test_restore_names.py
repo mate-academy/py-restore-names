3d6548ca4f6492428a685940d35faa105036bf8e
Sofia Zholud <szholuds@gmail.com>
2024-02-07 23:23:50 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,23 @@
+from typing import List, Dict, Any
 import pytest
 from app.restore_names import restore_names
+from unittest.mock import patch
 
-# write your tests here
+
+@pytest.fixture
+def mock_users() -> List[Dict[str, Any]]:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"},
+    ]
+
+
+@patch("app.restore_names")
+def test_restore_names(mock_restore_names: Any,
+                       mock_users: List[Dict[str, Any]]) -> None:
+    mock_restore_names.return_value = None
+
+    restore_names(mock_users)
+
+    assert mock_users[0]["first_name"] == "Jack"
+    assert mock_users[1]["first_name"] == "Mike"
