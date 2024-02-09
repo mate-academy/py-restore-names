a03f4c0f6b47e8699643c189e9a03ca7b96a5b4c
mkuchuman <mkuchuman1@gmail.com>
2024-02-09 01:24:44 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+users = [
+    {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
+]
+
+
+@pytest.fixture()
+def names_template() -> list:
+    return users
+
+
+def test_cannot_access_if_only_valid_url(names_template: callable) -> None:
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
