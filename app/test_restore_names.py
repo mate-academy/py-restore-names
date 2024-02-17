6c028e24ea4af25f81227b2f9525adfad9fee83d
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-18 00:23:26 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,27 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user() -> dict:
+    return {
+        "first_name": "Tony",
+        "last_name": "Stark",
+        "full_name": "Tony Stark",
+    }
+
+
+def test_restore_none_name(user: dict) -> None:
+    user["first_name"] = None
+
+    restore_names([user])
+
+    assert user["first_name"] == "Tony"
+
+
+def test_restore_missing_name(user: dict) -> None:
+    del user["first_name"]
+
+    restore_names([user])
+
+    assert user["first_name"] == "Tony"
