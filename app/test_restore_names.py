8d3a6a34aa596e861acb80f6688a438d6a49dc3c
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-02-19 10:49:51 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_list, result", [
+        pytest.param(
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            [{
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            id="Test when key 'first_name' is None"),
+        pytest.param(
+            [{
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }],
+            [{
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }],
+            id="Test when input dictionary does not have key 'first_name'")
+    ]
+)
+def test_restore_names(input_list: list[dict], result: list[dict]) -> None:
+    restore_names(input_list)
+    assert input_list == result
