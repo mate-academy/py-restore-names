40903d022e2ff58b94293183cd122761cc668e75
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-02-28 08:40:49 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,31 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users() -> list:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"},
+        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+    ]
+
+
+def test_restore_names(users) -> None:
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+    assert users[2]["first_name"] == "Jack"
+    assert users[3]["first_name"] == "Mike"
+
+
+def test_restore_names_no_change(users) -> None:
+    # Test if the function doesn't change the first_name if it's already correct
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+    assert users[2]["first_name"] == "Jack"
+    assert users[3]["first_name"] == "Mike"
