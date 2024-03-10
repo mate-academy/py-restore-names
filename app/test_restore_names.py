fb321db8e989ead294c4ec371de35ef954644bc6
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-10 20:17:17 +0100

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,21 @@
-import pytest
-from app.restore_names import restore_names
+from app.restore_names import restore_names  # Adjust the import as necessary
 
-# write your tests here
+
+def test_restore_names() -> None:
+    users = [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"},
+        {"first_name": "Sarah", "last_name": "Connor",
+         "full_name": "Sarah Connor"}
+    ]
+
+    expected_users = [
+        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+        {"first_name": "Mike", "last_name": "Adams",
+         "full_name": "Mike Adams"},
+        {"first_name": "Sarah", "last_name": "Connor",
+         "full_name": "Sarah Connor"}
+    ]
+    restore_names(users)
+    assert users == expected_users, ("The restore_names function did not"
+                                     " update the users as expected.")
