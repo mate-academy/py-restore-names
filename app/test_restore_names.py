b860bd9cdca0857205b95885f4b71f9cf235e6e2
Lumideus <d.bilan2004@gmail.com>
2024-03-20 06:40:06 +0200

    Add tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,46 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users_before_changes, users_expected",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+
+                }
+            ]
+        )
+    ]
+)
+def test_function_should_provide_correct_changes(
+        users_before_changes: list[dict],
+        users_expected: list[dict]
+) -> None:
+    restore_names(users_before_changes)
+    assert users_before_changes == users_expected
+
+
+def test_should_raise_correct_error() -> None:
+    with pytest.raises(TypeError):
+        restore_names(1)
