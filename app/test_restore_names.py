14fbff395d0de83c80d947b3638afd5d46c2402c
pochtalon <pochtalon1010@gmail.com>
2024-03-26 10:31:38 +0200

    Solved

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,30 +1,29 @@
-import pytest
 from app.restore_names import restore_names
 
 
 def test_restore_user_names() -> None:
     users = [
-      {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
     fixed_users = [
-      {
-        "first_name": "Jack",
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "first_name": "Mike",
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
     restore_names(users)
     assert users == fixed_users
