d1e85ab6f8b835c9486add55287298cc26ada415
Valentin Pozniak <waliker448@gmail.com>
2024-04-01 14:33:12 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,32 @@
-import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names() -> None:
+    users_lost_values = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users_lost_values)
+
+    expected_users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    assert users_lost_values == expected_users
