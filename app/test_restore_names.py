62227517e0ec18f20298f3d9b491dece38d6d671
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-04 17:19:52 +0300

    update using pytest

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,17 +1,27 @@
 import pytest
+
 from app.restore_names import restore_names
 
 
-def test_name_is_none() -> None:
-    users = [{
-        "first_name": None,
-        "full_name": "Jack Holy",
-    }]
+@pytest.fixture
+def users() -> list[dict]:
+    return [
+        {
+            "first_name": None,
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_name_is_none(users: list[dict]) -> None:
     restore_names(users)
     assert users[0]["first_name"] == "Jack"
 
 
-def test_name_is_missing() -> None:
-    users = [{"full_name": "Mike Adams"}]
+def test_name_is_missing(users: list[dict]) -> None:
     restore_names(users)
-    assert users[0]["first_name"]
+    assert users[1]["first_name"]
