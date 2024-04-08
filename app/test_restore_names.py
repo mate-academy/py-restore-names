c37a0331cb43ff7309ec12c3827ae7ee3e4ca7c7
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-04-07 20:08:17 -0600

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,31 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_template() -> list[dict]:
+    return [
+        {
+            "first_name": "Vladyslav",
+            "last_name": "Prokopiv",
+            "full_name": "Vladyslav Prokopiv"
+        }
+    ]
+
+
+def test_check_if_first_name_is_none(users_template: list[dict]) -> None:
+    users_template[0]["first_name"] = None
+    restore_names(users_template)
+    expected = users_template[0]["full_name"].split()[0]
+
+    assert users_template[0]["first_name"] == expected, \
+        "'first_name' shold be equal 'Vladyslav'"
+
+
+def test_check_if_first_name_not_in_dict(users_template: list[dict]) -> None:
+    del users_template[0]["first_name"]
+    restore_names(users_template)
+    expected = users_template[0]["full_name"].split()[0]
+
+    assert users_template[0]["first_name"] == expected, \
+        "'first_name' shold be equal 'Vladyslav'"
