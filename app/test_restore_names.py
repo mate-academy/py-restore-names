25c08195957e0f62e9b1dc95b4f8755d414a26c1
alexk <alexkochubey13@gmail.com>
2024-04-10 16:44:40 +0300

    Solution?

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,29 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def person_template() -> list[dict]:
+    return [
+        {
+            "first_name": "Test",
+            "last_name": "Testekno",
+            "full_name": "Test Testekno"
+        }
+    ]
+
+
+def test_can_restore_first_name(
+        person_template: list[dict]) -> None:
+    person_template[0]["first_name"] = None
+    restore_names(person_template)
+    expected_result = person_template[0]["full_name"].split()[0]
+    assert person_template[0]["first_name"] == expected_result
+
+
+def test_can_restore_first_name_if_there_is_no_dict(
+        person_template: list[dict]) -> None:
+    del person_template[0]["first_name"]
+    restore_names(person_template)
+    expected_result = person_template[0]["full_name"].split()[0]
+    assert person_template[0]["first_name"] == expected_result
