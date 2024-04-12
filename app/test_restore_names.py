32161219cb84b9cab2d30fef330ac65f8ec17305
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-12 18:11:04 +0300

    fixed

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,31 +6,31 @@ from app.restore_names import restore_names
 @pytest.fixture()
 def users() -> list:
     return [
-      {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
 
 
 @pytest.fixture()
 def expected_users() -> list:
     return [
-      {
-        "first_name": "Jack",
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "first_name": "Mike",
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
     ]
 
 
