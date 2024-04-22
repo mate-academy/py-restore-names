1c7b3cf50b8e0a6d9ff10ca04ef106e4fc621362
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-04-22 20:27:41 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("test_input, expected", [
+    ([
+        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+        {"first_name": "Jane", "last_name": "Smith", "full_name": "Jane Smith"}
+    ], [
+        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+        {"first_name": "Jane", "last_name": "Smith", "full_name": "Jane Smith"}
+    ]),
+    ([
+        {"last_name": "Doe", "full_name": "John Doe"},
+        {"first_name": None, "last_name": "Smith", "full_name": "Jane Smith"}
+    ], [
+        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+        {"first_name": "Jane", "last_name": "Smith", "full_name": "Jane Smith"}
+    ]),
+    ([
+        {"last_name": "Doe", "full_name": "John Doe"},
+        {"last_name": "Smith", "full_name": "Jane Smith"}
+    ], [
+        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+        {"first_name": "Jane", "last_name": "Smith", "full_name": "Jane Smith"}
+    ])
+])
+def test_restore_names(test_input: list, expected: list) -> None:
+    restore_names(test_input)
+    assert test_input == expected
