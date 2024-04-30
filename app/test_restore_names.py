a7a367e5f887e95528a5233b7136f43b06da7a1e
Liudmyla <4888814@gmail.com>
2024-04-30 11:37:21 +0300

    'solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,63 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "user_before, user_after ",
+    [
+        pytest.param([
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+        ],
+            id="None + empty"),
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="None + was first name"
+        )
+    ]
+)
+def test_add_first_name(user_before: list, user_after: list) -> None:
+    restore_names(user_before)
+    assert user_before == user_after
