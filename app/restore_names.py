a15ccd9b6924fc1385458a2a5b9aba11088057a7
Yakov K <hyakunosuke7@gmail.com>
2024-05-20 11:54:33 +0300

    NamesComplete

--- app/restore_names.py
+++ app/restore_names.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Dict
 
 
-def restore_names(users: List[dict]) -> None:
+def restore_names(users: List[Dict[str, str]]) -> None:
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
             user["first_name"] = user["full_name"].split()[0]
