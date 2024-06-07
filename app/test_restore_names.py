a1cfb36ad5175da6ec5856b7b3b01a7f0fdd7ad6
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-07 14:42:39 +0200

    Solution v2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,39 +1,13 @@
 import pytest
-from unittest import mock
+
 from app.restore_names import restore_names
 
 
-def test_first_name_is_none() -> None:
-    users = [
-      {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
-    ]
-    expected = [
-      {
-        "first_name": "Jack",
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-      },
-      {
-        "first_name": "Mike",
-        "last_name": "Adams",
-        "full_name": "Mike Adams",
-      },
-    ]
-    restore_names(users)
-    assert users == expected
-
-
-def test_without_first_name() -> None:
-    users = [
+@pytest.fixture()
+def users_first_name_equal_none() -> list:
+    return [
         {
+            "first_name": None,
             "last_name": "Holy",
             "full_name": "Jack Holy",
         },
@@ -42,6 +16,24 @@ def test_without_first_name() -> None:
             "full_name": "Mike Adams",
         },
     ]
+
+
+@pytest.fixture()
+def users_without_first_name() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_first_name_is_none(users_first_name_equal_none: list) -> None:
     expected = [
         {
             "first_name": "Jack",
@@ -54,5 +46,26 @@ def test_without_first_name() -> None:
             "full_name": "Mike Adams",
         },
     ]
-    restore_names(users)
-    assert users == expected
+    restore_names(users_first_name_equal_none)
+    assert (
+        users_first_name_equal_none == expected
+    ), "first name should exist"
+
+
+def test_without_first_name(users_without_first_name: list) -> None:
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users_without_first_name)
+    assert (
+        users_without_first_name == expected
+    ), "first name should be not None"
