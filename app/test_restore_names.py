2a493d5d88e822328ca6136371607a779feeee62
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-06-07 17:35:08 +0300

    Done

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,34 +2,19 @@ import pytest
 from app.restore_names import restore_names
 
 
-@pytest.mark.parametrize(
-    "users,expected_results",
-    pytest.param(
-        [
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ],
-        [
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ]
-    )
-)
-def test_restore_names(users: list, expected_results: list) -> None:
+@pytest.fixture
+def users() -> list[dict]:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"}
+    ]
+
+
+def test_restore_names(users: list) -> None:
     restore_names(users)
-    assert users == expected_results
+
+    expected_result = [
+        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+    ]
+    assert users == expected_result
