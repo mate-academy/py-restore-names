dce2b02f9b12e6c6e3a9a3df0604cd2b5ca50863
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-06-07 17:30:59 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -14,14 +14,14 @@ from app.restore_names import restore_names
             {
                 "last_name": "Adams",
                 "full_name": "Mike Adams",
-             },
+            },
         ],
         [
             {
                 "first_name": "Jack",
                 "last_name": "Holy",
                 "full_name": "Jack Holy",
-             },
+            },
             {
                 "first_name": "Mike",
                 "last_name": "Adams",
