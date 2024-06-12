90c233ddbac818224fb68c488f48889788cbedad
timur <stukantimur811@gmail.com>
2024-06-12 18:26:53 +0300

    create tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,47 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+@pytest.fixture()
+def user_template_without_name() -> list:
+    return [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_names_if_first_name_is_none(user_template: list) -> None:
+    restore_names(user_template)
+    assert user_template == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+def test_restore_name_if_name_is_not_exist(
+        user_template_without_name: list
+) -> None:
+    restore_names(user_template_without_name)
+    assert user_template_without_name == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
