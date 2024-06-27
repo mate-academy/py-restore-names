29eb2a2bd38b4f1eadeb1ec23f07042e6ecae331
Alexander <sasacihun@gmail.com>
2024-06-27 12:53:00 +0300

    Solution (test 3 possible options)

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,57 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture(params=[
+    [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ],
+    [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ],
+    [
+        {
+            # "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+])
+def data_template(request):
+    return request.param
+
+
+def test_restore_names(data_template):
+    restore_names(data_template)
+    assert data_template == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
