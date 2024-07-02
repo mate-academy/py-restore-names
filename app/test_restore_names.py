317f3df0a5bfbb9af6cc71f105bfb0daab61b65c
TarasFirst <firsttaras@mail.com>
2024-07-02 14:33:08 +0300

    Solution_3

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,28 +7,55 @@ from typing import List
     "users, expected_users",
     [
         (
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ]
         ),
         (
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ]
         )
     ]
 )
