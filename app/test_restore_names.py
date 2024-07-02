7f93ee15f4321489207f5db99b22e70e0d4f2745
TarasFirst <firsttaras@mail.com>
2024-07-02 14:10:44 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,66 +1,41 @@
 import pytest
 from app.restore_names import restore_names
+from typing import List
 
 
-def test_given_not_a_list() -> None:
-    with pytest.raises(TypeError):
-        restore_names("not a list")
+@pytest.mark.parametrize(
+    "users, expected_users",
+    [
+        (
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
 
+        ),
+        (
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+        )
+    ]
+)
+def test_should_fill_first_name_if_it_none_or_empty(
+        users: List[dict],
+        expected_users: List[dict]) -> None:
+    restore_names(users)
 
-def test_list_contains_dictionaries() -> None:
-    test_input = [{"full_name": "John Doe"}, {"full_name": "Jane Doe"}]
-    try:
-        restore_names(test_input)
-    except Exception as e:
-        pytest.fail(f"Unexpected error: {e}")
+    assert users == expected_users
 
-
-def test_dictionaries_have_full_name_key() -> None:
-    test_input = [{"full_name": "John Doe"}, {"full_name": "Jane Doe"}]
-    restore_names(test_input)
-    for user in test_input:
-        assert "full_name" in user,\
-            "Dictionary does not contain 'full_name' key"
-
-
-def test_full_name_is_string() -> None:
-    test_input = [{"full_name": "John Doe"}, {"full_name": "Jane Doe"}]
-    restore_names(test_input)
-    for user in test_input:
-        assert isinstance(
-            user["full_name"], str
-        ), "'full_name' is not a string"
-
-
-def test_full_name_contains_two_words() -> None:
-    test_input = [{"full_name": "John Doe"}, {"full_name": "Jane Doe"}]
-    restore_names(test_input)
-    for user in test_input:
-        assert len(
-            user["full_name"].split()
-        ) == 2, "'full_name' does not contain two words"
-
-
-def test_function_returns_none() -> None:
-    test_input = [{"full_name": "John Doe"}, {"full_name": "Jane Doe"}]
-    result = restore_names(test_input)
-    assert result is None, "Function does not return None"
-
-
-@pytest.mark.parametrize("input_dict, expected", [
-    ({"full_name": "John Doe", "first_name": "John"}, "John"),
-    ({"full_name": "John Doe"}, "John"),
-    ({"full_name": "John Doe", "first_name": None}, "John"),
-    ({"full_name": "John Doe", "first_name": ""}, "John")
-])
-def test_first_name_is_str_if_present(
-        input_dict: dict,
-        expected: str
-) -> None:
-    restore_names([input_dict])
-    assert isinstance(
-        input_dict["first_name"], str
-    ), "'first_name' is not a string"
-    assert input_dict["first_name"] == expected,\
-        (f"Expected 'first_name' to be {expected}, "
-         f"got {input_dict['first_name']}")
