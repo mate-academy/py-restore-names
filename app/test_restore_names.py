455b6d754a4721582e8644c125cb96c53dcbb88a
elfredburger <elfredfredburger@gmail.com>
2024-07-19 06:48:32 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,12 +7,12 @@ from app.restore_names import restore_names
     [
         [
             {"full_name": "John Doe", "first_name": None, "last_name": "Doe"},
-            {"full_name": "Jane Doe", "first_name": "Jane", "last_name": "Doe"},
-            {"full_name": "Jack Doe",  "last_name": "Doe"},
+            {"full_name": "Bob Doe", "first_name": "Bob", "last_name": "Doe"},
+            {"full_name": "Jack Doe", "last_name": "Doe"},
         ]
     ],
 )
-def test_restore_names(users):
+def test_restore_names(users: list[dict]) -> None:
     restore_names(users)
     assert users == [
         {"full_name": "John Doe", "first_name": "John", "last_name": "Doe"},
