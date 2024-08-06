1cf017a0a7de026214279e1db30545b6a93fd37c
Danyil <dartomov@gmail.com>
2024-08-06 17:48:17 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,45 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "corrupted_dict,expected_fixed_dict",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+        )
+    ]
+)
+def test_check_if_function_fixing_lost_first_name_correctly(
+        corrupted_dict: list,
+        expected_fixed_dict: list
+) -> None:
+    restore_names(corrupted_dict)
+    assert (
+        corrupted_dict[0]["first_name"] == expected_fixed_dict[0]["first_name"]
+    )
+    assert (
+        corrupted_dict[1]["first_name"] == expected_fixed_dict[1]["first_name"]
+    )
