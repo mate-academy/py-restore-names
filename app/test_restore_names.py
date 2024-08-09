1e93691d6e5c6780b95c958198b969f32d741cf0
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-09 19:34:03 +0300

    commit

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -97,36 +97,6 @@ def test_restore_name_with_no_first_name() -> None:
     assert users == expected
 
 
-def test_restore_name_with_none_last_name() -> None:
-    users = [
-        {
-            "first_name": "Jack",
-            "full_name": "Jack Holy",
-        },
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
-
-    restore_names(users)
-
-    expected = [
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        },
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
-
-    assert users == expected
-
-
 def test_restore_names_empty_list() -> None:
     users = []
     expected = []
