1303ec68a79fe484e4556e090c6a0f2f7e4d5e77
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-09 19:21:10 +0300

    commit

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,134 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_name_with_none_first_name():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users == expected
+
+
+def test_restore_name_with_exciting_first_name():
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users == expected
+
+
+def test_restore_name_with_no_first_name():
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users == expected
+
+def test_restore_name_with_none_last_name():
+    users = [
+        {
+            "first_name": "Jack",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+    assert users == expected
+
+def test_restore_names_empty_list() -> None:
+    users = []
+    expected = []
+
+    restore_names(users)
+    assert users == expected
