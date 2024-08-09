7c5fb2421fd62b77bea20d226d8d39862a8af91b
serhii <ockar2018@gmail.com>
2024-08-09 23:28:35 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,46 +2,37 @@ import pytest
 from app.restore_names import restore_names
 
 
-def test_restore_names(users: list) -> None:
-    for user in users:
-        if "first_name" not in user or user["first_name"] is None:
-            user["first_name"] = user["full_name"].split()[0]
-
-
 @pytest.mark.parametrize(
     "users, expected",
     [
-        ([
-             {"last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ], [
-             {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ]),
-        ([
-             {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ], [
-             {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ]),
-        ([
-             {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ], [
-             {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-             {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-         ]),
-        ([], []),
-        ([
-             {"first_name": None, "last_name": "Holy"},
-             {"last_name": "Adams"}
-         ], [
-             {"first_name": None, "last_name": "Holy"},
-             {"last_name": "AdAMS"}
-         ])
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ]
+        )
     ]
 )
-def test_restore_names(users, expected):
+def test_restore_names(users: list[dict], expected: list[dict]) -> None:
     restore_names(users)
     assert users == expected
