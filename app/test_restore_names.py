a9048ece73191aadb35d4b4cd88c373a4c333ff7
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-09 17:48:58 +0300

    solving2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,31 +1,24 @@
+import pytest
 from app.restore_names import restore_names
 
-def test_restore_names() -> None:
-    users = [
-        {
-            "first_name": None,
-            "last_name": "Shevchenko",
-            "full_name": "Sasha Shevchenko"},
-        {
-            "last_name": "Koval",
-            "full_name": "Ivan Koval"},
-        {
-            "first_name": "Mykola",
-            "last_name": "Syvchenko",
-            "full_name": "Mykola Syvchenko"},
+@pytest.mark.parametrize(
+    "users, user_list",
+    [
+        (
+            [
+                {"first_name": None, "full_name": "Jack Holy"},
+                {"full_name": "Mike Adams"}
+            ],
+            [
+                {"first_name": "Jack", "full_name": "Jack Holy"},
+                {"first_name": "Mike", "full_name": "Mike Adams"}
+            ]
+        ),
     ]
+)
+def test_restore_names(
+        users: list,
+        user_list: list
+) -> None:
     restore_names(users)
-    assert users == [
-        {
-            "first_name": "Sasha",
-            "last_name": "Shevchenko",
-            "full_name": "Sasha Shevchenko"},
-        {
-            "first_name": "Ivan",
-            "last_name": "Koval",
-            "full_name": "Ivan Koval"},
-        {
-            "first_name": "Mykola",
-            "last_name": "Syvchenko",
-            "full_name": "Mykola Syvchenko"},
-    ]
+    assert users == user_list
