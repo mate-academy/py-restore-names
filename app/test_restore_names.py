87c81e027f895b051b7ec1dd1cc908e62a9e4097
Igor Chapy <misha@mate.acedemy>
2024-08-20 19:52:45 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,42 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names() -> None:
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Adams",
+            "full_name": "John Adams",
+
+        },
+        {
+            "first_name": None,
+            "last_name": "Adams",
+            "full_name": "Sam Adams",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Billy Adams",
+        },
+    ]
+    expected_result = [
+        {
+            "first_name": "John",
+            "last_name": "Adams",
+            "full_name": "John Adams",
+
+        },
+        {
+            "first_name": "Sam",
+            "last_name": "Adams",
+            "full_name": "Sam Adams",
+        },
+        {
+            "first_name": "Billy",
+            "last_name": "Adams",
+            "full_name": "Billy Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == expected_result
