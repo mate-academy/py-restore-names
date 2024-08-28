06be04964a54aa76d4e1122c184055c02d087a70
Kulib <kulibchenkovalera@gmail.com>
2024-08-28 11:11:01 +0300

    first commit

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,37 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "incorrect_users,expected",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+        )
+    ]
+)
+def test_should_correct_user_without_firs_name(incorrect_users, expected) -> None:
+    restore_names(incorrect_users)
+    assert incorrect_users == expected
