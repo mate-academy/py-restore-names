020f265ac1d37a98b1a74efa392277f2419c5a4c
Ivan <vanya.bidnenko@gmail.com>
2024-09-11 19:58:26 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,23 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "list_users, expected_result",
+    [
+        (
+                [{"first_name": None, "full_name": "Jack Holy"}],
+                [{"first_name": "Jack", "full_name": "Jack Holy"}]
+        ),
+        (
+                [{"full_name": "Mike Adams"}],
+                [{"first_name": "Mike", "full_name": "Mike Adams"}]
+        )
+    ]
+)
+def test_restore_names(list_users, expected_result):
+    users_copy = [user.copy() for user in list_users]
+    restore_names(users_copy)
+    assert users_copy == expected_result
+
+
