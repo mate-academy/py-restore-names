a7c14d42be94a360bf322ea80ca4a19711ba9074
Andrii Levchenko <levchenkoandrii11@gmail.com>
2024-10-02 16:27:09 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,60 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
+            [{"last_name": "Doe",
+              "full_name": "John Doe"}],
+            [{"first_name": "John",
+              "last_name": "Doe",
+              "full_name": "John Doe"}]
+        ),
+        (
+            [{"first_name": None,
+              "last_name": "Doe",
+              "full_name": "John Doe"}],
+            [{"first_name": "John",
+              "last_name": "Doe",
+              "full_name": "John Doe"}]
+        ),
+        (
+            [{"first_name": "John",
+              "last_name": "Doe",
+              "full_name": "John Doe"}],
+            [{"first_name": "John",
+              "last_name": "Doe",
+              "full_name": "John Doe"}]
+        ),
+        (
+            [{"last_name": "Smith",
+              "full_name": "Jane Smith"},
+             {"first_name": None,
+              "last_name": "Jones",
+              "full_name": "Bob Jones"},
+             {"first_name": "Rick",
+              "last_name": "Dalton",
+              "full_name": "Rick Dalton"}],
+            [{"first_name": "Jane",
+              "last_name": "Smith",
+              "full_name": "Jane Smith"},
+             {"first_name": "Bob",
+              "last_name": "Jones",
+              "full_name": "Bob Jones"},
+             {"first_name": "Rick",
+              "last_name": "Dalton",
+              "full_name": "Rick Dalton"}]
+        )
+    ],
+    ids=[
+        "Missing first name",
+        "First name = None",
+        "First name is already present",
+        "Mixed cases"
+    ]
+)
+def test_restore_names(users: list, expected: list) -> None:
+    restore_names(users)
+    assert users == expected
