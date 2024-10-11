a170e0c4c31929fb232f9747ae9d18b8b1717a16
Fibonacci <volokitin.vitaliy@icloud.com>
2024-10-11 10:30:13 +0300

    Complated

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,59 @@
-import pytest
+import unittest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames(unittest.TestCase):
+    def test_restore_with_none_first_name(self) -> None:
+        users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ]
+        restore_names(users)
+        self.assertEqual(users[0]["first_name"], "Jack")
+
+    def test_restore_with_missing_first_name(self) -> None:
+        users = [
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }
+        ]
+        restore_names(users)
+        self.assertEqual(users[0]["first_name"], "Mike")
+
+    def test_first_name_already_present(self) -> None:
+        users = [
+            {
+                "first_name": "Anna",
+                "last_name": "Smith",
+                "full_name": "Anna Smith",
+            }
+        ]
+        restore_names(users)
+        self.assertEqual(users[0]["first_name"], "Anna")
+
+    def test_multiple_users(self) -> None:
+        users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Sara",
+                "last_name": "Jones",
+                "full_name": "Sara Jones",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }
+        ]
+        restore_names(users)
+        self.assertEqual(users[0]["first_name"], "Jack")
+        self.assertEqual(users[1]["first_name"], "Sara")
+        self.assertEqual(users[2]["first_name"], "Mike")
