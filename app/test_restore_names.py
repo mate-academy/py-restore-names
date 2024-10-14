bda7bb8c860404a8c3d817904f99dd19e4cc94b5
yura <pollo7964132@gmail.com>
2024-10-14 19:58:10 +0300

    implemented tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
-import pytest
+from unittest import TestCase
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNameFunc(TestCase):
+    def setUp(self) -> None:
+        self.user = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ]
+
+    def tearDown(self) -> None:
+        delattr(self, "user")
+
+    def test_should_set_first_name_value_when_first_name_is_none(self) -> None:
+        restore_names(self.user)
+        assert self.user == {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+
+    def test_should_set_first_name_key_and_value_when_first_name_not_exist(
+            self
+    ) -> None:
+        del self.user[0]["first_name"]
+        restore_names(self.user)
+        assert self.user == {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
