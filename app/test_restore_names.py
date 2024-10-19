33ddfa3ef9d5c2f1ebfd85300926d42223b36726
Den_k0 <denys.kotovskiy@gmail.com>
2024-10-19 20:20:35 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -18,17 +18,17 @@ from app.restore_names import restore_names
         ]
     ]
 )
-def test_restore_names(list_of_users):
+def test_restore_names(list_of_users: list[dict]) -> None:
     restore_names(list_of_users)
     assert list_of_users == [
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-            },
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-            },
-        ]
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
