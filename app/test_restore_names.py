3302e686bdeda796ad5e12f5bf7d99554d12ff27
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-21 16:12:38 +0300

    solution3

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,17 +5,12 @@ from app.restore_names import restore_names
 
 @pytest.fixture(scope="function")
 def users_data() -> Any:
-    return [
-              {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-              },
-              {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-              },
-            ]
+    return [{"first_name": None,
+             "last_name": "Holy",
+             "full_name": "Jack Holy",
+             },
+            {"last_name": "Adams",
+             "full_name": "Mike Adams", },]
 
 
 def test_restored_name(users_data: Any) -> Any:
