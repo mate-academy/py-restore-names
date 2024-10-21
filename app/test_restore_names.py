48b29b24574c67511d37b7cf711450fcf94462a9
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-10-21 12:56:58 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,15 +1,25 @@
-import pytest
 from app.restore_names import restore_names
 
 
 def test_without_first_name() -> None:
     data = [{"last_name": "Adams", "full_name": "Mike Adams"}]
     restore_names(data)
-    assert data == [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}]
+    assert data == [
+        {"first_name": "Mike",
+         "last_name": "Adams",
+         "full_name": "Mike Adams"}
+    ]
 
 
 def test_with_none_first_name() -> None:
-    data = [{"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"}]
+    data = [
+        {"first_name": None,
+         "last_name": "Adams",
+         "full_name": "Mike Adams"}
+    ]
     restore_names(data)
-    assert data == [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}]
-
+    assert data == [
+        {"first_name": "Mike",
+         "last_name": "Adams",
+         "full_name": "Mike Adams"}
+    ]
