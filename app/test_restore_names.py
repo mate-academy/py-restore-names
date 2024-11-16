0bb75bbcc6ee83a87d9a4b67d6aed969c1ecb1ae
Eldar <eldardemchenko19@mail.com>
2024-11-16 17:49:44 +0200

    fix_solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 from app.restore_names import restore_names
 
@@ -31,9 +33,64 @@ from app.restore_names import restore_names
                 }
             ],
             id="should restore missing first name"
-        )
+        ),
+        pytest.param(
+            [
+                {
+                    "first_name": "Sarah",
+                    "last_name": "Connor",
+                    "full_name": "Sarah Connor"
+                },
+                {
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "full_name": "John Doe"
+                }
+            ],
+            [
+                {
+                    "first_name": "Sarah",
+                    "last_name": "Connor",
+                    "full_name": "Sarah Connor"
+                },
+                {
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "full_name": "John Doe"
+                }
+            ],
+            id="first name already exists"
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith"
+                },
+                {
+                    "last_name": "Brown",
+                    "full_name": "Lisa Brown"
+                }
+            ],
+            [
+                {
+                    "first_name": "Anna",
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith"
+                },
+                {
+                    "first_name": "Lisa",
+                    "last_name": "Brown",
+                    "full_name": "Lisa Brown"
+                }
+            ],
+            id="should restore if no 'first name' key"
+        ),
     ]
 )
-def test_restore_first_name(users: dict, expected_users: dict) -> None:
+def test_restore_names(
+        users: List[dict],
+        expected_users: List[dict]
+) -> None:
     restore_names(users)
     assert users == expected_users
