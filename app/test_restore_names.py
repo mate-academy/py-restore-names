95ec2a718ae338b00a1ae970bc6ddbb494260668
Vova Savenkov <sava.v80@gmail.com>
2024-11-17 16:40:55 +0200

    Solution 2.0

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,28 +6,27 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users, result", [
         ([
-                {
-                    "first_name": None,
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ])
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ], [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ])
     ]
 )
 def test_restore_names(users: List[dict], result: List[dict]) -> None:
