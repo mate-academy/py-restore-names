72fb3e4fa34cb41a76b06b0efd106c7ed895d6b6
Adam Halka <galkaadam4@gmail.com>
2024-11-24 15:07:37 +0100

    Remake

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,36 +1,50 @@
 from app.restore_names import restore_names
 
 
-def test_restore_names_with_missing_first_name():
+def test_restore_names_with_missing_first_name() -> None:
     users = [
-        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {
+            "first_name": None, "last_name": "Holy", "full_name": "Jack Holy"
+        },
         {"last_name": "Adams", "full_name": "Mike Adams"}
     ]
     restore_names(users)
     expected = [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
     ]
     assert users == expected
 
-def test_no_modification_for_valid_first_name():
+def test_no_modification_for_valid_first_name() -> None:
     users = [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
     ]
     restore_names(users)
     expected = [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
     ]
     assert users == expected
 
-def test_empty_list():
+def test_empty_list() -> None:
     users = []
     restore_names(users)
     assert users == []
 
-def test_single_word_full_name():
+def test_single_word_full_name() -> None:
     users = [
         {"first_name": None, "last_name": "", "full_name": "Sam"}
     ]
