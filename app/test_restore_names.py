18887eead340876ae92ca3c1db951cf4eb0c0195
Beliar24 <blackdid7@gmail.com>
2024-12-05 23:52:03 +0200

    fix

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,19 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_no_change_if_first_name_exists() -> None:
+    users = [
+        {"first_name": "Anna", "last_name": "Smith", "full_name":
+            "Anna Smith"},
+        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+    ]
+    expected_users = users.copy()
+    restore_names(users)
+    assert users == expected_users
+
+
+def test_empty_user_list() -> None:
+    users = []
+    restore_names(users)
+    assert users == []
