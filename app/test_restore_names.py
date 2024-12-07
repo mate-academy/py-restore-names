b18539b9b90fd83bc09804363d2b191e403ec7bc
Глеб Дубинин <gleb1395@gmail.com>
2024-12-07 22:54:21 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -14,6 +14,16 @@ def data_users() -> list:
             "last_name": "Adams",
             "full_name": "Mike Adams",
         },
+        {
+            "first_name": "Alex",
+            "last_name": "Ivanov",
+            "full_name": "Alex Ivanov",
+        },
+        {
+            "first_name": "Maria",
+            "last_name": "Petrova",
+            "full_name": "Maria Petrova",
+        },
     ]
 
 
@@ -21,3 +31,9 @@ def test_restore_names_if_absent_first_name(data_users: list) -> None:
     restore_names(data_users)
     assert data_users[0]["first_name"] == "Jack"
     assert data_users[1]["first_name"] == "Mike"
+
+
+def test_restore_names_if_exist_first_name(data_users: list) -> None:
+    restore_names(data_users)
+    assert data_users[2]["first_name"] == "Alex"
+    assert data_users[3]["first_name"] == "Maria"
