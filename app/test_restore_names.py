b70b801acb91138dc1d99f0ac033246a43edf456
Illya Kovaliuk <kovalukilla271@gmail.com>
2024-12-14 22:45:48 +0200

    SOLUTION

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,21 +2,21 @@ import pytest
 from app.restore_names import restore_names
 from typing import Dict
 
-# write your tests her
+# write your tests here
 
 
 @pytest.fixture()
 def person_template() -> Dict[str, str]:
-    return {"first_name": "Illya",
-            "last_name": "Kovalevskaya",
-            "full_name": "Illya Kovalevskaya"}
+    return {"first_name": "Bob",
+            "last_name": "Smith",
+            "full_name": "Bob Smith"}
 
 
 def test_restore_names_if_firstname_is_none(person_template: Dict[str, str])\
         -> None:
     person_template["first_name"] = None
     restore_names([person_template])
-    assert person_template["first_name"] == "Illya"
+    assert person_template["first_name"] == "Bob"
 
 
 def test_restore_names_if_name_is_missing(person_template: Dict[str, str]) \
