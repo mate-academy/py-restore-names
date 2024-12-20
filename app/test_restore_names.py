c932ae4359f52fe41ab45ae8bbbfda90425c0c59
Vova <vova.gld05@gmail.com>
2024-12-20 15:43:16 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,42 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_dict,expected_dict",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ),
+        (
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }
+        )
+    ]
+)
+def test_should_fix_dict(
+        input_dict: list[dict],
+        expected_dict: dict
+) -> None:
+    restore_names(input_dict)
+    assert input_dict[0] == expected_dict
