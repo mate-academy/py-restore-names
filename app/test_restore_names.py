8107d63b5f073e1f6b7a97fd95e56d24c8b1b208
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-21 15:23:06 +0200

    Create task Restore names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,28 @@
 import pytest
 from app.restore_names import restore_names
+from typing import List
 
-# write your tests here
+
+@pytest.fixture()
+def users_template() -> list[dict]:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_names(users_template: List[dict]) -> None:
+
+    restore_names(users_template)
+
+    user1, user2 = users_template
+
+    assert user1["first_name"] == "Jack"
+    assert user2["first_name"] == "Mike"
