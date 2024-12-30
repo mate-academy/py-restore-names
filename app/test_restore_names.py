55037dd72f35ba71775ff1bd500f8d4138a009cd
Illya Kovaliuk <kovalukilla271@gmail.com>
2024-12-30 19:36:18 +0200

    Solutionn

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,8 +2,6 @@ import pytest
 from app.restore_names import restore_names
 from typing import Dict
 
-# write your tests here
-
 
 @pytest.fixture()
 def person_template() -> Dict[str, str]:
@@ -12,15 +10,33 @@ def person_template() -> Dict[str, str]:
             "full_name": "Bob Smith"}
 
 
-def test_restore_names_if_firstname_is_none(person_template: Dict[str, str])\
-        -> None:
+def test_restore_names_if_firstname_is_none(
+        person_template: Dict[str, str]) -> None:
     person_template["first_name"] = None
     restore_names([person_template])
     assert person_template["first_name"] == "Bob"
 
 
-def test_restore_names_if_name_is_missing(person_template: Dict[str, str]) \
-        -> None:
-    person_template["first_name"] = " "
+def test_restore_names_if_firstname_is_missing(
+        person_template: Dict[str, str]) -> None:
+    del person_template["first_name"]
     restore_names([person_template])
-    assert person_template["first_name"] == " "
+    assert person_template["first_name"] == "Bob"
+
+
+def test_restore_names_if_firstname_is_present(
+        person_template: Dict[str, str]) -> None:
+    restore_names([person_template])
+    assert person_template["first_name"] == "Bob"
+
+
+def test_restore_names_if_fullname_has_multiple_words() -> None:
+    user = {"last_name": "Smith", "full_name": "Robert John Smith"}
+    restore_names([user])
+    assert user["first_name"] == "Robert"
+
+
+def test_restore_names_if_fullname_has_one_word() -> None:
+    user = {"last_name": "Smith", "full_name": "Robert"}
+    restore_names([user])
+    assert user["first_name"] == "Robert"
