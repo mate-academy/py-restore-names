07af937e3e5df9698fee8f7da2cc545d86d0d717
nagafochfv <ohinskiy96@gmail.com>
2025-01-05 20:37:30 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_if_first_name_is_none_or_not_created():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == expected
