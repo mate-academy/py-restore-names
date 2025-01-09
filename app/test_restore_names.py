3ec460826184b0f536522d8f629a661d10f9aad2
Ivan Khomych <ivan.khomychh@gmail.com>
2025-01-09 12:52:46 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -9,17 +9,17 @@ from typing import List
         (
             [{"first_name": None,
               "last_name": "Holy",
-              "full_name": "Jack Holy",}],
+              "full_name": "Jack Holy"}],
             [{"first_name": "Jack",
               "last_name": "Holy",
-              "full_name": "Jack Holy",}]
+              "full_name": "Jack Holy"}]
         ),
         (
-             [{"last_name": "Adams",
-              "full_name": "Mike Adams",},],
+            [{"last_name": "Adams",
+              "full_name": "Mike Adams"}],
             [{"first_name": "Mike",
               "last_name": "Adams",
-              "full_name": "Mike Adams",},]
+              "full_name": "Mike Adams"}]
         )
     ]
 )
