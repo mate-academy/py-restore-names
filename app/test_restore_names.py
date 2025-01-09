0c0eb8d71031dcb7077af9c846b34670425c55bb
Taras <t.kudinov90@gmail.com>
2025-01-09 16:22:36 +0200

    Created dictionary and test-function for main method test.

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,31 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("users_in, result", [
+    ([
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ], [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ])
+])
+def test_restore_names(users_in: list, result: list) -> None:
+    restore_names(users_in)
+    assert users_in == result
