2c06d7f826888c139bf978d366ecb1f840ba4f67
Oksana Dzeryn <oks.dzeryn@gmail.com>
2025-01-09 21:00:43 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -13,7 +13,6 @@ from app.restore_names import restore_names
                     "full_name": "Jack Holy",
                 },
                 {
-                    "first_name": None,
                     "last_name": "Adams",
                     "full_name": "Mike Adams",
                 },
