5e6a8dad5e73b3f2c2d8f7de32d169ca5c284902
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2025-01-15 00:56:17 +0200

    implement tests for restore_names function

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,24 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_should_restore_name_if_first_name_equal_to_none() -> None:
+    user = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    restore_names(user)
+    assert user[0]["first_name"] == "Jack"
+
+
+def test_should_restore_name_if_first_name_is_missing() -> None:
+    user = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(user)
+    assert user[0]["first_name"] == "Mike"
