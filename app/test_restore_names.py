628d79fb3f872d28f507ff49f8a9008a20cbfcd5
unknown <bku089@gmail.com>
2025-01-16 20:51:25 +0200

    short names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,61 +3,61 @@ from app.restore_names import restore_names
 
 def test_restore_only_none_names() -> None:
     users = [
-        {"first_name": " ", "last_name": "Brown", "full_name": "Mike Brown"},
-        {"first_name": " ", "last_name": "Black", "full_name": "Anna Black"}
+        {"first_name": " ", "last_name": "Bo", "full_name": "Mike Bo"},
+        {"first_name": " ", "last_name": "Bl", "full_name": "Anna Bl"}
     ]
     restore_names(users)
     assert users == [
-        {"first_name": " ", "last_name": "Brown", "full_name": "Mike Brown"},
-        {"first_name": " ", "last_name": "Black", "full_name": "Anna Black"}
+        {"first_name": " ", "last_name": "Bo", "full_name": "Mike Bo"},
+        {"first_name": " ", "last_name": "Bl", "full_name": "Anna Bl"}
     ]
 
 
 def test_restore_missing_first_name_key() -> None:
     users = [
-        {"last_name": "Smith", "full_name": "John Smith"},
-        {"last_name": "Taylor", "full_name": "Anna Taylor"}
+        {"last_name": "Sm", "full_name": "John Sm"},
+        {"last_name": "Ta", "full_name": "Anna Ta"}
     ]
     restore_names(users)
     assert users == [
-        {"first_name": "John", "last_name": "Smith", "full_name": "John Smith"},
-        {"first_name": "Anna", "last_name": "Taylor", "full_name": "Anna Taylor"}
+        {"first_name": "John", "last_name": "Sm", "full_name": "John Sm"},
+        {"first_name": "Anna", "last_name": "Ta", "full_name": "Anna Ta"}
     ]
 
 
 def test_restore_only_missing_names() -> None:
     users = [
-        {"first_name": None, "last_name": "Smith", "full_name": "John Smith"},
-        {"first_name": None, "last_name": "Taylor", "full_name": "Anna Taylor"}
+        {"first_name": None, "last_name": "Sm", "full_name": "John Sm"},
+        {"first_name": None, "last_name": "Ta", "full_name": "Anna Ta"}
     ]
     restore_names(users)
     assert users == [
-        {"first_name": "John", "last_name": "Smith", "full_name": "John Smith"},
-        {"first_name": "Anna", "last_name": "Taylor", "full_name": "Anna Taylor"}
+        {"first_name": "John", "last_name": "Sm", "full_name": "John Sm"},
+        {"first_name": "Anna", "last_name": "Ta", "full_name": "Anna Ta"}
     ]
 
 
 def test_preserve_existing_first_name() -> None:
     users = [
-        {"first_name": "Emily", "last_name": "Blunt", "full_name": "Emily Blunt"},
-        {"first_name": "Chris", "last_name": "Evans", "full_name": "Chris Evans"}
+        {"first_name": "Em", "last_name": "Bl", "full_name": "Em Bl"},
+        {"first_name": "Ch", "last_name": "Ev", "full_name": "Ch Ev"}
     ]
     restore_names(users)
     assert users == [
-        {"first_name": "Emily", "last_name": "Blunt", "full_name": "Emily Blunt"},
-        {"first_name": "Chris", "last_name": "Evans", "full_name": "Chris Evans"}
+        {"first_name": "Em", "last_name": "Bl", "full_name": "Em Bl"},
+        {"first_name": "Ch", "last_name": "Ev", "full_name": "Ch Ev"}
     ]
 
 
 def test_empty_full_name() -> None:
     users = [
-        {"first_name": None, "last_name": "Brown", "full_name": "Jay Brown"},
-        {"first_name": None, "last_name": "Black", "full_name": "Joe Black"}
+        {"first_name": None, "last_name": "Br", "full_name": "Jay Br"},
+        {"first_name": None, "last_name": "Bl", "full_name": "Joe Bl"}
     ]
     restore_names(users)
     assert users == [
-        {"first_name": "Jay", "last_name": "Brown", "full_name": "Jay Brown"},
-        {"first_name": "Joe", "last_name": "Black", "full_name": "Joe Black"}
+        {"first_name": "Jay", "last_name": "Br", "full_name": "Jay Br"},
+        {"first_name": "Joe", "last_name": "Bl", "full_name": "Joe Bl"}
     ]
 
 
