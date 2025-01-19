ecb051b4c812b0e83e155b2e9106e7e968dbba43
neeeksa <avgust.k31@gmail.com>
2025-01-19 22:57:14 +0200

    first_try

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNamesClass:
+    @pytest.fixture()
+    def users_data_template(self) -> list:
+        users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+        return users
+
+    def test_users_first_name_is_numeric(self,
+                                         users_data_template: list
+                                         ) -> None:
+        users_data_template[0]["first_name"] = 1
+        restore_names(users_data_template)
+        assert users_data_template[0]["first_name"] == 1
+
+    def test_users_first_name_is_bool(self, users_data_template: list) -> None:
+        users_data_template[0]["first_name"] = False
+        restore_names(users_data_template)
+        assert users_data_template[0]["first_name"] is False
+
+    def test_users_first_name_is_none(self, users_data_template: list) -> None:
+        users_data_template[0]["first_name"] = None
+        restore_names(users_data_template)
+        assert users_data_template[0]["first_name"] == "Jack"
+
+    def test_users_first_name_is_empty_string(self,
+                                              users_data_template: list
+                                              ) -> None:
+        users_data_template[0]["first_name"] = ""
+        restore_names(users_data_template)
+        assert users_data_template[0]["first_name"] == ""
