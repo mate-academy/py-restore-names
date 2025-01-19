7f0c2ef262937ff665d50a776e52408b41d4f071
shafran <shafran.andriy@gmail.com>
2025-01-19 21:02:27 +0200

    Created tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,32 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("initial_value, expected_value", [
+                         pytest.param([{
+                             "last_name": "Holy",
+                             "full_name": "Jack Holy",
+                         }],
+                             [{
+                                 "first_name": "Jack",
+                                 "last_name": "Holy",
+                                 "full_name": "Jack Holy",
+                             }],
+                             id="Restore only missing names"
+                         ),
+                         pytest.param([{
+                             "first_name": None,
+                             "last_name": "Holy",
+                             "full_name": "Jack Holy",
+                         }],
+                             [{
+                                 "first_name": "Jack",
+                                 "last_name": "Holy",
+                                 "full_name": "Jack Holy",
+                             }],
+                             id="Restore only missing names")]
+                         )
+def test_restore_names(initial_value: list[dict],
+                       expected_value: list[dict]) -> None:
+    restore_names(initial_value)
+    assert initial_value == expected_value
