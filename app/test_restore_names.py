1694c4e1b14b06f6dbc7cb6989e2c21d95edac46
BohdanMT <metelua@gmail.com>
2025-01-24 23:18:13 +0200

    My solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_dict() -> list:
+    return [{"first_name": "Jack",
+             "last_name": "Holy",
+             "full_name": "Jack Holy"}]
+
+
+def test_restore_names_with_none(user_dict: list) -> None:
+    user_dict[0]["first_name"] = None
+    restore_names(user_dict)
+    assert user_dict[0] == {
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy"
+    }
+
+
+def test_restore_names_with_no_key(user_dict: list) -> None:
+    del user_dict[0]["first_name"]
+    restore_names(user_dict)
+    assert user_dict[0] == {
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy"
+    }
+
+
+def test_restore_names_do_nothing(user_dict: list) -> None:
+    restore_names(user_dict)
+    assert user_dict[0] == {
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy"
+    }
