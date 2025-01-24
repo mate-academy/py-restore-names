dff81bda5e0e62e70ba21161268b5f2e11f8cc17
IrynaBrenko <brenko.iryna1997@gmail.com>
2025-01-24 13:45:36 +0200

    Solutio01

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,92 @@
-import pytest
+import unittest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames(unittest.TestCase):
+    def test_restore_missing_first_name(self) -> None:
+        users = [
+            {"first_name": None,
+             "last_name": "Holy",
+             "full_name": "Jack Holy"
+             },
+            {"last_name": "Adams",
+             "full_name": "Mike Adams"
+             },
+        ]
+        expected = [
+            {"first_name": "Jack",
+             "last_name": "Holy",
+             "full_name": "Jack Holy"
+             },
+            {"first_name": "Mike",
+             "last_name": "Adams",
+             "full_name": "Mike Adams"
+             },
+        ]
+        restore_names(users)
+        self.assertEqual(users, expected)
+
+    def test_no_missing_first_name(self) -> None:
+        users = [
+            {"first_name": "Anna",
+             "last_name": "Smith",
+             "full_name": "Anna Smith"
+             },
+            {"first_name": "John",
+             "last_name": "Doe",
+             "full_name": "John Doe"
+             },
+        ]
+        expected = [
+            {"first_name": "Anna",
+             "last_name": "Smith",
+             "full_name": "Anna Smith"
+             },
+            {"first_name": "John",
+             "last_name": "Doe",
+             "full_name": "John Doe"
+             },
+        ]
+        restore_names(users)
+        self.assertEqual(users, expected)
+
+    def test_mixed_cases(self) -> None:
+        users = [
+            {"first_name": None,
+             "last_name": "Brown",
+             "full_name": "Emily Brown"
+             },
+            {"first_name": "Chris",
+             "last_name": "Evans",
+             "full_name": "Chris Evans"
+             },
+            {"last_name": "Johnson",
+             "full_name": "Emma Johnson"
+             },
+        ]
+        expected = [
+            {"first_name": "Emily",
+             "last_name": "Brown",
+             "full_name": "Emily Brown"
+             },
+            {"first_name": "Chris",
+             "last_name": "Evans",
+             "full_name": "Chris Evans"
+             },
+            {"first_name": "Emma",
+             "last_name": "Johnson",
+             "full_name": "Emma Johnson"
+             },
+        ]
+        restore_names(users)
+        self.assertEqual(users, expected)
+
+    def test_empty_users(self) -> None:
+        users = []
+        expected = []
+        restore_names(users)
+        self.assertEqual(users, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
