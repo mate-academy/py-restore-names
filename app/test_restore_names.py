0fb827a570e6a8aaae7d5ca38af01aed84260eda
Ron Phillips <mcspidy@gmail.com>
2025-01-29 14:34:59 -0600

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,36 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,result",
+    [
+        ([{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
+         ["Jack"]),
+        
+        ([{"last_name": "Adams", "full_name": "Mike Adams"}], ["Mike"]),
+        
+        ([{"first_name": "Mike", "last_name": "Adams", 
+           "full_name": "Mike Adams"}], ["Mike"]),
+        
+        ([{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"
+           }], ["Jack"]),
+        
+        ([{"full_name": "Barack Obama"},
+          {"full_name": "Donald Trump"},
+          {"first_name": None, "full_name": "George R.R. Martin"},
+          {"first_name": "Ada", "full_name": "Ada Lovelace"}],
+         ["Barack", "Donald", "George", "Ada"])
+        ],
+    ids=[
+        "first name none",
+        "first name missing",
+        "first name already present",
+        "first name already present",
+        "multiple users"
+    ]
+)
+def test_restore_names(users: list, result: set) -> None:
+    restore_names(users)
+    for idx in range(len(users)):
+        assert users[idx]["first_name"] == result[idx]
