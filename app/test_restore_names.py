a11d566a3f0148321a8440f19d79dfee6fff3a51
Ron Phillips <mcspidy@gmail.com>
2025-01-29 14:38:48 -0600

    Flake8 fix

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -8,7 +8,7 @@ from app.restore_names import restore_names
         ([{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
          ["Jack"]),
         ([{"last_name": "Adams", "full_name": "Mike Adams"}], ["Mike"]),
-        ([{"first_name": "Mike", "last_name": "Adams", 
+        ([{"first_name": "Mike", "last_name": "Adams",
            "full_name": "Mike Adams"}], ["Mike"]),
         ([{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"
            }], ["Jack"]),
