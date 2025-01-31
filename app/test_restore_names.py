f91d9556e507f612d3cae0eb6f4c0e32e3f7f264
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-31 12:23:40 +0200

    upd

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,40 +7,70 @@ from app.restore_names import restore_names
     [
         (
             [
-                {"first_name": "Jack", "last_name": "Holy", 
-                 "full_name": "Jack Holy"}
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
             ],
             [
-                {"first_name": "Jack", "last_name": "Holy", 
-                 "full_name": "Jack Holy"}
-            ]
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
         ),
         (
             [
-                {"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"}
+                {
+                    "first_name": None,
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
             ],
             [
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-            ]
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
         ),
         (
             [
-                {"first_name": "", "last_name": "Adams", "full_name": "Mike Adams"}
+                {
+                    "first_name": "",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
             ],
             [
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
-            ]
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
         ),
         (
             [
-                {"first_name": "", "last_name": "", "full_name": "Mike Adams"}
+                {
+                    "first_name": "",
+                    "last_name": "",
+                    "full_name": "Mike Adams",
+                }
             ],
             [
-                {"first_name": "Mike", "last_name": "", "full_name": "Mike Adams"}
-            ]
+                {
+                    "first_name": "Mike",
+                    "last_name": "",
+                    "full_name": "Mike Adams",
+                }
+            ],
         ),
-    ]
+    ],
 )
-def test_restore_names(input_users, expected):
+def test_restore_names(input_users: list[dict[str, str | None]], expected: list[dict[str, str]]) -> None:
     restore_names(input_users)
     assert input_users == expected
