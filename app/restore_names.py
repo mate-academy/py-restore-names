077793a6c8abd4d93a36be81d1bda087b574b62d
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-01 16:26:40 +0200

    Solution

--- app/restore_names.py
+++ app/restore_names.py
@@ -5,4 +5,3 @@ def restore_names(users: List[dict]) -> None:
     for user in users:
         if user.get("first_name") is None or user["first_name"] == "":
             user["first_name"] = user["full_name"].split()[0]
-
--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,7 +6,9 @@ def test_restore_names():
         {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
         {"last_name": "Adams", "full_name": "Mike Adams"},
         {
-            "first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"
+            "first_name": "Anna",
+            "last_name": "Smith",
+            "full_name": "Anna Smith"
         },
         {"first_name": "", "last_name": "Brown", "full_name": "Tom Brown"},
     ]
@@ -14,14 +16,17 @@ def test_restore_names():
     expected = [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
         {
-            "first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
         },
         {
-            "first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"
+            "first_name": "Anna",
+            "last_name": "Smith",
+            "full_name": "Anna Smith"
         },
         {"first_name": "Tom", "last_name": "Brown", "full_name": "Tom Brown"},
     ]
 
     restore_names(users)
     assert users == expected
-
