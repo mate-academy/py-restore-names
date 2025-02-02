f6db0e77dc3284ab0748bb3c7932c46502d7ce2d
JulianskyMe <juliakamynskaya@gmail.com>
2025-02-02 15:20:41 +0200

    upd

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,19 +5,59 @@ def restore_names(users):
         if not user.get("first_name"):
             user["first_name"] = user["full_name"].split()[0]
 
-def test_restore_names():
-    users = [
-        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-        {"last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "Alice", "last_name": "Brown", "full_name": "Alice Brown"},
-        {"first_name": None, "last_name": "Smith", "full_name": "John Smith"}
-    ]
-    expected = [
-        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "Alice", "last_name": "Brown", "full_name": "Alice Brown"},
-        {"first_name": "John", "last_name": "Smith", "full_name": "John Smith"}
-    ]
-    
+@pytest.mark.parametrize("users, expected", [
+    ([
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        },
+        {
+            "first_name": "Alice",
+            "last_name": "Brown",
+            "full_name": "Alice Brown"
+        },
+        {
+            "first_name": None,
+            "last_name": "Smith",
+            "full_name": "John Smith"
+        },
+        {
+            "last_name": "Smith",
+            "full_name": "John Smith"
+        }
+    ], [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        },
+        {
+            "first_name": "Alice",
+            "last_name": "Brown",
+            "full_name": "Alice Brown"
+        },
+        {
+            "first_name": "John",
+            "last_name": "Smith",
+            "full_name": "John Smith"
+        },
+        {
+            "first_name": "John",
+            "last_name": "Smith",
+            "full_name": "John Smith"
+        }
+    ])
+])
+def test_restore_names(users, expected):
     restore_names(users)
     assert users == expected
