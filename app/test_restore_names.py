b276d82fbd84314c204dc71f2e155fbfa5b7acf8
Nastya Kalinina <kalininan.kalinina@gmail.com>
2025-02-05 00:12:45 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,23 +4,25 @@ from app.restore_names import restore_names
 
 @pytest.mark.parametrize(
     "what_we_get, expected",
-    (
-        [
-            {"first_name": None,
-             "last_name": "Holy",
-             "full_name": "Jack Holy"},
-            {"last_name": "Adams",
-             "full_name": "Mike Adams"}
-        ],
-        [
-            {"first_name": "Jack",
-             "last_name": "Holy",
-             "full_name": "Jack Holy"},
-            {"first_name": "Mike",
-             "last_name": "Adams",
-             "full_name": "Mike Adams"}
-        ]
-    )
+    [
+        (
+            [
+                {"first_name": None,
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
+                {"last_name": "Adams",
+                 "full_name": "Mike Adams"}
+            ],
+            [
+                {"first_name": "Jack",
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
+                {"first_name": "Mike",
+                 "last_name": "Adams",
+                 "full_name": "Mike Adams"}
+            ]
+        )
+    ]
 )
 def test_restore_name(what_we_get: list[dict], expected: list[dict], ) -> None:
     restore_names(what_we_get)
