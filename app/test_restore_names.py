641c2f231ef3f6be23e25478f3d1f53d16ad2346
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-05 19:31:30 +0100

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,21 +2,28 @@ import pytest
 from app.restore_names import restore_names
 
 
-def test_restore_names():
-    users = [
-        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-        {"last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
-        {"first_name": None, "last_name": "Brown", "full_name": "Sarah Brown"},
+@pytest.mark.parametrize(
+    "what_we_get, expected",
+    [
+        (
+            [
+                {"first_name": None,
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
+                {"last_name": "Adams",
+                 "full_name": "Mike Adams"}
+            ],
+            [
+                {"first_name": "Jack",
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
+                {"first_name": "Mike",
+                 "last_name": "Adams",
+                 "full_name": "Mike Adams"}
+            ]
+        )
     ]
-
-    restore_names(users)
-
-    expected_users = [
-        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
-        {"first_name": "Sarah", "last_name": "Brown", "full_name": "Sarah Brown"},
-    ]
-
-    assert users == expected_users
+)
+def test_restore_name(what_we_get: list[dict], expected: list[dict], ) -> None:
+    restore_names(what_we_get)
+    assert what_we_get == expected
