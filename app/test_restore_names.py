d89a50c791479d61903901c36a73c5b0035c359e
Odin-max <meh84741@gmail.com>
2025-02-11 23:45:44 +0200

    4

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,7 +1,7 @@
 from app.restore_names import restore_names
 
 
-def test_restore_names():
+def test_restore_names() -> None:
     users = [
         {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
         {"last_name": "Adams", "full_name": "Mike Adams"},
@@ -15,7 +15,9 @@ def test_restore_names():
 
     expected_users = [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+        {
+        "first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"
+        },
         {
             "first_name": "Alice",
             "last_name": "Smith",
