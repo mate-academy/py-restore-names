9657bf6194cc2dcf33258d423340d98f205884e6
Bartolomeu <bartoszokroj@wp.pl>
2025-02-11 02:04:29 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,17 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_should_restore_name_if_key_first_name_does_not_exist() -> None:
+    user = {"last_name": "Belly", "full_name": "Chris Belly"}
+    restore_names([user])
+    assert user["first_name"] == "Chris"
+
+
+def test_should_restore_name_if_first_name_is_none() -> None:
+    user = {
+        "first_name": None,
+        "last_name": "Clinton",
+        "full_name": "Alan Clinton"
+    }
+    restore_names([user])
+    assert user["first_name"] == "Alan"
