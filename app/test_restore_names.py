adc34420306659b4019761874f92bee616962383
Marek Smolana <msmolana@gmail.com>
2025-02-16 13:18:51 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,24 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,exp_first_name",
+    [
+        ([{
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }], "Jack"),
+        ([{
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }], "Jack")
+    ]
+)
+def test_first_name_is_restored(
+        users: list[dict],
+        exp_first_name: str
+) -> None:
+    restore_names(users)
+    assert users[0]["first_name"] == exp_first_name
