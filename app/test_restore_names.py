4a1d48086bf00786403bb7009ce9116dfad06722
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-02-27 15:43:10 +0200

    Finish

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,27 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names() -> None:
+    users = [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"},
+        {
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "full_name": "Alice Smith"
+        },
+        {
+            "first_name": None,
+            "last_name": "Brown",
+            "full_name": "Charlie Brown"
+        },
+        {"last_name": "Williams", "full_name": "John"}
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+    assert users[2]["first_name"] == "Alice"
+    assert users[3]["first_name"] == "Charlie"
+    assert users[4]["first_name"] == "John"
