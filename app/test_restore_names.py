7df0a41994498240761072e6c8859a1b1a8eb2dd
PrimeGlorious <TheComboPunk@gmail.com>
2025-03-05 08:08:56 +0200

    Implement tests for restore_names function

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users_template() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Hello",
+            "last_name": "World",
+            "full_name": "Hello World",
+        },
+    ]
+
+
+def test_should_restore_names(users_template: list) -> None:
+    restore_names(users_template)
+    assert users_template == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Hello",
+            "last_name": "World",
+            "full_name": "Hello World",
+        },
+    ]
