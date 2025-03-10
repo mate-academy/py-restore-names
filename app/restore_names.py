6753ede4fb96695152fdb185f063beb2e17463a4
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-10 16:54:28 +0200

    Solution

--- app/restore_names.py
+++ app/restore_names.py
@@ -1,7 +1,10 @@
-from typing import List
+from typing import Dict, List, Optional
 
 
-def restore_names(users: List[dict]) -> None:
+def restore_names(users: List[Dict[str, Optional[str]]]) -> None:
+    """
+    Восстанавливает первые имена пользователей, если они отсутствуют.
+    """
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
             user["first_name"] = user["full_name"].split()[0]
--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
-import pytest
+import unittest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames(unittest.TestCase):
+    def test_restore_names(self) -> None:
+        users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": None,
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+        restore_names(users)
+        self.assertEqual(users[0]["first_name"], "Jack")
+        self.assertEqual(users[1]["first_name"], "Mike")
+
+
+if __name__ == "__main__":
+    unittest.main()
