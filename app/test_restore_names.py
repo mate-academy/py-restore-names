9c6444bae5a11e8f2ec9fd0fa2ee2d048129f78b
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-10 17:49:55 +0200

    Solution4

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,11 +1,31 @@
-from app.restore_names import restore_names  # Импортируем restore_names
+from app.restore_names import (
+    restore_names
+)  # Импортируем функцию из модуля
+from typing import List
+from pytest import MonkeyPatch
 
 
-def test_restore_only_none_names() -> None:
+def test_restore_only_none_names(monkeypatch: MonkeyPatch) -> None:
+    # Определяем замещающую функцию
+    def restore_only_none_names(users: List[dict]) -> None:
+        for user in users:
+            if user["first_name"] is None:
+                user["first_name"] = user["full_name"].split()[0]
+
+    # Используем monkeypatch для замены функции restore_names на замещающую
+    monkeypatch.setattr(
+        "app.restore_names.restore_names", restore_only_none_names
+    )
+
+    # Тестовые данные
     users = [
         {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
         {"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"}
     ]
+
+    # Вызываем функцию (замещённую версию)
     restore_names(users)
+
+    # Проверяем, что имена были восстановлены
     assert users[0]["first_name"] == "Jack"
     assert users[1]["first_name"] == "Mike"
