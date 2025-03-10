a770bfb89510973417afcc04e778ccfc1ec7dcc3
Nikita Tkachenko <7heswatgg@gmail.com>
2025-03-10 03:33:21 +0100

    fexed flake

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,19 +5,27 @@ import pytest
 @pytest.mark.parametrize("users, expected", [
     (
         [
-            {"first_name": None,
-             "last_name": "Holy",
-             "full_name": "Jack Holy"},
-            {"last_name": "Adams",
-             "full_name": "Mike Adams"},
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            },
         ],
         [
-            {"first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy"},
-            {"first_name": "Mike",
-             "last_name": "Adams",
-             "full_name": "Mike Adams"}
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }
         ]
     )
 ])
