ac8a7f0eee167eb98eadcba4dd58dcce76087cde
dima <giomozda74@gmail.com>
2025-03-12 19:10:59 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,108 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_name_all_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": None,
+            "last_name": None,
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_name_one_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adam",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_name_both_does_not_have_name() -> None:
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
+
+
+def test_restore_name_one_does_not_have_name() -> None:
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adam",
+            "full_name": "Mike Adams",
+        },
+    ]
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        }
+    ]
