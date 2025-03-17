5851348a3248f253c18bd5c30294fe3f6060d832
Platon Goncharevskyi <platongoncharevskyi@gmail.com>
2025-03-17 21:33:07 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,43 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+def test_restore_names():
+
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Anna",
+            "last_name": "Smith",
+            "full_name": "Anna Smith",
+        },
+    ]
+
+    restore_names(users)
+
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Anna",
+            "last_name": "Smith",
+            "full_name": "Anna Smith",
+        },
+    ]
+
+    assert users == expected, "Users' first names should be restored correctly"
