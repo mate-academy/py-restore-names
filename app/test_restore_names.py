cf3134defa2b124b7a7a7b0868be0d07e48de011
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-20 19:52:06 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_only_missing_names() -> None:
+    assert restore_names(
+        [
+            {
+                "last_name": "Holy",
+                "full_name": "Martin Holy",
+            }
+        ]
+    ) == (
+        [
+            {
+                "first_name": "Martin",
+                "last_name": "Holy",
+                "full_name": "Martin Holy",
+            }
+        ]
+    )
+
+
+def test_restore_only_none_names() -> None:
+    assert restore_names(
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Holy",
+            }
+        ]
+    ) == (
+        [
+            {
+                "first_name": "Mask",
+                "last_name": "Holy",
+                "full_name": "Mask Holy",
+            }
+        ]
+    )
