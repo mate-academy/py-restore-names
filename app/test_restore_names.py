548ab81cb07e9893e7d83984458708709ed16bd4
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-20 20:28:21 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,39 +2,37 @@ from app.restore_names import restore_names
 
 
 def test_restore_only_missing_names() -> None:
-    assert restore_names(
-        [
-            {
-                "last_name": "Holy",
-                "full_name": "Martin Holy",
-            }
-        ]
-    ) == (
-        [
-            {
-                "first_name": "Martin",
-                "last_name": "Holy",
-                "full_name": "Martin Holy",
-            }
-        ]
-    )
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Martin Holy",
+        }
+    ]
+    expected = [
+        {
+            "first_name": "Martin",
+            "last_name": "Holy",
+            "full_name": "Martin Holy",
+        }
+    ]
+    restore_names(users)
+    assert users == expected
 
 
 def test_restore_only_none_names() -> None:
-    assert restore_names(
-        [
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Holy",
-            }
-        ]
-    ) == (
-        [
-            {
-                "first_name": "Mask",
-                "last_name": "Holy",
-                "full_name": "Mask Holy",
-            }
-        ]
-    )
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Martin Holy",
+        }
+    ]
+    expected = [
+        {
+            "first_name": "Martin",
+            "last_name": "Holy",
+            "full_name": "Martin Holy",
+        }
+    ]
+    restore_names(users)
+    assert users == expected
