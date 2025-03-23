6faf0950d2241bb2d9e73259d17d22f6a66b0e64
Iryna <irakirvas78@gmail.com>
2025-03-23 17:24:34 +0300

    Implement py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+users = [
+    {
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    },
+    {
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    },
+    {
+        "first_name": "Monica",
+        "last_name": "Geller",
+        "full_name": "Monica Geller",
+    },
+]
+
+
+def test_restore_names_correctly_change_names() -> None:
+    restore_names(users)
+    for user in users:
+        assert user["first_name"] == user["full_name"].split()[0]
