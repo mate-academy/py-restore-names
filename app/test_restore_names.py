4624e0bafec7a20f99f358482c091a52183e16fa
Anton Vynychenko <kinid@ukr.net>
2025-03-23 20:18:23 +0200

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,9 +1,9 @@
 import pytest
-from main import restore_names
+from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def list_before() -> list:
+def list_with_none() -> list:
     return [
         {"first_name": None,
          "last_name": "Smith",
@@ -14,13 +14,23 @@ def list_before() -> list:
     ]
 
 
-def test_should_set_first_name_if_none(list_before: list) -> None:
-    restore_names(list_before)
-    assert list_before[0]["first_name"] == "John"
-    assert list_before[1]["first_name"] == "Test"
+@pytest.fixture()
+def list_with_empty() -> list:
+    return [
+        {"last_name": "Smith",
+         "full_name": "John Smith"},
+        {"last_name": "Testovich",
+         "full_name": "Test Testovich"}
+    ]
 
 
-def test_should_define_first_name_and_set(list_before: list) -> None:
-    restore_names(list_before)
-    assert list_before[0]["first_name"] == "John"
-    assert list_before[1]["first_name"] == "Test"
+def test_should_set_first_name_if_none(list_with_none: list) -> None:
+    restore_names(list_with_none)
+    assert list_with_none[0]["first_name"] == "John"
+    assert list_with_none[1]["first_name"] == "Test"
+
+
+def test_should_define_first_name_and_set(list_with_empty: list) -> None:
+    restore_names(list_with_empty)
+    assert list_with_empty[0]["first_name"] == "John"
+    assert list_with_empty[1]["first_name"] == "Test"
