05356e69f9784eecfa8a33ced2df1d14d1db17e6
Anton Vynychenko <kinid@ukr.net>
2025-03-23 20:14:22 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
 import pytest
-from app.restore_names import restore_names
+from main import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def list_before() -> list:
+    return [
+        {"first_name": None,
+         "last_name": "Smith",
+         "full_name": "John Smith"},
+        {"first_name": None,
+         "last_name": "Testovich",
+         "full_name": "Test Testovich"}
+    ]
+
+
+def test_should_set_first_name_if_none(list_before: list) -> None:
+    restore_names(list_before)
+    assert list_before[0]["first_name"] == "John"
+    assert list_before[1]["first_name"] == "Test"
+
+
+def test_should_define_first_name_and_set(list_before: list) -> None:
+    restore_names(list_before)
+    assert list_before[0]["first_name"] == "John"
+    assert list_before[1]["first_name"] == "Test"
