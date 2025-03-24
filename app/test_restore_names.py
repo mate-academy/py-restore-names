a3752c1b4c8f8d3a7b1ec8f009fe00f7d134ec34
Windings-Lab <janton20001@gmail.com>
2025-03-24 19:10:44 +0200

    Anton Zhukov - Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,53 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": None,
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ]
+        ),
+    ]
+)
+def test_should_set_first_name_if_none(users: list[dict]) -> None:
+    restore_names(users)
+
+    for user in users:
+        assert user["first_name"] is not None
+
+
+@pytest.mark.parametrize(
+    "users",
+    [
+        (
+            [
+                {
+                    "last_name": "Jam",
+                    "full_name": "Sam Jam",
+                },
+                {
+                    "last_name": "Rem",
+                    "full_name": "Dean Rem",
+                },
+            ]
+        ),
+    ]
+)
+def test_should_define_first_name_and_set(users: list[dict]) -> None:
+    restore_names(users)
+
+    for user in users:
+        assert "first_name" in user
