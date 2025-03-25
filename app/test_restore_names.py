4bdc8539a3a854d98a658587aa7aecdf451da1ca
Viktor Stadnik <stadhik.victor@gmail.com>
2025-03-25 11:26:35 +0200

    implemented 'py-restore-names'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames:
+    @pytest.mark.parametrize(
+        "users, expected",
+        [
+            ([{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            }], "Jack"),
+            ([{
+                "last_name": "Adams", "full_name": "Mike Adams"
+            }], "Mike"),
+        ],
+        ids=[
+            "First name is None",
+            "First name is empty",
+        ]
+    )
+    def test_restore_empty_names(self, users: list, expected: str) -> None:
+        restore_names(users)
+
+        assert users[0]["first_name"] == expected
