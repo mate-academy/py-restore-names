977a4c758f8c6dc39733b912b6b8c7066b89e8db
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-04-01 18:45:01 +0200

    test: create test cases for restore_names func

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
 from app.restore_names import restore_names
+from copy import deepcopy
 
-# write your tests here
+
+@pytest.fixture(scope="function")
+def get_users_list() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+class TestRestoreNames:
+    def test_restore_names_func(self, get_users_list: list) -> None:
+        users = deepcopy(get_users_list)
+        restore_names(users)
+
+        if get_users_list[0]["first_name"] is None:
+            assert users[0]["first_name"] == "Jack"
+
+        if "first_name" not in get_users_list[1]:
+            assert users[1]["first_name"] == "Mike"
+
+    def test_restore_names_return_none(self, get_users_list: list) -> None:
+        assert restore_names(get_users_list) is None
