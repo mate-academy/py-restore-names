1fa2046846c391ab8da689edf30b3648ab26e28b
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-01 16:06:10 +0300

    Added tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,51 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize("users,expected", [
+    pytest.param(
+        [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ], [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ],
+        id="Add first name if it is None"
+    ),
+    pytest.param(
+        [
+            {
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ], [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ],
+        id="Add first name if it is not there"
+    ),
+])
+def test_restore_names(users: list, expected: list) -> None:
+    restore_names(users)
+    assert users == expected
+
+
+def test_restore_names_return_none() -> None:
+    assert (
+        restore_names([
+            {
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }
+        ]) is None
+    ), "Should return None"
