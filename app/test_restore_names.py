09d7e04833d178468c974cff45a0434db7592b37
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 14:27:49 +0300

    test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,29 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names() -> None:
+    users_1 = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    users_2 = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users_1)
+    assert users_1 == users_2
