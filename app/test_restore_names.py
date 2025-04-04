733cb9b5daacc10fb7a2b718e3869fdde99e871f
oleanto <olegantonenko100@gmail.com>
2025-04-04 15:00:59 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,16 +5,21 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users,expected", [
         (
-            [{"first_name": None, "last_name": "Roberts", "full_name": "Julia Roberts"}],
-            [{"first_name": "Julia", "last_name": "Roberts", "full_name": "Julia Roberts"}],
+            [{"first_name": None, "last_name": "Roberts",
+              "full_name": "Julia Roberts"}],
+            [{"first_name": "Julia", "last_name": "Roberts",
+              "full_name": "Julia Roberts"}],
         ),
         (
             [{"last_name": "Walker", "full_name": "Paul Walker"}],
-            [{"first_name": "Paul", "last_name": "Walker", "full_name": "Paul Walker"}],
+            [{"first_name": "Paul", "last_name": "Walker",
+              "full_name": "Paul Walker"}],
         ),
         (
-            [{"first_name": "Luke", "last_name": "Skywalker", "full_name": "Luke Skywalker"}],
-            [{"first_name": "Luke", "last_name": "Skywalker", "full_name": "Luke Skywalker"}],
+            [{"first_name": "Luke", "last_name": "Skywalker",
+              "full_name": "Luke Skywalker"}],
+            [{"first_name": "Luke", "last_name": "Skywalker",
+              "full_name": "Luke Skywalker"}],
         )
     ]
 )
