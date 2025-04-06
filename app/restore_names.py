82774110ac98d8f919d7883c4e08594f9ac9f41c
Marcin <grzechnikmarcin@wp.pl>
2025-04-06 21:09:12 +0200

    My solution

--- app/restore_names.py
+++ app/restore_names.py
@@ -4,4 +4,6 @@ from typing import List
 def restore_names(users: List[dict]) -> None:
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
-            user["first_name"] = user["full_name"].split()[0]
+            full_name_parts = user["full_name"].split()
+            if full_name_parts:  # Sprawdza, czy lista nie jest pusta
+                user["first_name"] = full_name_parts[0]
