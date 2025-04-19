a4ef2f5bde42aeba9175231f8767ef2529d2a6d0
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-19 20:30:07 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "initial_list, adding_key, expected_value",
+    [
+        (
+            pytest.param(
+                [{"full_name": "Sherlock Holmes"}],
+                "first_name",
+                "Sherlock",
+                id="missing first name"
+            )
+        ),
+        (
+            pytest.param(
+                [{"first_name": None,
+                  "full_name": "Sherlock Holmes"}],
+                "first_name",
+                "Sherlock",
+                id="none first name"
+            )
+        ),
+        (
+            pytest.param(
+                [{"first_name": "Sherlok",
+                  "full_name": "Sherlock Holmes"}],
+                "first_name",
+                "Sherlok",
+                id="existing first name"
+            )
+        )
+    ]
+)
+def test_restore_names(
+        initial_list: list[dict],
+        adding_key: str,
+        expected_value: str) -> None:
+    restore_names(initial_list)
+    assert initial_list[0][adding_key] == expected_value
