c0e0a6ce4642c89983c3ca8c704ebb06c82d4fd5
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-04-27 17:55:27 +0200

    Add parameterized tests for restore_names function

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,19 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "user_list,result_name",
+    [
+        pytest.param([{
+            "last_name": "Adams", "full_name": "John Adams"}],
+            "John", id="first_name_does_not_exist"),
+        pytest.param([{
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"}],
+            "Jack", id="first_name_is_None")
+    ])
+def test_restore_names(user_list: list[dict], result_name: str) -> None:
+    restore_names(user_list)
+    assert user_list[0]["first_name"] == result_name
