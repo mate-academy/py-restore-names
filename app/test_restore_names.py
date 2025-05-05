888f1e121691018bb640f2b5a36820bf30e4f54c
Bogdan0101 <kononovb777@gmail.com>
2025-05-05 20:43:31 +0300

    Solution#2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,38 +1,38 @@
 import pytest
 from app.restore_names import restore_names
 
+
 class TestModify:
     @pytest.mark.parametrize(
         "users, result_users",
         [
             pytest.param(
-            [
-                {
-                    "first_name": None,
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            [
-                {
-                    "first_name": "Jack",
-                    "last_name": "Holy",
-                    "full_name": "Jack Holy",
-                },
-                {
-                    "first_name": "Mike",
-                    "last_name": "Adams",
-                    "full_name": "Mike Adams",
-                },
-            ],
-            id="Test1"
-        ),
+                [
+                    {
+                        "first_name": None,
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    },
+                    {
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams",
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy",
+                    },
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams",
+                    }
+                ],
+                id="Test1"
+            ),
         ])
-    def test_modify(self, users, result_users):
+    def test_modify(self, users: list[dict], result_users: list[dict]) -> None:
         restore_names(users)
         assert users == result_users
-
