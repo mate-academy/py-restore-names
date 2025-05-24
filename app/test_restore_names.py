7ce49a3bd53146ed6cc9d0febbba44550c89102a
Viktor Velychkocd <velviktor831@gmail.com>
2025-05-24 11:24:39 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,77 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected_first_names",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+                {
+                    "first_name": "Anna",
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith",
+                },
+            ],
+            ["Jack", "Mike", "Anna"],
+        ),
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": None,
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith",
+                },
+            ],
+            ["Jack", "Anna"],
+        ),
+        (
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+                {
+                    "last_name": "Brown",
+                    "full_name": "Lisa Brown",
+                },
+            ],
+            ["Mike", "Lisa"],
+        ),
+        (
+            [
+                {
+                    "first_name": "Sam",
+                    "last_name": "Green",
+                    "full_name": "Sam Green",
+                },
+                {
+                    "first_name": "Liz",
+                    "last_name": "Blue",
+                    "full_name": "Liz Blue",
+                },
+            ],
+            ["Sam", "Liz"],
+        ),
+    ],
+)
+def test_restore_names(
+    users: list[dict[str, str | None]], expected_first_names: list[str]
+) -> None:
+    restore_names(users)
+    for user, expected_name in zip(users, expected_first_names):
+        assert user["first_name"] == expected_name
