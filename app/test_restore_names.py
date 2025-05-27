44a2234060390be4f9fb79c4be7277898de1cae6
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 19:06:12 +0300

    Need help

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,11 +3,11 @@ from typing import Callable
 from app.restore_names import restore_names
 
 
-def test_restore_names_without_name(user_info: Callable) -> None:
+def test_restore_names_without_name() -> None:
     user = {"last_name": "Doe", "full_name": "Jack Holy"}
-    assert restore_names(user_info(user)) == "first_name: ""Jack"
+    assert restore_names(user) == "first_name: ""Jack"
 
 
-def test_restore_names_when_name_is_none(user_info: Callable) -> None:
+def test_restore_names_when_name_is_none() -> None:
     user = {"first_name": None, "last_name": "Doe", "full_name": "Jack Holy"}
-    assert restore_names(user_info(user)) == "first_name: ""Jack"
+    assert restore_names(user) == "first_name: ""Jack"
