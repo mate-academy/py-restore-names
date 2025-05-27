284c070cfd3a711be68ce9b8b2b9c3ec5e69041b
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 18:18:48 +0300

    Need help

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,17 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_info():
+    return User(
+        last_name="Holy",
+        full_name="Jack Holy",
+    )
+
+def test_restore_names_without_name(user_info):
+    assert restore_names(user_info) == "first_name: ""Jack"
+
+def test_restore_names_when_name_is_None(user_info):
+    User.first_name = None
+    assert restore_names(user_info) == "first_name: ""Jack"
