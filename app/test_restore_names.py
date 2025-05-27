272217223fcac14c3097272b06a0a3f8dcbd509a
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 18:25:59 +0300

    Need help

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,17 +1,21 @@
+from typing import Callable
 import pytest
+
 from app.restore_names import restore_names
 
 
 @pytest.fixture()
-def user_info():
-    return User(
+def user_info() -> dict:
+    return user(
         last_name="Holy",
         full_name="Jack Holy",
     )
 
-def test_restore_names_without_name(user_info):
+
+def test_restore_names_without_name(user_info: Callable) -> None:
     assert restore_names(user_info) == "first_name: ""Jack"
 
-def test_restore_names_when_name_is_None(user_info):
-    User.first_name = None
+
+def test_restore_names_when_name_is_none(user_info: Callable) -> None:
+    user.first_name = None
     assert restore_names(user_info) == "first_name: ""Jack"
