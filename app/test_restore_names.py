19e2851e5cf27c98cc991a0da31d80e1e5bd07e0
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 19:09:49 +0300

    Need help

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,11 +5,11 @@ def test_restore_names_without_name() -> None:
     user = [
         {"last_name": "Doe", "full_name": "Jack Holy"},
     ]
-    assert restore_names(user) == "first_name: ""Jack"
+    assert restore_names(user) == "first_name: Jack"
 
 
 def test_restore_names_when_name_is_none() -> None:
     user = [
         {"first_name": None, "last_name": "Doe", "full_name": "Jack Holy"},
     ]
-    assert restore_names(user) == "first_name: ""Jack"
+    assert restore_names(user) == "first_name: Jack"
