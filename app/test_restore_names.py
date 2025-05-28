76808e3fd860469bd07ba1bcaa80a335385f517e
Vitalii <vitaliy.honchar@gmail.com>
2025-05-28 23:54:10 +0300

    Solution2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,29 +1,35 @@
 import pytest
 from app.restore_names import restore_names
 
-def test_restore_names():
-    users = [
-        {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        },
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
-    ]
 
-    restore_names(users)
-    assert users == [
-        {
-            "first_name": "Jack",
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        },
-        {
-            "first_name": "Mike",
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        },
+@pytest.mark.parametrize(
+    "input_list,out_list",
+    [
+        (
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            [{
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }]
+        ),
+        (
+            [{
+                "last_name": "Lite",
+                "full_name": "Eva Lite",
+            }],
+            [{
+                "first_name": "Eva",
+                "last_name": "Lite",
+                "full_name": "Eva Lite",
+            }]
+        )
     ]
+)
+def test_restore_names(input_list: list, out_list):
+    restore_names(input_list)
+    assert input_list == out_list
