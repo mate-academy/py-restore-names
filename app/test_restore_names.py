2546e3aebf8ab7c7f9075c46296de0edb83596bc
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-06-02 20:19:17 +0200

    task

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,2 +1,18 @@
 import pytest
 from app.restore_names import restore_names
+
+
+@pytest.fixture
+def sample_users() -> list[dict]:
+    return [
+        {"full_name": "Иван Иванов"},
+        {"full_name": "Анна Смирнова", "first_name": None},
+        {"full_name": "Петр Петров", "first_name": "Петя"}
+    ]
+
+
+def test_restore_names(sample_users: list[dict]) -> None:
+    restore_names(sample_users)
+    assert sample_users[0]["first_name"] == "Иван"
+    assert sample_users[1]["first_name"] == "Анна"
+    assert sample_users[2]["first_name"] == "Петя"
