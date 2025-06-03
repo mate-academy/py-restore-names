e1548cd2f37f6a096e26e31cd38165aece9c2358
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-06-03 22:07:53 +0200

    restore name test

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,19 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names_example():
+    users = [
+        {"first_name": None, "last_name": "Holy",
+         "full_name": "Jack Holy"},
+        {"last_name": "Adams",
+         "full_name": "Mike Adams"}
+    ]
+    expected = [
+        {"first_name": "Jack", "last_name": "Holy",
+         "full_name": "Jack Holy"},
+        {"first_name": "Mike", "last_name": "Adams",
+         "full_name": "Mike Adams"}
+    ]
+    restore_names(users)
+    assert users == expected
