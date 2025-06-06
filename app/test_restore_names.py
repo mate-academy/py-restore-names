8a6ef1182b919174cce885a53078eb39b06a541f
nkkmkk <nikitmkk@gmail.com>
2025-06-06 20:31:13 +0200

    Test added

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,25 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "user, expected",
+    [
+        (
+            {
+                "first_name": None,
+                "full_name": "Jack Holy"
+            },
+            "Jack"
+        ),
+        (
+            {
+                "full_name": "Mike Adams"
+            },
+            "Mike"
+        )
+    ]
+)
+def test_restore_names(user: dict, expected: str) -> None:
+    restore_names([user])
+    assert user["first_name"] == expected
