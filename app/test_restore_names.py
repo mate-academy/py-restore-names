3a65e6bfff7ad5e733ca7a8b28a3283492113dd2
kbatrakov <kirillbatrakov0819@gmail.com>
2025-06-14 07:38:18 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def data_set_for_testing() -> list[dict]:
+    data = [
+        {
+            "first_name": None,
+            "last_name": "Owen",
+            "full_name": "Clive Owen",
+        },
+        {
+            "last_name": "Jones",
+            "full_name": "Jon Jones",
+        },
+    ]
+    return data
+
+
+def test_restore_names_function(data_set_for_testing: list[dict]) -> None:
+    restore_names(data_set_for_testing)
+    assert data_set_for_testing == [
+        {
+            "first_name": "Clive",
+            "last_name": "Owen",
+            "full_name": "Clive Owen",
+        },
+        {
+            "first_name": "Jon",
+            "last_name": "Jones",
+            "full_name": "Jon Jones",
+        },
+    ]
