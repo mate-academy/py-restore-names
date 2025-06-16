d740d67a0e9342d8d716096219b457fe52445584
kravetst <taraskravetc28@gmail.com>
2025-06-16 12:36:33 +0300

    test: add test for restoring first_name from full_name

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_users, expected_users",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="should produce the correct list as a result"
+        )
+    ]
+)
+def test_input_users(input_users: list, expected_users: list) -> None:
+    restore_names(input_users)
+    assert input_users == expected_users
