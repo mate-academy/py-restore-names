4b4166be4ba998ad664353ddac84a7cc69e9ccb5
Maksym Subchak <donjkee2@gmail.com>
2025-06-16 15:22:35 +0300

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,23 +1,31 @@
 import pytest
 from app.restore_names import restore_names
 
+
 @pytest.mark.parametrize(
     "users, expected",
     [
         (
             [
-                {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+                {"first_name": None,
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
             ],
             [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+                {"first_name": "Jack",
+                 "last_name": "Holy",
+                 "full_name": "Jack Holy"},
             ],
         ),
         (
             [
-                {"last_name": "Adams", "full_name": "John Adams"},
+                {"last_name": "Adams",
+                 "full_name": "John Adams"},
             ],
             [
-                {"first_name": "John", "last_name": "Adams", "full_name": "John Adams"},
+                {"first_name": "John",
+                 "last_name": "Adams",
+                 "full_name": "John Adams"},
             ],
         ),
     ],
