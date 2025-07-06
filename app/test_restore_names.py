53ca5bffab7e473c5065667e1b2f24064a12f046
Artem Shlychkin <artemshlychkin1995@gmail.com>
2025-07-06 20:32:58 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+@pytest.mark.parametrize("input_users, expected_users", [
+    (
+        [  # first_name empty
+            {"last_name": "White", "full_name": "Alice White"},
+            {"last_name": "Brown", "full_name": "Chris Brown"},
+        ],
+        [
+            {"first_name": "Alice", "last_name": "White", "full_name": "Alice White"},
+            {"first_name": "Chris", "last_name": "Brown", "full_name": "Chris Brown"},
+        ]
+    ),
+])
+def test_restore_only_missing_names(input_users, expected_users):
+    restore_names(input_users)
+    assert input_users == expected_users, "Tests should check function with users whose first_name is missing"
+
+
+@pytest.mark.parametrize("input_users, expected_users", [
+    (
+        [  # first_name = None
+            {"first_name": None, "last_name": "Green", "full_name": "Lana Green"},
+            {"first_name": None, "last_name": "Black", "full_name": "Derek Black"},
+        ],
+        [
+            {"first_name": "Lana", "last_name": "Green", "full_name": "Lana Green"},
+            {"first_name": "Derek", "last_name": "Black", "full_name": "Derek Black"},
+        ]
+    ),
+])
+def test_restore_only_none_names(input_users, expected_users):
+    restore_names(input_users)
+    assert input_users == expected_users, "Tests should check function with users whose first_name is equal to None"
