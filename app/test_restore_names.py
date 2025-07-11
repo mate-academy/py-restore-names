079b533939fcd03ee0a37d2804afb51363acb2e3
Serhii Basok <sergeybsk19@gmail.com>
2025-07-12 00:06:29 +0300

    create test for restore_names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,16 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "param, count",
+    [
+        (
+            [{ "first_name": None,  "full_name": "Jack Holy"}],
+            [{ "first_name": "Jack",  "full_name": "Jack Holy"}]
+        )
+    ]
+)
+def test_restore_names_function(param: list, count: list) -> None:
+    restore_names(param)
+    assert param == count
