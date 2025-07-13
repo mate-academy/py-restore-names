0dcc8ff930ad5caa1513a2f34f60b66cc13b175f
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-07-13 11:08:32 +0300

    my-solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected_users",
+    [
+        (
+            [{"first_name": None, "full_name": "Jack Holy"}],
+            [{"first_name": "Jack", "full_name": "Jack Holy"}]
+        ),
+        (
+            [{"full_name": "Mike Adams"}],
+            [{"first_name": "Mike", "full_name": "Mike Adams"}]
+        ),
+        ([], [])
+    ],
+    ids=[
+        "first name is None",
+        "first name is absent in the dict",
+        "empty lists"
+    ]
+)
+def test_restore_names(users: list, expected_users: list) -> None:
+    restore_names(users)
+    assert users == expected_users
