1b3ffa8fcfd585809914d474703256174cad73f6
Mykola Totsevych <totsevich02@gmail.com>
2025-07-14 21:09:04 +0300

    Added solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,98 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "incoming_data,expected_result",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Johnson",
+                    "full_name": "Anna Johnson",
+                },
+                {
+                    "last_name": "Brown",
+                    "full_name": "Emma Brown",
+                }
+            ],
+            [
+                {
+                    "first_name": "Anna",
+                    "last_name": "Johnson",
+                    "full_name": "Anna Johnson",
+                },
+                {
+                    "first_name": "Emma",
+                    "last_name": "Brown",
+                    "full_name": "Emma Brown",
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "first_name": "Bob",
+                    "last_name": "Smith",
+                    "full_name": "Bob Smith",
+                },
+                {
+                    "first_name": "George",
+                    "last_name": "Clooney",
+                    "full_name": "George Clooney",
+                }
+            ],
+            [
+                {
+                    "first_name": "Bob",
+                    "last_name": "Smith",
+                    "full_name": "Bob Smith",
+                },
+                {
+                    "first_name": "George",
+                    "last_name": "Clooney",
+                    "full_name": "George Clooney",
+                }
+            ]
+        )
+    ]
+)
+def test_should_return_dict_with_correct_first_name_key(
+    incoming_data: list,
+    expected_result: list
+) -> None:
+    restore_names(incoming_data)
+    assert incoming_data == expected_result
