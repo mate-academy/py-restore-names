2a3a569fd8d66d6b298ca1b463fd87e888d6c46d
Vadym <kirilkovadim193@gmail.com>
2025-07-14 17:11:07 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,87 @@
-import pytest
 from app.restore_names import restore_names
+import pytest
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "input_users, expected_users",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
+        ),
+        (
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
+        ),
+        (
+            [], []
+        ),
+        (
+            [
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
+            ]
+        ),
+    ],
+    ids=[
+        "test if first_name is None",
+        "test if first_name already correct",
+        "test if first_name differs from full_name",
+        "test if empty list",
+        "test if first_name not defined"
+    ]
+)
+def test_restore_names(
+        input_users: list,
+        expected_users: list) -> None:
+    restore_names(input_users)
+    assert input_users == expected_users
