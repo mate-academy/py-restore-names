05d07a4b9ff193e7f420fc3cf2516d8af3097356
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-15 11:48:51 +0300

    fix: compled restore names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,13 +1,13 @@
 from app.restore_names import restore_names
 
 
-def test_restore_names():
+def test_restore_names() -> None:
     users = [
         {
-        "first_name": None,
-        "last_name": "Holy",
-        "full_name": "Jack Holy",
-    },
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
         {
             "last_name": "Adams",
             "full_name": "Mike Adams",
