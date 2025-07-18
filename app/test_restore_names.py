88c39a3e1af0204ead24fd632b1fc7bfac0ef43c
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-07-18 15:11:09 +0200

    all

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,36 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_without_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+
+
+def test_full_name() -> None:
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
