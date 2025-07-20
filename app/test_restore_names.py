3f45a2a6d6e5f1fb45cf52f281999fb69a73e94a
helix09825 <maksilczuk@gmail.com>
2025-07-21 00:14:58 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
 import pytest
+import copy
+from typing import Any
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> list:
+    users = [{
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy", } , {
+        "last_name": "Adams",
+        "full_name": "Mike Adams", } ,
+    ]
+    return copy.deepcopy(users)
+
+
+def test_restore_names_when_none(user_template: Any) -> None:
+    restore_names(user_template)
+    assert user_template[0]["first_name"] == "Jack"
+
+
+def test_restore_names_when_nothing(user_template: Any) -> None:
+    restore_names(user_template)
+    assert user_template[1]["first_name"] == "Mike"
