d1035c30a11fa09d6f3d5ac98228750226531fee
Dasha <dariia.leshchenko@gmail.com>
2025-08-02 13:53:25 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,73 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_should_handle_absence_off_first_name() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Anna Adams",
+        }
+    ]
+    restore_names(users)
+    expected_list = [
+        {
+            "first_name": "Anna",
+            "last_name": "Adams",
+            "full_name": "Anna Adams",
+        }
+    ]
+    assert users == expected_list
+
+
+def test_should_handle_none_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Berry",
+            "full_name": "Holly Berry",
+        }
+    ]
+    restore_names(users)
+    expected_list = [
+        {
+            "first_name": "Holly",
+            "last_name": "Berry",
+            "full_name": "Holly Berry",
+        }
+    ]
+    assert users == expected_list
+
+
+def test_should_handle_multiple_users() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Lesh",
+            "full_name": "Dash Lesh",
+        },
+        {
+            "last_name": "Tkal",
+            "full_name": "Illya Tkal",
+        }
+    ]
+    restore_names(users)
+    expected_list = [
+        {
+            "first_name": "Dash",
+            "last_name": "Lesh",
+            "full_name": "Dash Lesh",
+        },
+        {
+            "first_name": "Illya",
+            "last_name": "Tkal",
+            "full_name": "Illya Tkal",
+        }
+    ]
+    assert users == expected_list
+
+
+def test_should_handle_empty_list() -> None:
+    users = []
+    restore_names(users)
+    expected_list = []
+    assert users == expected_list
