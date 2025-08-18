9b342e9fcdcfedc9729fa840bdf8e070e6c776fd
Tomasz Herman <tomekh2015@gmail.com>
2025-08-18 15:41:19 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,45 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
+            [
+                {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+                {"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"},
+            ],
+            [
+                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+            ],
+        ),
+        (
+            [
+                {"last_name": "Holy", "full_name": "Jack Holy"},
+                {"last_name": "Adams", "full_name": "Mike Adams"},
+            ],
+            [
+                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+            ],
+        ),
+        (
+            [
+                {"first_name": None, "last_name": "Smith", "full_name": "John Smith"},
+                {"first_name": "Emily", "last_name": "Jones", "full_name": "Emily Jones"},
+                {"last_name": "Davis", "full_name": "Mark Davis"},
+            ],
+            [
+                {"first_name": "John", "last_name": "Smith", "full_name": "John Smith"},
+                {"first_name": "Emily", "last_name": "Jones", "full_name": "Emily Jones"},
+                {"first_name": "Mark", "last_name": "Davis", "full_name": "Mark Davis"},
+            ],
+        ),
+        ([], []),
+    ],
+)
+def test_restore_names(users, expected):
+    restore_names(users)
+    assert users == expected
