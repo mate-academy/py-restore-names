768564390e30089449a3775e98ec3a52ba0ab2fd
Yaroslav Matviienko <volodimirovicaroslav7@gmail.com>
2025-08-24 20:33:26 +0200

    Tests for func

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,18 +7,41 @@ from app.restore_names import restore_names
     "users,expected",
     [
         pytest.param(
-            [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
-            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            }],
+            [{
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy"
+            }],
             id="test_if_first_name_is_None"
         ),
         pytest.param(
-            [{"last_name": "Adams", "full_name": "Mike Adams"}],
-            [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}],
+            [{
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }],
+            [{
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            }],
             id="test_if_first_name_is_missing"
         ),
         pytest.param(
-            [{"first_name": "Harry", "last_name": "Potter", "full_name": "Harry Potter"}],
-            [{"first_name": "Harry", "last_name": "Potter", "full_name": "Harry Potter"}],
+            [{
+                "first_name": "Harry",
+                "last_name": "Potter",
+                "full_name": "Harry Potter"
+            }],
+            [{
+                "first_name": "Harry",
+                "last_name": "Potter",
+                "full_name": "Harry Potter"
+            }],
             id="test_if_all_info_is_valid"
         )
     ]
