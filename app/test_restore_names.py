b35764796f51610d5d0a3cf2b214fd8b0cc2cf02
Yaroslav Matviienko <volodimirovicaroslav7@gmail.com>
2025-08-24 20:31:03 +0200

    Tests for func

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,28 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,expected",
+    [
+        pytest.param(
+            [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+            id="test_if_first_name_is_None"
+        ),
+        pytest.param(
+            [{"last_name": "Adams", "full_name": "Mike Adams"}],
+            [{"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}],
+            id="test_if_first_name_is_missing"
+        ),
+        pytest.param(
+            [{"first_name": "Harry", "last_name": "Potter", "full_name": "Harry Potter"}],
+            [{"first_name": "Harry", "last_name": "Potter", "full_name": "Harry Potter"}],
+            id="test_if_all_info_is_valid"
+        )
+    ]
+)
+def test_(users: list[dict], expected: dict) -> None:
+    restore_names(users)
+    assert users == expected
