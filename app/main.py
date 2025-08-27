2bf797e317171b629142771ef2f628b78b2614d0
Samuel Yudi <toyotaa038@gmail.com>
2025-08-27 18:59:56 -0300

    1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from typing import List, Dict
 
 def restore_names(users: List[Dict]) -> None:
     """
-    Restaura o first_name dos usuários a partir do full_name caso esteja ausente
+    Restaura o first_name dos usuários
+    a partir do full_name
+    caso esteja ausente
     ou seja None.
     """
     for user in users:
