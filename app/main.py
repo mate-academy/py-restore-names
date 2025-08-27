4842e5b69f3571d1f99c68f088fbc091cdec6184
Samuel Yudi <toyotaa038@gmail.com>
2025-08-27 18:58:44 -0300

    q

--- app/main.py
+++ app/main.py
@@ -14,8 +14,7 @@ def restore_names(users: List[Dict]) -> None:
                 user["first_name"] = full_name.split()[0]
 
         # Caso a chave first_name exista mas seja None
-        elif user["first_name"] is None:
+        elif user.get("first_name") is None:
             full_name = user.get("full_name", "")
             if full_name:
                 user["first_name"] = full_name.split()[0]
-
