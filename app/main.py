50a20fb6123938033f60f3033fde9c69cc2ac059
Samuel Yudi <toyotaa038@gmail.com>
2025-08-27 18:41:04 -0300

    aa

--- app/main.py
+++ app/main.py
@@ -1,2 +1,5 @@
-def restore_names():
-    return None
+def restore_names(users: list[dict]) -> None:
+    """Restaura o first_name dos usuários a partir do full_name se estiver ausente ou None."""
+    for user in users:
+        if "first_name" not in user or user["first_name"] is None:
+            user["first_name"] = user["full_name"].split()[0]
