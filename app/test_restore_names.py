ecc0ce477281dc1c4ed87928928e2624f270a398
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-09-07 19:48:06 -0300

    resolution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -50,12 +50,13 @@ def test_logic_restore_names(users: list, result: str) -> None:
     restore_names(users)
     assert users[0]["first_name"] == result
 
+
 def test_without_complete_name() -> None:
     user = [
-                {
-                    "first_name": None,
-                    "last_name": "Adams",
-                }
-            ]
+        {
+            "first_name": None,
+            "last_name": "Adams",
+        }
+    ]
     with pytest.raises(KeyError):
         restore_names(user)
