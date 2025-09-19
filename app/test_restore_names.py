e99856abfae35c459a86fe3e3c4550b744ab6f41
Radoslaw Ptak <radek1026@interi.pl>
2025-09-20 00:09:36 +0200

    Change in implement py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,15 +6,21 @@ from app.restore_names import restore_names
     "first_name, last_name, result",
     [
         (None, "Holy", "Jack Holy"),
-        ("", "Adams", "Mike Adams"),
+        (None, "Adams", "Mike Adams"),
     ]
 )
 def test_restore_names(first_name: str, last_name: str, result: str) -> None:
 
-    users = [{
-        "first_name": first_name,
-        "last_name": last_name,
-        "full_name": result
-    }]
+    users = [
+        {
+            "first_name": first_name,
+            "last_name": last_name,
+            "full_name": result},
+        {
+            "last_name": last_name,
+            "full_name": result
+        }
+    ]
     restore_names(users)
     assert result.split()[0] == users[0]["first_name"]
+    assert result.split()[0] == users[1]["first_name"]
