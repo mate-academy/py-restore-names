cbad0b40762731168e8c41d5c70d6e073b7abfad
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-09-21 08:35:52 +0300

    Complete

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,18 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Kiporenko",
+            "full_name": "Maxym Kiporenko"
+        },
+        {
+            "last_name": "Pustovit",
+            "full_name": "Natalia Pustovit"
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Maxym"
+    assert users[1]["first_name"] == "Natalia"
