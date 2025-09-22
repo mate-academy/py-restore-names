1b7d4f4285e361b464e4cbff73b737481c064664
Gena <gena.anzin@ukr.net>
2025-09-22 10:52:35 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,83 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names_without_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": None,
+            "last_name": None,
+            "full_name": "Mike Adams",
+        }
+    ]
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_names_without_last_name() -> None:
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": None,
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": None,
+        }
+    ]
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_restore_without_full_name() -> None:
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": None,
+        },
+        {
+            "first_name": "Mike",
+            "last_name": None,
+            "full_name": "Mike Adams",
+        }
+    ]
+    assert restore_names(users) == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
