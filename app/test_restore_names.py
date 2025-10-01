14b4ffeb72d75650a4f5b1ea4a7e27a4517e7710
Gabriel <gfraycastilho@gmail.com>
2025-10-01 01:12:15 -0300

    solved

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,51 +1,44 @@
 import pytest
 from app.restore_names import restore_names
 
-@pytest.mark.parametrize("users, expected", [
-    (
-        # input
-        [
-            {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}
-        ],
-        # output esperado
-        [
-            {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
-        ]
-    ),
-    (
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
             # input
-            [
-                {"first_name": "", "last_name": "Holy", "full_name": "Jack Holy"}
-            ],
+            [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
             # output esperado
-            [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
-            ]
-    ),
-    (
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+        ),
+        (
             # input
-            [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
-            ],
+            [{"first_name": "", "last_name": "Holy", "full_name": "Jack Holy"}],
             # output esperado
-            [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
-            ]
-    ),
-])
-def test_restore_names(users, expected) -> None:
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+        ),
+        (
+            # input
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+            # output esperado
+            [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}],
+        ),
+    ],
+)
+def test_restore_names(users: list[dict], expected: list[dict]) -> None:
     restore_names(users)
     assert users == expected
 
 
-@pytest.mark.parametrize("bad_input",
-                         [
-                             3.5,
-                             10,
-                             [1],
-                             {"word": 10}
-                         ]
-                         )
-def test_restore_names_non_dict_raises(bad_input: object):
+@pytest.mark.parametrize(
+    "bad_input",
+    [
+        3.5,
+        10,
+        [1],
+        {"word": 10},
+    ],
+)
+def test_restore_names_non_dict_raises(bad_input: object) -> None:
     with pytest.raises(AttributeError):
         restore_names(bad_input)
