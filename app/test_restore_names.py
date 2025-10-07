c24400783b3e4ee1024fa42bde65cb36c8c49c78
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-10-07 20:12:01 +0200

    first!

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,59 @@
+from typing import List
+
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def user_list_template_with_no_fn() -> List[dict]:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+
+
+@pytest.fixture
+def user_list_template_with_fn() -> List[dict]:
+    users = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+    return users
+
+
+def test_if_works_with_first_user(
+        user_list_template_with_no_fn: List[dict]
+) -> None:
+    restore_names(user_list_template_with_no_fn)
+    assert (
+        user_list_template_with_no_fn[0]["first_name"] == "Jack"
+    )
+
+
+def test_if_works_with_second_user(
+        user_list_template_with_no_fn: List[dict]
+) -> None:
+    restore_names(user_list_template_with_no_fn)
+    assert (
+        user_list_template_with_no_fn[1]["first_name"] == "Mike"
+    )
+
+
+def test_if_theres_a_first_name(
+        user_list_template_with_fn: List[dict]
+) -> None:
+    restore_names(user_list_template_with_fn)
+    assert (
+        user_list_template_with_fn[0]["first_name"] == "Jack"
+    )
