4be2d4800140f10b495893e6219948ab93915769
Dawid <kucharzjaw@gmail.com>
2025-10-17 11:06:39 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,24 +1,56 @@
 import pytest
 from app.restore_names import restore_names
+
+
 @pytest.mark.parametrize(
     "users",
     [
         [
-            {"first_name": "Jack", "last_name": "Holly", "full_name": "Jack Holly"},
-            {"first_name": None, "last_name": "Holly", "full_name": "Jack Holly"},
-            {"first_name": "", "last_name": "Holly", "full_name": "Jack Holly"},
-            {"last_name": "Holly", "full_name": "Jack Holly"},
+            {
+                "first_name": "Jack",
+                "last_name": "Holly",
+                "full_name": "Jack Holly"
+            },
+            {
+                "first_name": None,
+                "last_name": "Holly",
+                "full_name": "Jack Holly"
+            },
+            {
+                "first_name": "",
+                "last_name": "Holly",
+                "full_name": "Jack Holly"
+            },
+            {
+                "last_name": "Holly",
+                "full_name": "Jack Holly"
+            },
 
-            {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-            {"first_name": None, "last_name": "Adams", "full_name": "Mike Adams"},
-            {"first_name": "", "last_name": "Adams", "full_name": "Mike Adams"},
-            {"last_name": "Adams", "full_name": "Mike Adams"},
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            },
+            {
+                "first_name": None,
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            },
+            {
+                "first_name": "",
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams"
+            },
         ]
     ]
 
 
 )
-def test_restore_names(users) -> None:
+def test_restore_names(users: list[dict]) -> None:
 
     restore_names(users)
     for user in users:
