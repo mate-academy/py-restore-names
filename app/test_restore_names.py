a95b1359a5b19c5440353ef746f0ba294fa985d4
Anton Tsikhanovich <alluspeh@gmail.com>
2025-11-10 15:06:39 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,26 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture(scope="function")
+def users() -> list:
+    return [
+        {"first_name": "Jack",
+         "last_name": "Holy",
+         "full_name": "Jack Holy"},
+        {"first_name": "Mike",
+         "last_name": "Adams",
+         "full_name": "Mike Adams"},
+    ]
+
+
+def test_if_user_name_not_in_user(users: list) -> None:
+    for user in users:
+        if "first_name" not in user:
+            assert restore_names(users) == user["first_name"]
+
+
+def test_if_user_name_already_in_user(users: list) -> None:
+    for user in users:
+        if user["first_name"] is None:
+            assert restore_names(users) == user["first_name"]
