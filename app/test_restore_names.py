c03003fd5a6fedd4eb40f656f598100d797a0f61
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-11-11 23:28:08 +0000

    finish task

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "list_name, expected",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+        )
+    ],
+)
+def test_restore_names(list_name: list[dict], expected: list[dict]) -> None:
+    """Тестирует функцию restore_names на корректное восстановление имён."""
+    restore_names(list_name)
+    assert list_name == expected
