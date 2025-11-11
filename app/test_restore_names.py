f16ba1528d58e532a2d7cbb180f767af72e17941
HAIOVIY <gaevoywork@gmail.com>
2025-11-12 00:10:46 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,29 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def dict_template() -> list:
+    return [
+        {
+        "full_name": None
+        }
+    ]
+
+
+def test_restore_names_raise_exception(dict_template) -> None:
+    with pytest.raises(AttributeError):
+        restore_names(dict_template)
+
+
+@pytest.mark.parametrize(
+    "dict_template, expected",
+    [
+        ([{"full_name": "Peter Metr"}], [{"full_name": "Peter Metr", "first_name": "Peter"}]),
+        ([{"full_name": "Peter Metr", "first_name": "Peter"}], [{"full_name": "Peter Metr", "first_name": "Peter"}]),
+        ([{"full_name": "Peter Metr", "first_name": None}], [{"full_name": "Peter Metr", "first_name": "Peter"}]),
+    ]
+)
+def test_restore_names(dict_template, expected) -> None:
+    restore_names(dict_template)
+    assert dict_template == expected
