c59cf9a7c814e01158819850de6030eb5a327599
Mike <tradewcs@gmail.com>
2025-11-13 18:29:53 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def user() -> list[dict]:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Freecss",
+            "full_name": "Gon Freecss",
+        },
+        {
+            "last_name": "Zoldyck",
+            "full_name": "Killua Zoldyck",
+        },
+    ]
+
+
+def test_should_restore_missing_first_name(user: dict) -> None:
+    restore_names(user)
+    assert user == [
+        {
+            "first_name": "Gon",
+            "last_name": "Freecss",
+            "full_name": "Gon Freecss",
+        },
+        {
+            "first_name": "Killua",
+            "last_name": "Zoldyck",
+            "full_name": "Killua Zoldyck",
+        },
+    ]
