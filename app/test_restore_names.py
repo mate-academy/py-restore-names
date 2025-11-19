8d5bf29b12309f063890ed493b55048e84da64ac
Lukian <lukian55d@gmail.com>
2025-11-19 23:36:40 +0000

    done task

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,68 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_first_name_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    result = restore_names(users)
+
+    assert result is None
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_first_name_missing() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Mike"
+
+
+def test_does_not_modify_existing_first_name() -> None:
+    users = [
+        {
+            "first_name": "Sarah",
+            "last_name": "Connor",
+            "full_name": "Sarah Connor",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Sarah"
+
+
+def test_multiple_something_test() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Lukian",
+            "last_name": "Shkvarla",
+            "full_name": "Lukian Shkvarla",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
+    assert users[2]["first_name"] == "Lukian"
