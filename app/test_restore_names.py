4fd603c97efe4d1eb297b49df7edfc7ca5a12c30
Vova <vovakucin082@gmail.com>
2025-12-03 09:53:45 +0100

    code write

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,27 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def dict_user() -> list[dict]:
+    return [{
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy",
+    }]
+
+
+def test_return_restore_names(dict_user: list[dict]) -> None:
+    assert restore_names(dict_user) is None
+
+
+def test_first_name_is_none(dict_user: list[dict]) -> None:
+    dict_user[0]["first_name"] = None
+    restore_names(dict_user)
+    assert dict_user[0]["first_name"] == "Jack"
+
+
+def test_last_name_is_none(dict_user: list[dict]) -> None:
+    del dict_user[0]["first_name"]
+    restore_names(dict_user)
+    assert dict_user[0]["first_name"] == "Jack"
