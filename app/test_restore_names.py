9e5e7f24853e736c13a89985d646b601f6fa1728
Anatolii Hrechaniuk <genezisium@gmail.com>
2025-12-03 19:38:44 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,68 @@
-import pytest
+import copy
+
 from app.restore_names import restore_names
 
-# write your tests here
+users_expected = [
+    {
+        "first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams",
+    }
+]
+
+
+def test_first_name_not_in_user() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == users_expected
+
+
+def test_first_name_is_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == users_expected
+
+
+def test_first_name_is_valid() -> None:
+    users = [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == users_expected
+
+
+def test_full_name_has_one_word() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike",
+        }
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike",
+        }
+    ]
+
+
+def test_function_return_none() -> None:
+    users_copy = copy.deepcopy(users_expected)
+    assert restore_names(users_copy) is None
