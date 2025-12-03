0c1e7f92bc9b1b79329aa62efb8048b702e3379c
Paweł Dudek <dudek136@gmail.com>
2025-12-03 22:10:00 +0100

    Solution v2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,16 +1,13 @@
-import pytest
 from app.restore_names import restore_names
 
-users = [
-  {
+
+users = [{
     "first_name": None,
     "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
+    "full_name": "Jack Holy"},
+    {
     "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
+    "full_name": "Mike Adams"}
 ]
 
 users_after_restoration = [
@@ -18,19 +15,14 @@ users_after_restoration = [
         "first_name": "Jack",
         "last_name": "Holy",
         "full_name": "Jack Holy",
-    },
-    {
+    }, {
         "first_name": "Mike",
         "last_name": "Adams",
         "full_name": "Mike Adams",
     },
 ]
 
+
 def test_restore_names() -> None:
     restore_names(users)
     assert users == users_after_restoration
-
-
-
-
-
