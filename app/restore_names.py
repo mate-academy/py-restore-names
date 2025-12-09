afa1b4858b3163ac68588de0f545ad91c90999f8
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-09 13:14:28 -0300

    Solution

--- app/restore_names.py
+++ app/restore_names.py
@@ -5,4 +5,3 @@ def restore_names(users: List[dict]) -> None:
     for user in users:
         if "first_name" not in user or user["first_name"] is None:
             user["first_name"] = user["full_name"].split()[0]
-
--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,66 +4,65 @@ from unittest import mock
 
 
 @pytest.mark.parametrize(
-        "users, exp",
-        [([
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-], 
-[
-  {
-    "first_name": "Jack",
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "first_name": "Mike",
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-])]
+    "users, exp",
+    [([
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ],
+        [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ])]
 )
-def test_restore_names_works(users, exp):
+def test_restore_names_works(users: list, exp: list) -> None:
     restore_names(users)
     assert users == exp
 
 
-def test_restore_names_returns():
+def test_restore_names_returns() -> None:
     users = [
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
     expected = None
     actual = restore_names(users)
     assert expected == actual
 
 
-def test_restore_names_has_called():
+def test_restore_names_has_called() -> None:
     users = [
-  {
-    "first_name": None,
-    "last_name": "Holy",
-    "full_name": "Jack Holy",
-  },
-  {
-    "last_name": "Adams",
-    "full_name": "Mike Adams",
-  },
-]
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
     with mock.patch("app.restore_names.restore_names") as mock_restore:
         mock_restore(users)
         mock_restore.assert_called_once_with(users)
-    
