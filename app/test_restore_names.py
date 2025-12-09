dd40b91d073c208bd6610cb8a98dbc90fc2a40f7
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-09 13:02:03 -0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,69 @@
 import pytest
 from app.restore_names import restore_names
+from unittest import mock
 
-# write your tests here
+
+@pytest.mark.parametrize(
+        "users, exp",
+        [([
+  {
+    "first_name": None,
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+], 
+[
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+])]
+)
+def test_restore_names_works(users, exp):
+    restore_names(users)
+    assert users == exp
+
+
+def test_restore_names_returns():
+    users = [
+  {
+    "first_name": None,
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+    expected = None
+    actual = restore_names(users)
+    assert expected == actual
+
+
+def test_restore_names_has_called():
+    users = [
+  {
+    "first_name": None,
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+    with mock.patch("app.restore_names.restore_names") as mock_restore:
+        mock_restore(users)
+        mock_restore.assert_called_once_with(users)
+    
