b23e1b5596e59d521f28ee7ba79e2923c6d1be84
alexnrn2204 <alexnrn2204@gmail.com>
2025-12-10 21:52:17 +0200

    Solution1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,28 +1,37 @@
+import pytest
+from typing import List
 from app.restore_names import restore_names
 
 
-def test_only_missing_names() -> None:
-    users = [
-        {"last_name": "Adams",
-         "full_name": "Mike Adams"}
-    ]
-    restore_names(users)
-    assert users == [
-        {"first_name": "Mike",
-         "last_name": "Adams",
-         "full_name": "Mike Adams"}
-    ]
+@pytest.fixture()
+def users_template() -> List[dict]:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }]
 
 
-def test_only_none_names() -> None:
-    users = [
-        {"first_name": None,
-         "last_name": "Holy",
-         "full_name": "Jack Holy"}
-    ]
-    restore_names(users)
-    assert users == [
+def test_only_none_names(users_template: List[dict]) -> None:
+    del users_template[1]
+    restore_names(users_template)
+    assert users_template == [
         {"first_name": "Jack",
          "last_name": "Holy",
          "full_name": "Jack Holy"}
+    ]
+
+
+def test_only_missing_names(users_template: List[dict]) -> None:
+    del users_template[0]
+    restore_names(users_template)
+    assert users_template == [
+        {"first_name": "Mike",
+         "last_name": "Adams",
+         "full_name": "Mike Adams"}
     ]
