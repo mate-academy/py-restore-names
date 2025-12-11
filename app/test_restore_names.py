1c18b57dd588edcd7cc126368c707434f32644ac
Vladislav <ilichoff88@gmail.com>
2025-12-12 00:28:15 +0200

    'Solution5'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,65 +1,36 @@
 from typing import List
-import pytest
-
 from restore_names import restore_names
+import pytest
 
 
 @pytest.mark.parametrize(
     "input_users, expected_users",
     [
         (
-            [
-                {
-                    "first_name": None,
-                    "last_name": "Skywalker",
-                    "full_name": "Luke Skywalker",
-                }
-            ],
-            [
-                {
-                    "first_name": "Luke",
-                    "last_name": "Skywalker",
-                    "full_name": "Luke Skywalker",
-                }
-            ],
+            [{"first_name": None,
+              "last_name": "Skywalker",
+              "full_name": "Luke Skywalker"}],
+            [{"first_name": "Luke",
+              "last_name": "Skywalker",
+              "full_name": "Luke Skywalker"}]
         ),
         (
-            [
-                {
-                    "first_name": None,
-                    "last_name": "White",
-                    "full_name": "Walter White",
-                }
-            ],
-            [
-                {
-                    "first_name": "Walter",
-                    "last_name": "White",
-                    "full_name": "Walter White",
-                }
-            ],
+            [{"last_name": "White",
+              "full_name": "Walter White"}],
+            [{"first_name": "Walter",
+              "last_name": "White",
+              "full_name": "Walter White"}]
         ),
         (
-            [
-                {
-                    "first_name": "Rick",
-                    "last_name": "Sanchez",
-                    "full_name": "Rick Sanchez",
-                }
-            ],
-            [
-                {
-                    "first_name": "Rick",
-                    "last_name": "Sanchez",
-                    "full_name": "Rick Sanchez",
-                }
-            ],
+            [{"first_name": "Rick",
+              "last_name": "Sanchez",
+              "full_name": "Rick Sanchez"}],
+            [{"first_name": "Rick",
+              "last_name": "Sanchez",
+              "full_name": "Rick Sanchez"}]
         ),
-    ],
+    ]
 )
-def test_restore_names(
-    input_users: List[dict],
-    expected_users: List[dict],
-) -> None:
+def test_restore_names(input_users: List[dict], expected_users: List[dict]) -> None:
     restore_names(input_users)
     assert input_users == expected_users
