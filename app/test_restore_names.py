739c4703e56f183bfcb1b5257196d4cc72c6fa41
Oleh Klivanets <oleg.klivanets@gmail.com>
2025-12-17 15:13:49 +0200

    Add tests for restore_names function

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,90 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                }
+            ],
+            id="user_with_none_first_name",
+        ),
+        pytest.param(
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            [
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            id="user_without_first_name_key",
+        ),
+        pytest.param(
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                }
+            ],
+            id="user_with_first_name",
+        ),
+        pytest.param(
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Holy",
+                    "full_name": "John Holy",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+            ],
+            id="two_users_simultaneously",
+        ),
+        pytest.param([], [], id="empty_list"),
+    ],
+)
+def test_restore_names(users: list[dict], expected: list[dict]) -> None:
+    restore_names(users)
+    assert users == expected
