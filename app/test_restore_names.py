e75d61d638c6b5f99aafe9b4818f44a3a8571af8
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-25 14:17:44 +0100

    implement test_restore_names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,45 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def users_without_first_name() -> list[dict]:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    "expected",
+    [
+        [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+    ],
+    ids=["users without first name"]
+)
+def test_restore_names(
+    users_without_first_name: list[dict],
+    expected: list[dict]
+) -> None:
+    restore_names(users_without_first_name)
+    assert (
+        users_without_first_name == expected
+    ), f"Users first names should be: {expected}"
